b89620e1d1ab2927bf479d6039cf87f3e68d3bd9
Max.Pargmann <max.pargmann@dlr.de>
2021-08-12 14:52:23 +0200

    added function load_deflec and add_distortion. load_deflec works
    Signed-off-by: Max.Pargmann <max.pargmann@dlr.de>

--- main.py
+++ main.py
@@ -11,27 +11,36 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import torch as th
 import time
+import glob
+from PIL import Image
+import math
+from rotation import rot_apply, rot_as_euler, rot_from_matrix, rot_from_rotvec
+from scipy.spatial.transform import Rotation as R
 
-from utils import compute_receiver_intersections, heliostat_coord_system, define_heliostat, invert_bitmap, rotate_heliostat, sort_indices, to_prediction, draw_raytracer, draw_heliostat
+from utils import compute_receiver_intersections, curl, draw_raytracer, draw_heliostat, heliostat_coord_system, define_heliostat, rotate_heliostat, sample_bitmap, sample_bitmap_, add_distortion, load_deflec
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
 
 #####Parameters#####
-# grids = (int(DIM**2)//256//fac, fac//1) #cuda grid from threads , optimale anordnung
-# threads = (256, 1)
 seed = 0
 use_gpu = True
-epochs = 500
-bitmap_width = 50
-bitmap_height = 50
+# Using curl takes a _lot_ of memory
+use_curl = False
+#load defec settings 
+load_deflec_data = True
+filename = "Helio_AA33_Rim0_STRAL-Input.binp"
+take_n_vectors = 5000
+epochs = 2000
+bitmap_width = 256
+bitmap_height = 256
 
 
 th.manual_seed(0)
 device = th.device('cuda' if use_gpu and th.cuda.is_available() else 'cpu')
 
 ##Aimpoints
-aimpoint = th.tensor([-50,0,0], dtype=th.float32, device=device)
+aimpoint = th.tensor([-100,0,0], dtype=th.float32, device=device)
 planeNormal = th.tensor([1, 0, 0], dtype=th.float32, device=device) # Muss noch dynamisch gestaltet werden
 ##Receiver specific parameters
 planex = 10 # Receiver width
@@ -39,53 +48,72 @@ planey = 10 #Receiver height
 
 ####Heliostat specific Parameters
 h_width = 4 # in m
-h_height = 4 # in m
-rows = 64 #rows of reflection points. total number is rows**2
+h_height = 4 # in m 
+rows = 32 #rows of reflection points. total number is rows**2
 position_on_field = th.tensor([0,0,0], dtype=th.float32, device=device)
 
 #sunposition
-sun = th.tensor([0,1,0], dtype=th.float32, device=device)
+sun = th.tensor([0,0,1], dtype=th.float32, device=device)
 mean = th.tensor([0, 0], dtype=th.float32, device=device)
 cov = th.tensor([[0.000001, 0], [0, 0.000001]], dtype=th.float32, device=device)  # diagonal covariance, used for ray scattering
-num_rays = 1000
+
+num_rays = 100
 
 
-###Define derived variables#####
-
-total_bitmap = th.zeros([bitmap_height, bitmap_width], dtype=th.float32, device=device) # Flux density map for heliostat field
 
 
+##Define Target Heliostat##
+if load_deflec_data:
+    target_normal_vectors, target_hel_origin  = load_deflec(filename, take_n_vectors, device)
+else:
+    points_on_hel   = rows**2 # reflection points on hel
+    points_on_hel   = th.tensor(points_on_hel, dtype=th.float32, device=device)
+    target_hel_origin      = define_heliostat(h_height, h_width, rows, points_on_hel, device)
+    target_normal_vector   = th.tensor([0,0,1], dtype=th.float32, device=device)
+    target_normal_vectors  = th.tile(target_normal_vector, (len(target_hel_origin), 1))
 
 sun = sun/th.linalg.norm(sun)
+target_hel_coords = th.stack(heliostat_coord_system(position_on_field, sun, aimpoint))
+target_hel_rotated = rotate_heliostat(target_hel_origin,target_hel_coords)
+target_hel_in_field = target_hel_rotated+ position_on_field
 
-points_on_hel = rows**2 # reflection points on hel
-hel_origin = define_heliostat(h_height, h_width, rows, points_on_hel, device)
-hel_coordsystem = th.stack(heliostat_coord_system(position_on_field, sun, aimpoint))
-hel_rotated = rotate_heliostat(hel_origin,hel_coordsystem, points_on_hel)
-hel_in_field = hel_rotated+ position_on_field
-
-ray_directions =  th.tile(aimpoint- position_on_field, (len(hel_in_field), 1))
+del target_hel_origin
+del target_hel_coords
+del target_hel_rotated
 
 
+
+rotation = th.tensor([0,90,0], dtype=th.float32, device=device) # musste gerade schnell gehen, die rotation muss natürlich berechnet werden. Kannst du das nachholen? ich hatte über die th funktionen keinen Überblick.
+
+r = rot_from_rotvec(rotation, degrees= True)
+target_normal_vectors = rot_apply(r, target_normal_vectors.unsqueeze(-1)).squeeze()
+target_ray_directions = target_normal_vectors
+target_rayPoints = target_hel_in_field #Any point along the ray
 xi, yi = th.distributions.MultivariateNormal(mean, cov).sample((num_rays,)).T.to(device) # scatter rays a bit
-
-# print(as_)
-
-
-
-
-
-rayPoints = hel_in_field #Any point along the ray
 
 intersections = compute_receiver_intersections(
     planeNormal,
     aimpoint, # any point on plane
-    ray_directions,
-    rayPoints,
-    hel_in_field,
+    target_ray_directions,
+    target_rayPoints,
     xi,
     yi,
 )
+
+target_total_bitmap = sample_bitmap(intersections, planex, planey, bitmap_height, bitmap_width)
+targets = target_total_bitmap.detach().clone().unsqueeze(0)
+# plt.imshow(target_total_bitmap.detach().cpu().numpy(), cmap='jet')
+# plt.show()
+
+
+
+##Define ideal heliostat.
+
+ray_direction = (aimpoint- position_on_field)
+ray_directions =  th.tile(ray_direction, (len(target_hel_in_field), 1)) #works only for planar heliostat
+ray_directions += th.randn_like(ray_directions) * 0.3 # Da wir jetzt nicht mehr mit Idealen Heliostaten Rechnen kann das eigentlich weg, es schadet aber glaube ich auch nicht
+rayPoints = target_hel_in_field #maybe define the ideal heliostat on its own
+
 
 # draw_raytracer(hel_rotated.detach().cpu().numpy(),
 #                 hel_coordsystem.detach().cpu().numpy(),
@@ -94,50 +122,36 @@ intersections = compute_receiver_intersections(
 #                 intersections.detach().cpu().numpy(),
 #                 sun.detach().cpu().numpy())
 
-dx_ints = intersections[:, :, 1] +planex/2
-dy_ints = intersections[:, :, 2] +planey/2
-# checks the points of intersection  and chooses bins in bitmap
-indices = ( (0 <= dx_ints) & (dx_ints < planex) & (0 <= dy_ints) & (dy_ints < planey))
-
-x_int = (dx_ints[indices]/planex*bitmap_height).long()
-y_int = (dy_ints[indices]/planey*bitmap_width).long()
-total_bitmap.index_put_(
-    (x_int, y_int),
-    th.ones(len(x_int), dtype=th.float32, device=device),
-    accumulate=True,
-)
-
 
 
 
 # Dataset
-target_imgs = total_bitmap.detach().clone().unsqueeze(0)
-targets = th.stack(list(map(
-    lambda img: sort_indices(
-        invert_bitmap(img, planex, planey, bitmap_height, bitmap_width, add_noise=True),
-        bitmap_height,
-    ),
-    target_imgs,
-)))
-pred = to_prediction(intersections, bitmap_height)
-print(targets[0])
-# print('inversion error', th.nn.functional.mse_loss(pred, targets[0]))
+# When we load images, we should also normalize them here towards [0, 1]
 
 # Optimization setup
-# ray_directions += th.randn_like(ray_directions) * 0.1 # Durch den Invertierung gibt es schon einen minimalen Fehler, zeile kann übergangen werden bis der Algorithmus konvergiert
+
 ray_directions.requires_grad_(True)
 opt = th.optim.Adam([ray_directions], lr=3e-2)
 sched = th.optim.lr_scheduler.ReduceLROnPlateau(
     opt,
     factor=0.5,
     min_lr=1e-12,
-    patience=1000,
+    patience=10,
     verbose=True,
 )
 
+
+def loss_func(pred, target, compute_intersections, rayPoints):
+    loss = th.nn.functional.mse_loss(pred, target, 0.1)
+    if use_curl:
+        loss += th.sum(th.abs(curl(compute_intersections, rayPoints)))
+    return loss
+
+
 # Just for printing purposes
+im = plt.imshow(target_total_bitmap.detach().cpu().numpy(), cmap='jet')
+del target_total_bitmap
 epoch_shift_width = len(str(epochs))
-im = plt.imshow(total_bitmap.detach().cpu().numpy(), cmap='jet')
 
 xi, yi = th.distributions.MultivariateNormal(mean, cov).sample((num_rays,)).T.to(device) #has to be another xi and yi as in the preprocessing
 for epoch in range(epochs):
@@ -149,35 +163,32 @@ for epoch in range(epochs):
             planeNormal,
             aimpoint,
             ray_directions,
-            rayPoints, #Raypoints und hel_in_field scheinen aktuell das gleiche zu sein
-            hel_in_field, #Sobald wir mehrere Bilder verwenden stimmt das hier nicht mehr, dann sollte hier etwas stehen wie hel_in_field[target], da mehrere Bilder unterschiedlichen Sonnenstanden entsprechen und die Punkte sich dann auch woanders befinden.
+            rayPoints, 
             xi,
             yi,
         )
-        pred = to_prediction(intersections, bitmap_height)
-        loss += th.nn.functional.l1_loss(pred, target, 0.1)
-        
-    if epoch %  5== 0 and not epoch == 0:#
-        dx_pred = intersections[:, :, 1] +planex/2
-        dy_pred = intersections[:, :, 2] +planey/2
-        indices = ( (0 <= dx_ints) & (dx_ints < planex) & (0 <= dy_ints) & (dy_ints < planey))
-        x_pred = (dx_pred[indices]/planex*bitmap_height).long()
-        y_pred = (dy_pred[indices]/planey*bitmap_width).long()
-        total_bitmap.index_put_(
-        (x_pred, y_pred),
-        th.ones(len(x_pred), dtype=th.float32, device=device),
-        accumulate=True,
+        dx_ints = intersections[:, :, 1] +planex/2
+        dy_ints = intersections[:, :, 2] +planey/2
+        indices = (-1 <= dx_ints) & (dx_ints < planex + 1) & (-1 <= dy_ints) & (dy_ints < planey + 1)
+        pred = sample_bitmap_(dx_ints, dy_ints, indices, planex, planey, bitmap_height, bitmap_width)
+        loss += loss_func(
+            pred,
+            target,
+            lambda rayPoints: compute_receiver_intersections(
+                planeNormal,
+                aimpoint,
+                ray_directions,
+                rayPoints,
+                xi,
+                yi,
+            ),
+            rayPoints,
         )
-        im.set_data(total_bitmap.detach().cpu().numpy())
-        im.autoscale()
-        plt.pause(0.001)  # In interactive mode, need a small delay to get the plot to appear
-        plt.draw()
-        # print(pred)
-        # exit()
-        
-            # exit()
 
-    
+    if epoch %  10== 0:#
+        im.set_data(pred.detach().cpu().numpy())
+        im.autoscale()
+        plt.savefig(f"images\\{epoch}.png")
 
     loss /= len(targets)
     loss.backward()
@@ -188,7 +199,6 @@ for epoch in range(epochs):
     opt.step()
     sched.step(loss)
     if epoch % 10 == 0:
-        indices = (0 <= dx_ints) & (dx_ints < planex) & (0 <= dy_ints) & (dy_ints < planey)
         num_missed = indices.numel() - indices.count_nonzero()
         print(
             f'[{epoch:>{epoch_shift_width}}/{epochs}] '
@@ -196,17 +206,11 @@ for epoch in range(epochs):
             f'missed: {num_missed.detach().cpu().item()}'
         )
 
-# print(ray_directions)
-# draw_heliostat(hel_rotated.detach().cpu().numpy(), ray_directions.detach().cpu().numpy())
-# total_bitmap.fill_(0)
-
-# x_int = x_int.long()
-# y_int = y_int.long()
-# total_bitmap.index_put_(
-#     (x_int, y_int),
-#     th.ones(len(x_int), dtype=th.float32, device=device),
-#     accumulate=True,
-# )
-# plt.imshow(total_bitmap.detach().cpu().numpy(), cmap='jet')
-# plt.show()
-
+fp_in = "images/*.png"
+fp_out = "images/results.gif"
+img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
+img.save(fp=fp_out, format='GIF', append_images=imgs,
+         save_all=True, duration=500, loop=1)
+total_bitmap = sample_bitmap(intersections, planex, planey, bitmap_height, bitmap_width)
+plt.imshow(total_bitmap.detach().cpu().numpy(), cmap='jet')
+plt.show()
--- utils.py
+++ utils.py
@@ -6,12 +6,63 @@ Created on Mon Jul  5 12:38:11 2021
 """
 import matplotlib.pyplot as plt
 import torch as th
-
+import struct
+import numpy as np
 from rotation import rot_apply, rot_as_euler, rot_from_matrix, rot_from_rotvec
 
+device = th.device('cuda' if th.cuda.is_available() else 'cpu')
+
+def load_deflec(filename, take_n_vectors,device="cpu", concentratorHeader_struct_fmt ='=5f2I2f',facetHeader_struct_fmt = '=i9fI', ray_struct_fmt = '=7f'): 
+    """
+    binp_filename : string including file ending binp or bpro
+    Concentrator Header is expected to be float[5], unsigned int[2], float[2]
+    Facet Header is expected to be  int, float[9], unsigned int
+    Ray is Decribed by 3 positions floats, 3 directions floats, 1 power float. Therefor expected to be float[7]
+    """
+    concentratorHeader_struct_len = struct.calcsize(concentratorHeader_struct_fmt)
+    facetHeader_struct_len = struct.calcsize(facetHeader_struct_fmt)
+    ray_struct_len = struct.calcsize(ray_struct_fmt)
+    
+    positions= []
+    directions = []
+    # powers = []
+    with open(filename, "rb") as file:
+        byte_data = file.read(concentratorHeader_struct_len)
+        concentratorHeader_data = struct.Struct(concentratorHeader_struct_fmt).unpack_from(byte_data)
+        print("READING bpro filename: " + filename)
+    
+        #hel_pos = concentratorHeader_data[0:3]
+        #width_height = concentratorHeader_data[3:5]
+        #offsets = concentratorHeader_data[7:9]
+        n_xy = concentratorHeader_data[5:7]
+        
+        
+        nFacets = n_xy[0] * n_xy[1]
+        for f in range(nFacets):
+        # for f in range(1):
+            byte_data = file.read(facetHeader_struct_len)
+            facetHeader_data = struct.Struct(facetHeader_struct_fmt).unpack_from(byte_data)
+            
+            #facetshape = facetHeader_data[0] # 0 for square, 1 for round 2 triangle ....
+            #facet_pos = facetHeader_data[1:4]
+            #facet_vec_x = facetHeader_data[4:7]
+            #facet_vec_y = facetHeader_data[7:10]
+            n_rays = facetHeader_data[10]
+
+            for r in range(n_rays):
+                byte_data = file.read(ray_struct_len)	
+                ray_data = struct.Struct(ray_struct_fmt).unpack_from(byte_data)
+                
+                positions.append([ray_data[0],ray_data[1],ray_data[2]])
+                directions.append([ray_data[3],ray_data[4],ray_data[5]])
+                # powers.append(ray_data[6])
+
+        directions = th.tensor(directions[0::int(len(directions)/take_n_vectors)], device=device)
+        positions = th.tensor(positions[0::int(len(positions)/take_n_vectors)], device = device)
+        return directions, positions #,powers
 
 def define_heliostat(h_height, h_width, rows, points_on_hel, device):
-    columns = points_on_hel//rows
+    columns = int(points_on_hel)//rows
     column = th.arange(columns, device=device)
     row = th.arange(rows, device=device)
 
@@ -24,7 +75,7 @@ def define_heliostat(h_height, h_width, rows, points_on_hel, device):
     h = th.hstack(list(map(lambda t: t.unsqueeze(-1), [h_x, h_y, h_z]))).reshape(len(h_x), -1)
     return h
 
-def rotate_heliostat(h,hel_coordsystem, points_on_hel):
+def rotate_heliostat(h,hel_coordsystem):
     r = rot_from_matrix(hel_coordsystem)
     euler = rot_as_euler(r, 'xyx', degrees = True)
     ele_degrees = 90-euler[2]
@@ -43,7 +94,31 @@ def rotate_heliostat(h,hel_coordsystem, points_on_hel):
     h_rotated = rot_apply(azi, rot_apply(ele, h.unsqueeze(-1))) # darray with all heliostats (#heliostats, 3 coords)
     return h_rotated.squeeze(-1)
 
+def add_distortion(vector_field, distortion_center, points_on_hel, threshold = 1.5, positive = True):
+    device
+    dc_x, dc_y = distortion_center
+    
+    x,y = th.meshgrid(th.linspace(-5+dc_x,5+dc_x,int(th.sqrt(points_on_hel))),
+                      th.linspace(-5+dc_y,5+dc_y,int(th.sqrt(points_on_hel)))
+                      )
+    distortion_x = (x/2).reshape(int(points_on_hel))
+    distortion_y = (y/2).reshape(int(points_on_hel))
+    distortion = th.zeros((int(points_on_hel),3), device=device)
+    distortion[:,0] = distortion_x
+    distortion[:,1] = distortion_y
+    
+    for i in range(len(distortion)):
+        if th.norm(distortion[i]) > threshold:
+          distortion[i] = th.tensor([0,0,0], device=device)
 
+    if positive:
+        new_vec_field = (vector_field - distortion)/th.norm(vector_field - distortion, dim=1)[:,None]
+    else:
+        new_vec_field = (vector_field + distortion)/th.norm(vector_field + distortion, dim=1)[:,None]
+        
+    # print(new_vec_field)
+    # exit()
+    return new_vec_field
 
 def flatten_aimpoints(aimpoints):
     X = th.flatten(aimpoints[0])
@@ -169,12 +244,11 @@ def compute_receiver_intersections(
         planeNormal,
         planePoint,
         ray_directions,
-        rayPoints,
         hel_in_field,
         xi,
         yi,
 ):
-    intersections = LinePlaneCollision(planeNormal, planePoint, ray_directions, rayPoints)
+    intersections = LinePlaneCollision(planeNormal, planePoint, ray_directions, hel_in_field)
     as_ = intersections
     has = as_-hel_in_field
 
@@ -192,14 +266,14 @@ def compute_receiver_intersections(
 
     # rays_tmp: first rotate aimpoint in right coord system, aplay xi,yi distortion, rotate back
     rotated_has = th.matmul(rotates, has.unsqueeze(-1))
-
+    # rays = rotated_has.transpose(0, -1).transpose(1, -1)
     rays = th.matmul(inv_rot,
-                               Rz(yi,
+                                Rz(yi,
                                   Ry(xi,
-                                     rotated_has
-                                     )
+                                      rotated_has
+                                      )
                                   ).transpose(0, -1)
-                               ).transpose(0, -1).transpose(1, -1)
+                                ).transpose(0, -1).transpose(1, -1)
 
 
     # rays = rays.to(th.float32)
@@ -209,34 +283,89 @@ def compute_receiver_intersections(
     # print(intersections)
     return intersections
 
-def invert_bitmap(img, planex, planey, bitmap_height, bitmap_width, add_noise=True):
-    assert img.ndim == 2, 'need a 2-D picture to invert it'
-    indices = th.empty(int(img.sum().item()), 2, device=img.device)
-    index_row = 0
-    for row in range(img.shape[0]):
-        for col in range(img.shape[1]):
-            intensity = int(img[row, col])
-            curr_indices = th.tile(
-                th.tensor([row, col], dtype=th.float32, device=img.device),
-                (intensity, 1),
-            )
-            if add_noise:
-                curr_indices += th.rand_like(curr_indices)
-            indices[index_row:index_row + intensity] = curr_indices
+def sample_bitmap(intersections, planex, planey, bitmap_height, bitmap_width):
+    dx_ints = intersections[:, :, 1] +planex/2
+    dy_ints = intersections[:, :, 2] +planey/2
+    # checks the points of intersection  and chooses bins in bitmap
+    indices = ( (-1 <= dx_ints) & (dx_ints < planex + 1) & (-1 <= dy_ints) & (dy_ints < planey + 1))
+    return sample_bitmap_(dx_ints, dy_ints, indices, planex, planey, bitmap_height, bitmap_width)
 
-            index_row += intensity
-            # indices[row:] = th.tile(th.tensor([row, col], dtype=th.float32), (intensity, 1)))
-    indices[:, 0] = indices[:, 0] / bitmap_height * planex - planex / 2
-    indices[:, 1] = indices[:, 1] / bitmap_width * planey - planey / 2
-    return indices
+def sample_bitmap_(dx_ints, dy_ints, indices, planex, planey, bitmap_height, bitmap_width):
 
-def sort_indices(tensor, num_rows, descending=False):
-    assert tensor.ndim == 2, 'can only sort 2-D tensors'
-    index_tensor = tensor[:, 0] * num_rows + tensor[:, 1]
-    indices = index_tensor.sort(descending=descending).indices
-    return tensor[indices]
+    x_ints = dx_ints[indices]/planex*bitmap_height
+    y_ints = dy_ints[indices]/planey*bitmap_width
 
-def to_prediction(intersections, bitmap_height):
-    pred = intersections[:, :, 1:3].reshape(-1, 2)
-    pred = sort_indices(pred, bitmap_height)
-    return pred
+    x_inds_low = x_ints.long()
+    y_inds_low = y_ints.long()
+    x_inds_high = x_inds_low + 1
+    y_inds_high = y_inds_low + 1
+
+    x_ints_low = x_inds_high - x_ints
+    y_ints_low = y_inds_high - y_ints
+    x_ints_high = x_ints - x_inds_low
+    y_ints_high = y_ints - y_inds_low
+    del x_ints
+    del y_ints
+
+    x_inds_1 = x_inds_low
+    y_inds_1 = y_inds_low
+    ints_1 = x_ints_low * y_ints_low
+
+    x_inds_2 = x_inds_high
+    y_inds_2 = y_inds_low
+    ints_2 = x_ints_high * y_ints_low
+
+    x_inds_3 = x_inds_low
+    y_inds_3 = y_inds_high
+    ints_3 = x_ints_low * y_ints_high
+    del x_inds_low
+    del y_inds_low
+    del x_ints_low
+    del y_ints_low
+
+    x_inds_4 = x_inds_high
+    y_inds_4 = y_inds_high
+    ints_4 = x_ints_high * y_ints_high
+    del x_inds_high
+    del y_inds_high
+    del x_ints_high
+    del y_ints_high
+
+    x_inds = th.hstack([x_inds_1, x_inds_2, x_inds_3, x_inds_4]).long().ravel()
+    del x_inds_1
+    del x_inds_2
+    del x_inds_3
+    del x_inds_4
+
+    y_inds = th.hstack([y_inds_1, y_inds_2, y_inds_3, y_inds_4]).long().ravel()
+    del y_inds_1
+    del y_inds_2
+    del y_inds_3
+    del y_inds_4
+
+    ints = th.hstack([ints_1, ints_2, ints_3, ints_4]).ravel()
+    del ints_1
+    del ints_2
+    del ints_3
+    del ints_4
+    # Normalize
+    ints = ints / th.max(ints)
+
+    indices = (0 <= x_inds) & (x_inds < bitmap_width) & (0 <= y_inds) & (y_inds < bitmap_height)
+
+    total_bitmap = th.zeros([bitmap_height, bitmap_width], dtype=th.float32, device=dx_ints.device) # Flux density map for heliostat field
+    total_bitmap.index_put_(
+        (x_inds[indices], y_inds[indices]),
+        ints[indices],
+        accumulate=True,
+    )
+    return total_bitmap
+
+def curl(f, arg):
+    jac = th.autograd.functional.jacobian(f, arg, create_graph=True)
+
+    rot_x = jac[2][1] - jac[1][2]
+    rot_y = jac[0][2] - jac[2][0]
+    rot_z = jac[1][0] - jac[0][1]
+    print(rot_x)
+    return th.tensor([rot_x, rot_y, rot_z])
