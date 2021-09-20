9994f13ead85a489df9f36cf4a48949e143b7732
ja.ebert <ja.ebert@fz-juelich.de>
2021-09-20 13:54:59 +0200

    Adjust code style
    And fix comment placement

--- defaults.py
+++ defaults.py
@@ -2,37 +2,40 @@ from yacs.config import CfgNode as CN
 import os
 
 _C = CN()
-_C.ID                                   = 'Default' # UNIQUE EXPERIMENT IDENTIFIER
+# UNIQUE EXPERIMENT IDENTIFIER
+_C.ID                                   = 'Default'
 _C.LOGDIR                               = 'Images'
 _C.SEED                                 = 0
 _C.USE_GPU                              = True
 _C.USE_CURL                             = False
-_C.USE_NURBS                            = False # If setting this to `False`, be aware that the NURBS surface will
-                                                # always be evaluated at each surface position independently of the ray
-                                                # origins.
-
-
+_C.USE_NURBS                            = True
 
 # NURBS settings
 
 _C.NURBS                                = CN()
+# If setting this to `False`, be aware that the NURBS surface will
+# always be evaluated at each surface position independently of the ray
+# origins.
 _C.NURBS.SET_UP_WITH_KNOWLEDGE          = True
 _C.NURBS.FIX_SPLINE_CTRL_WEIGHTS        = True
 _C.NURBS.SPLINE_DEGREE                  = 2
 
-_C.H                                    = CN() # H = Heliostat
-_C.H.POSITION_ON_FIELD                  = [0,0,0] # in m
-_C.H.SHAPE                              = "Ideal" # Possible Modes: "Ideal", "Real", "Other" || "Real" For Deflec Data
+# H = Heliostat
+_C.H                                    = CN()
+_C.H.POSITION_ON_FIELD                  = [0, 0, 0] # in m
+# Possible Modes: "Ideal", "Real", "Other"
+# "Real" For Deflec Data
+_C.H.SHAPE                              = "Ideal"
 
 _C.H.IDEAL                              = CN()
-_C.H.IDEAL.NORMAL_VECS                  = [0,0,1]
+_C.H.IDEAL.NORMAL_VECS                  = [0, 0, 1]
 _C.H.IDEAL.WIDTH                        = 4 # in m
 _C.H.IDEAL.HEIGHT                       = 4 # in m
 _C.H.IDEAL.ROWS                         = 32
 _C.H.IDEAL.COLS                         = 32
 
 _C.H.NURBS                              = CN()
-_C.H.NURBS.NORMAL_VECS                  = [0,0,1]
+_C.H.NURBS.NORMAL_VECS                  = [0, 0, 1]
 _C.H.NURBS.WIDTH                        = 4 # in m
 _C.H.NURBS.HEIGHT                       = 4 # in m
 _C.H.NURBS.ROWS                         = 32
@@ -50,10 +53,12 @@ _C.H.OTHER                              = CN()
 
 # TODO add heliostat up vec ("rotation")
 
-_C.AC                                   = CN() # AC = Ambiant Conditions
+# AC = Ambiant Conditions
+_C.AC                                   = CN()
 _C.AC.RECEIVER                          = CN()
-_C.AC.RECEIVER.CENTER                   = [-25,0,0] # in m in global coordinates
-_C.AC.RECEIVER.PLANE_NORMAL             = [1,0,0] # NWU
+# in m in global coordinates
+_C.AC.RECEIVER.CENTER                   = [-25, 0, 0]
+_C.AC.RECEIVER.PLANE_NORMAL             = [1, 0, 0] # NWU
 _C.AC.RECEIVER.PLANE_X                  = 10 # in m
 _C.AC.RECEIVER.PLANE_Y                  = 10 # in m
 # These X and Y are height and width respectively.
@@ -61,7 +66,7 @@ _C.AC.RECEIVER.RESOLUTION_X             = 256
 _C.AC.RECEIVER.RESOLUTION_Y             = 256
 
 _C.AC.SUN                               = CN()
-_C.AC.SUN.ORIGIN                        = [-1,0,0]
+_C.AC.SUN.ORIGIN                        = [-1, 0, 0]
 _C.AC.SUN.GENERATE_N_RAYS               = 1000
 _C.AC.SUN.DISTRIBUTION                  = "Normal"
 
@@ -69,15 +74,8 @@ _C.AC.SUN.NORMAL_DIST                   = CN()
 _C.AC.SUN.NORMAL_DIST.MEAN              = [0,0]
 _C.AC.SUN.NORMAL_DIST.COV               = [[0.002090**2, 0], [0, 0.002090**2]]
 
-
-
 _C.TRAIN_PARAMS                         = CN()
 _C.TRAIN_PARAMS.EPOCHS                  = 2000
-
-
-
-
-
 
 
 def get_cfg_defaults():
