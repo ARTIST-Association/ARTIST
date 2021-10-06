aea275bdfbcc01793fff8ac34bc1a13c4aa8e97e
ja.ebert <ja.ebert@fz-juelich.de>
2021-10-06 09:34:27 +0200

    Fix config being respected more
    Previously, we subtracted a point due to the edge evaluation change.
    This resulted in fewer points than specified. We simply add one now to
    achieve the same result with one more point, again respecting the config
    perfectly.

--- heliostat_models.py
+++ heliostat_models.py
@@ -83,20 +83,20 @@ def ideal_heliostat(ideal_configs, device):
     cfg = ideal_configs
 
     columns = cfg.COLS
-    column = th.arange(columns, device=device)
-    row = th.arange(cfg.ROWS, device=device)
+    column = th.arange(columns + 1, device=device)
+    row = th.arange(cfg.ROWS + 1, device=device)
 
     h_x = (row/(cfg.ROWS-1) * cfg.HEIGHT) - (cfg.HEIGHT / 2)
     # Use points at centers of grid squares.
     h_x = h_x[:-1] + (h_x[1:] - h_x[:-1]) / 2
     # As we lost a point in the step above, subtract one.
-    h_x = th.tile(h_x, (columns - 1,))
+    h_x = th.tile(h_x, (columns,))
     # heliostat y position
     h_y = (column/(columns-1) * cfg.WIDTH) - (cfg.WIDTH / 2)
     # Use points at centers of grid squares.
     h_y = h_y[:-1] + (h_y[1:] - h_y[:-1]) / 2
     # As we lost a point in the step above, subtract one.
-    h_y = th.tile(h_y.unsqueeze(-1), (1, cfg.ROWS - 1)).ravel()
+    h_y = th.tile(h_y.unsqueeze(-1), (1, cfg.ROWS)).ravel()
     h_z = th.zeros_like(h_x)
 
     h = th.hstack(list(map(
