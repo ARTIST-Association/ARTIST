c7e09bb2b4f91b7a05401784a0cfcabdc3c58732
ja.ebert <ja.ebert@fz-juelich.de>
2022-01-14 10:19:47 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='diff_stral',
+    python_requires='>=3.6<3.10',
+    version='0.0.1',
+    install_requires=[
+        'matplotlib',
+        'mpl_toolkits',
+        'numpy',
+        'torch',
+        'yacs',
+    ],
+)
