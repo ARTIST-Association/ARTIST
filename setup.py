448f475c4512fdb47f044a68c26da97fb1dfc540
ja.ebert <ja.ebert@fz-juelich.de>
2022-01-14 10:32:39 +0100

    Specify version requirements
    These are chosen a bit arbitrarily.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     python_requires='>=3.6<3.10',
     version='0.0.1',
     install_requires=[
-        'matplotlib',
-        'numpy',
-        'tensorboard',
-        'torch',
-        'yacs',
+        'matplotlib>=3.4<4.0,',
+        'numpy>=1.17,<2.0',
+        'tensorboard>=2.0,<3.0',
+        'torch>=1.8,<2.0',
+        'yacs>=0.1,<0.2',
     ],
 )
