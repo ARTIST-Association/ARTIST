440e3de12957b0dbbdb50e91789cda42963bb634
ja.ebert <ja.ebert@fz-juelich.de>
2022-09-15 11:01:57 +0200

    Update cache version

--- disk_cache.py
+++ disk_cache.py
@@ -32,7 +32,7 @@ DISABLE_DISK_CACHE = False
 
 # This magic number should be updated to invalidate out-of-date caches
 # due to breaking changes.
-_CACHE_VERSION = 0
+_CACHE_VERSION = 1
 
 _DYNAMIC_CONFIG_KEYS = ['LOGDIR']
 
