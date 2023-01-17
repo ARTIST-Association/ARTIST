4feb1ec9a06af29350529ce0da42b8719c8f7f20
ja.ebert <ja.ebert@fz-juelich.de>
2023-01-17 12:11:07 +0100

    Clean up dataset caching

--- dataset_cache.py
+++ dataset_cache.py
@@ -1,32 +1,31 @@
+import functools
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Optional,
+    Tuple,
+    Union,
+)
+
 import torch
 import torch as th
 from torch.utils.tensorboard import SummaryWriter
 from yacs.config import CfgNode
-import functools
-import disk_cache
+
 import data
-from heliostat_models import AbstractHeliostat, Heliostat
+import disk_cache
 from environment import Environment
-from typing import (
-    Any,
-    Callable,
-    cast,
-    Dict,
-    List,
-    Optional,
-    Tuple,
-    Type,
-    Union,
-)
-def set_up_dataset_caching(
+from heliostat_models import AbstractHeliostat
+
+
+def make_cached_generate_sun_array_factory(
         device: th.device,
-        writer: Optional[SummaryWriter],
-) -> Tuple[
-    Tuple[Callable, Callable],
-    Tuple[
-        Callable,
-        Callable,
-        Callable,
+) -> Callable[
+    [str],
+    Callable[
+        [CfgNode, th.device, Optional[torch.Tensor], Optional[str]],
+        Tuple[torch.Tensor, Union[torch.Tensor, Dict[str, Any]]],
     ],
 ]:
     def make_cached_generate_sun_array(
@@ -43,6 +42,42 @@ def set_up_dataset_caching(
             ignore_argnums=[1],
         )
 
+    return make_cached_generate_sun_array
+
+
+def make_cached_generate_dataset_factory(
+        device: th.device,
+        writer: Optional[SummaryWriter],
+) -> Union[
+    Callable[
+        [str],
+        Callable[
+            [
+                AbstractHeliostat,
+                Environment,
+                torch.Tensor,
+                Optional[str],
+                str,
+                Optional[SummaryWriter],
+            ],
+            torch.Tensor,
+        ],
+    ],
+    Callable[
+        [str, bool],
+        Callable[
+            [
+                AbstractHeliostat,
+                Environment,
+                torch.Tensor,
+                Optional[str],
+                str,
+                Optional[SummaryWriter],
+            ],
+            torch.Tensor,
+        ],
+    ],
+]:
     def make_cached_generate_dataset(
             prefix: str,
             tb_log: bool = True,
@@ -74,6 +109,25 @@ def set_up_dataset_caching(
             on_load=log_dataset,
             ignore_argnums=[3, 4, 5],
         )
+
+    return make_cached_generate_dataset
+
+
+def set_up_dataset_caching(
+        device: th.device,
+        writer: Optional[SummaryWriter],
+) -> Tuple[
+    Tuple[Callable, Callable],
+    Tuple[
+        Callable,
+        Callable,
+        Callable,
+    ],
+]:
+    make_cached_generate_sun_array = make_cached_generate_sun_array_factory(
+        device)
+    make_cached_generate_dataset = make_cached_generate_dataset_factory(
+        device, writer)
 
     return (
         (
@@ -102,64 +156,23 @@ def set_up_test_dataset_caching(
         Callable,
     ],
 ]:
-    def make_cached_generate_sun_array(
-            prefix: str = '',
-    ) -> Callable[
-        [CfgNode, th.device, Optional[torch.Tensor], Optional[str]],
-        Tuple[torch.Tensor, Union[torch.Tensor, Dict[str, Any]]],
-    ]:
-        return disk_cache.disk_cache(
-            data.generate_sun_array,
-            device,
-            'cached',
-            prefix,
-            ignore_argnums=[1],
-        )
-
-    def make_cached_generate_dataset(
-            prefix: str,
-            tb_log: bool = True,
-    ) -> Callable[
-        [
-            AbstractHeliostat,
-            Environment,
-            torch.Tensor,
-            Optional[str],
-            str,
-            Optional[SummaryWriter],
-        ],
-        torch.Tensor,
-    ]:
-        if tb_log:
-            log_dataset: Optional[disk_cache.OnLoadFn] = functools.partial(
-                data.log_dataset,
-                prefix,
-                writer,
-            )
-        else:
-            log_dataset = None
-
-        return disk_cache.disk_cache(
-            data.generate_dataset,
-            device,
-            'cached',
-            prefix,
-            on_load=log_dataset,
-            ignore_argnums=[3, 4, 5],
-        )
+    make_cached_generate_sun_array = make_cached_generate_sun_array_factory(
+        device)
+    make_cached_generate_dataset = make_cached_generate_dataset_factory(
+        device, writer)
 
     return (
         (
-        make_cached_generate_sun_array('grid_'),
-        make_cached_generate_sun_array('spheric_'),
-        make_cached_generate_sun_array('season_'),
+            make_cached_generate_sun_array('grid_'),
+            make_cached_generate_sun_array('spheric_'),
+            make_cached_generate_sun_array('season_'),
         ),
         (
-        make_cached_generate_dataset('grid', False),
-        make_cached_generate_dataset('naive_grid', False),
-        make_cached_generate_dataset('spheric', False),
-        make_cached_generate_dataset('naive_spheric', False),
-        make_cached_generate_dataset('season', False),
-        make_cached_generate_dataset('naive_season', False),
+            make_cached_generate_dataset('grid', False),
+            make_cached_generate_dataset('naive_grid', False),
+            make_cached_generate_dataset('spheric', False),
+            make_cached_generate_dataset('naive_spheric', False),
+            make_cached_generate_dataset('season', False),
+            make_cached_generate_dataset('naive_season', False),
         ),
     )
