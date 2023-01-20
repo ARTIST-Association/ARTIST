d8b48803fe71f3b868986431e9ea42c38ad3e007
ja.ebert <ja.ebert@fz-juelich.de>
2023-01-20 16:11:32 +0100

    Clean up

--- heliostat_models.py
+++ heliostat_models.py
@@ -78,7 +78,7 @@ def load_heliostat_position_file(
 ]:
     df = pd.read_json(json_file_path, orient="table")
     values = df.loc[df['Name'] == heliostat_name]
-    name = values["Name"]
+    # name = values["Name"]
     position = values["Position"].item()
     facet_positions = values["FacetPositions"].item()
     facet_spans_n = values["FacetSpansN"].item()
@@ -1418,7 +1418,13 @@ class AlignedHeliostat(AbstractHeliostat):
             offset = len(discrete_points)
 
             # Facet repositioning (not active)
-            if reposition and not isinstance(self._heliostat.canting_algo, ActiveCanting):
+            if (
+                    reposition
+                    and not isinstance(
+                        self._heliostat.canting_algo,
+                        ActiveCanting,
+                    )
+            ):
                 discrete_points = discrete_points + position
 
             # Canting
@@ -1433,9 +1439,11 @@ class AlignedHeliostat(AbstractHeliostat):
                 align_origin,
             )
             # Facet repositioning (active)
-            if reposition and isinstance(self._heliostat.canting_algo, ActiveCanting):
+            if (
+                    reposition
+                    and isinstance(self._heliostat.canting_algo, ActiveCanting)
+            ):
                 discrete_points_rotated = discrete_points_rotated + position
-
 
             hel_rotated[i:i + offset] = discrete_points_rotated
             normal_vectors_rotated[i:i + offset] = normals_rotated
