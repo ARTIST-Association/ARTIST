56a852712db86e3550e62cae054255fb2869162b
Marie Weiel <marie.weiel@kit.edu>
2023-11-14 15:05:58 +0100

    apply black code style and make variable names all lowercase

--- artist/physics_objects/heliostats/heliostat.py
+++ artist/physics_objects/heliostats/heliostat.py
@@ -2,16 +2,15 @@ import ConcentratorModule
 import AlignmentModule
 from module import AModule
 
+
 class HeliostatModule(AModule):
-    def __init__(
-        self,
-        concentrator: ConcentratorModule,
-        alignment: AlignmentModule
-    ):
+    def __init__(self, concentrator: ConcentratorModule, alignment: AlignmentModule):
         self.concentrator = concentrator
         self.alignment = alignment
 
-    def get_aligned_surface(self, Datapoint):
+    def get_aligned_surface(self, datapoint):
         surface_points, surface_normals = self.concentrator.get_surface()
-        aligned_surface_points, aligned_surface_normals = self.alignment.align_surface(Datapoint, surface_points, surface_normals)
+        aligned_surface_points, aligned_surface_normals = self.alignment.align_surface(
+            datapoint, surface_points, surface_normals
+        )
         return aligned_surface_points, aligned_surface_normals
