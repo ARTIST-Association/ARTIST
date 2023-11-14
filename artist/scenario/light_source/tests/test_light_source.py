3dd61453b9e9d39d9afaf2e55f0617f97bcb1204
Marie Weiel <marie.weiel@kit.edu>
2023-11-14 15:17:38 +0100

    apply black code style

--- artist/scenario/light_source/tests/test_light_source.py
+++ artist/scenario/light_source/tests/test_light_source.py
@@ -1,7 +1,8 @@
 import sys
 
 from matplotlib import pyplot as plt
-sys.path.append('../../..')
+
+sys.path.append("../../..")
 import unittest
 import torch
 from PIL import Image
@@ -10,31 +11,39 @@ from artist.scenario.light_source.sun import Sun
 from artist.io.datapoint import HeliostatDataPoint, HeliostatDataPointLabel
 from artist.physics_objects.heliostats.alignment.alignment import AlignmentModule
 
+
 class TestASunModule(unittest.TestCase):
     def setUp(self):
         torch.manual_seed(0)
         self.light_direction = torch.tensor([0.0, 0.0, 1.0])
         self.heliostat_position = torch.tensor([0.0, 10.0, 0.0])
         self.receiver_center = torch.tensor([0.0, -100.0, 0.0])
-        
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.sun = Sun("Normal", 10000, [0, 0], [[4.3681e-06, 0], [0, 4.3681e-06]], device)
-        surface_normals = torch.tensor([[0, 0, 1],
-                                        [0, 0, 1],
-                                        [0, 0, 1],
-                                        [0, 0, 1],
-                                        [0, 0, 1]])
-        surface_points = torch.tensor([[-1, -1, 0],
-                                       [1, 1, 0],
-                                       [-1, 1, 0],
-                                       [1, -1, 0],
-                                       [0, 0, 0]])
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.sun = Sun(
+            "Normal", 10000, [0, 0], [[4.3681e-06, 0], [0, 4.3681e-06]], device
+        )
+        surface_normals = torch.tensor(
+            [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]]
+        )
+        surface_points = torch.tensor(
+            [[-1, -1, 0], [1, 1, 0], [-1, 1, 0], [1, -1, 0], [0, 0, 0]]
+        )
         self.alignmentModel = AlignmentModule(position=self.heliostat_position)
-        self.datapoint = HeliostatDataPoint(point_id=1,
-                                            light_directions=self.light_direction,
-                                            desired_aimpoint=self.receiver_center,
-                                            label=HeliostatDataPointLabel())
-        self.aligned_surface_points, self.aligned_surface_normals = self.alignmentModel.align_surface(Datapoint=self.datapoint, surface_points=surface_points, surface_normals=surface_normals)
+        self.datapoint = HeliostatDataPoint(
+            point_id=1,
+            light_directions=self.light_direction,
+            desired_aimpoint=self.receiver_center,
+            label=HeliostatDataPointLabel(),
+        )
+        (
+            self.aligned_surface_points,
+            self.aligned_surface_normals,
+        ) = self.alignmentModel.align_surface(
+            datapoint=self.datapoint,
+            surface_points=surface_points,
+            surface_normals=surface_normals,
+        )
 
     def test_compute_rays(self):
         expected = False
@@ -46,33 +55,28 @@ class TestASunModule(unittest.TestCase):
         receiver_resolution_x = 64
         receiver_resolution_y = 64
         sun_position = self.light_direction
-        
-        ray_directions = self.sun.reflect_rays_(-sun_position, self.aligned_surface_normals)
+
+        ray_directions = self.sun.reflect_rays_(
+            -sun_position, self.aligned_surface_normals
+        )
         xi, yi = self.sun.sample(1)
 
-        rays = self.sun.compute_rays(receiver_plane_normal,
-                                    receiver_center,
-                                    ray_directions,
-                                    self.aligned_surface_points,
-                                    xi, 
-                                    yi)
-        
-        intersections = self.sun.compute_receiver_intersections(receiver_plane_normal,
-                                                                receiver_center,
-                                                                rays,
-                                                                self.aligned_surface_points)
+        rays = self.sun.compute_rays(
+            receiver_plane_normal,
+            receiver_center,
+            ray_directions,
+            self.aligned_surface_points,
+            xi,
+            yi,
+        )
 
-        dx_ints = (
-            intersections[:, :, 0]
-            + receiver_plane_x / 2
-            - receiver_center[0]
+        intersections = self.sun.compute_receiver_intersections(
+            receiver_plane_normal, receiver_center, rays, self.aligned_surface_points
         )
-   
-        dy_ints = (
-            intersections[:, :, 2]
-            + receiver_plane_y / 2
-            - receiver_center[2]
-        )
+
+        dx_ints = intersections[:, :, 0] + receiver_plane_x / 2 - receiver_center[0]
+
+        dy_ints = intersections[:, :, 2] + receiver_plane_y / 2 - receiver_center[2]
 
         indices = (
             (-1 <= dx_ints)
@@ -80,7 +84,7 @@ class TestASunModule(unittest.TestCase):
             & (-1 <= dy_ints)
             & (dy_ints < receiver_plane_y + 1)
         )
- 
+
         total_bitmap = self.sun.sample_bitmap(
             dx_ints,
             dy_ints,
@@ -90,13 +94,14 @@ class TestASunModule(unittest.TestCase):
             receiver_resolution_x,
             receiver_resolution_y,
         )
-        
+
         plt.imshow(total_bitmap.detach().numpy())
         plt.show()
 
-        expected = torch.load('Bitmaps/testMap.pt')
-        
+        expected = torch.load("Bitmaps/testMap.pt")
+
         torch.testing.assert_close(total_bitmap, expected)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
