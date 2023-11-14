cb26c06f3ba13fffdb69023cbc77e83429516a03
Marie Weiel <marie.weiel@kit.edu>
2023-11-14 14:24:44 +0100

    remove shebang and automatically generated but useless docstring

--- artist/io/datapoint.py
+++ artist/io/datapoint.py
@@ -1,21 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  8 15:32:12 2023
-
-@author: user
-"""
-
 from dataclasses import dataclass
 import datetime
 from typing import Optional
 import torch
+
 
 @dataclass
 class HeliostatDataPointLabel(object):
     bitmap: Optional[torch.Tensor] = None
     orientation: Optional[torch.Tensor] = None
     measured_aimpoint: Optional[torch.Tensor] = None
+
 
 @dataclass
 class HeliostatDataPoint(object):
