4e14284cf9a73a58c51c5291f3a570b49fbc0236
Marie Weiel <marie.weiel@kit.edu>
2023-11-14 16:14:20 +0100

    add dummy docstring templates + type hints where possible and make variable + function names lowercase

--- artist/physics_objects/heliostats/alignment/actuator.py
+++ artist/physics_objects/heliostats/alignment/actuator.py
@@ -4,25 +4,22 @@ from artist.physics_objects.heliostats.normalization import ParameterNormalizer
 from artist.physics_objects.parameter import AParameter
 
 
-
 class ActuatorModule(AModule):
-    ## This class implements the behavior of a linear actuator that moves within a 2D plane as a pytorch module.
-    #
-    # The actuator is parametrized by 5 parameters:
-    # - increment: stroke length change per motor step
-    # - initial_stroke_length: stroke length for a motor step of 0
-    # - actuator_offset: offset between the linear actuator's pivoting point and the point around which the actuator is allowed to pivot
-    # - joint_radius: the actuator's pivoting radius
-    # - phi_0 : the angle the actuator introduces to the manipulated coordinate system at the initial stroke length
-    #
-    # The actuator is modelled by the law of cosine. Two directions are implemented:
-    # - motor steps -> angle
-    # - angle -> motor steps
+    """
+    This class implements the behavior of a linear actuator that moves within a 2D plane as a pytorch module.
 
-    #######################
-    #   Parametrization   #
-    #######################
-    
+    The actuator is parametrized by 5 parameters:
+    - increment: stroke length change per motor step
+    - initial_stroke_length: stroke length for a motor step of 0
+    - actuator_offset: offset between the linear actuator's pivoting point and the point around which the actuator is allowed to pivot
+    - joint_radius: the actuator's pivoting radius
+    - phi_0 : the angle the actuator introduces to the manipulated coordinate system at the initial stroke length
+
+    The actuator is modelled by the law of cosine. Two directions are implemented:
+    - motor steps -> angle
+    - angle -> motor steps
+    """
+
     class DevRotationParameter(AParameter):
         def __init__(
             self,
@@ -87,7 +84,7 @@ class ActuatorModule(AModule):
 
     def _phi_0(self) -> torch.Tensor:
         return self._rotation_with_deviation("phi_0")
-    
+
     def _register_parameter(self, parameter: AParameter):
         if not hasattr(self, "parameter_normalizer"):
             self.parameter_normalizer = ParameterNormalizer()
@@ -108,11 +105,6 @@ class ActuatorModule(AModule):
             name, self.get_parameter(name)
         )
 
-
-    #######################
-    #     Constructor     #
-    #######################
-
     def __init__(self, joint_number: int, clockwise: bool, params: dict, **deviations):
         super().__init__()
         self.JOINT_NUMBER = joint_number
@@ -128,13 +120,10 @@ class ActuatorModule(AModule):
             # register and normalize deviations
             self._register_parameter(param)
 
-    #######################
-    #       Methods       #
-    #######################
-
     # TODO remove self.JOINT_NUMBER
-    def _stepsToPhi(self, actuator_pos: torch.Tensor):
-        # access actuator_pos via joint number: items in actuator_pos list have to be ordered by number
+
+    def _steps_to_phi(self, actuator_pos: torch.Tensor):
+        # Access actuator_pos via joint number: items in actuator_pos list have to be ordered by number
         stroke_length = (
             actuator_pos[:, self.JOINT_NUMBER - 1] / self._increment()
             + self._initial_stroke_length()
@@ -148,29 +137,34 @@ class ActuatorModule(AModule):
         calc_step_3 = calc_step_1 / calc_step_2
         angle = torch.arccos(calc_step_3)
         return angle
-    
-    def _stepsToAngles(self, actuator_pos: torch.Tensor):
 
-        phi = self._stepsToPhi(actuator_pos=actuator_pos)
-        phi_0 = self._stepsToPhi(actuator_pos=torch.zeros(actuator_pos.shape))
+    def _steps_to_angles(self, actuator_pos: torch.Tensor):
+        phi = self._steps_to_phi(actuator_pos=actuator_pos)
+        phi_0 = self._steps_to_phi(actuator_pos=torch.zeros(actuator_pos.shape))
         delta_phi = phi_0 - phi
 
-        angles = self._phi_0() + delta_phi if self.CLOCKWISE else self._phi_0() - delta_phi
+        angles = (
+            self._phi_0() + delta_phi if self.CLOCKWISE else self._phi_0() - delta_phi
+        )
         return angles
-    
-    def _anglesToSteps(self, angles : torch.Tensor):
+
+    def _angles_to_steps(self, angles: torch.Tensor):
         delta_phi = angles - self._phi_0() if self.CLOCKWISE else self._phi_0() - angles
-        
-        phi_0 = self._stepsToPhi(actuator_pos=torch.zeros(angles.shape[0], 2))
+
+        phi_0 = self._steps_to_phi(actuator_pos=torch.zeros(angles.shape[0], 2))
         phi = phi_0 - delta_phi
-        
+
         calc_step_3 = torch.cos(phi)
         calc_step_2 = 2.0 * self._actuator_offset() * self._joint_radius()
         calc_step_1 = calc_step_3 * calc_step_2
-        stroke_length = torch.sqrt( self._actuator_offset() ** 2  + self._joint_radius() ** 2 - calc_step_1)
-        actuator_steps = (stroke_length - self._initial_stroke_length()) * self._increment()
+        stroke_length = torch.sqrt(
+            self._actuator_offset() ** 2 + self._joint_radius() ** 2 - calc_step_1
+        )
+        actuator_steps = (
+            stroke_length - self._initial_stroke_length()
+        ) * self._increment()
         return actuator_steps
 
     def forward(self, actuator_pos: torch.Tensor):
-        # access actuator_pos via joint number: items in actuator_pos list have to be ordered by number
-        return self._stepsToAngles(actuator_pos=actuator_pos)
+        # Access actuator_pos via joint number: items in actuator_pos list have to be ordered by number
+        return self._steps_to_angles(actuator_pos=actuator_pos)
