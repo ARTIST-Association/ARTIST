f219ea1b9e75fdfdc9a14dc182ae18fe8817ecb7
Busch <marlene.busch@dlr.de>
2023-11-16 14:43:12 +0100

    add docstrings and type hinting

--- artist/scenario/light_source/light_source.py
+++ artist/scenario/light_source/light_source.py
@@ -3,12 +3,29 @@ import torch
 
 
 class ALightSource(torch.nn.Module):
+    """
+    Abstract base class for all light sources.
+    
+    See Also
+    --------
+    :class: torch.nn.Module : Reference to the parent class
+    """
     def __init__(self):
         super().__init__()
 
-    def sample(self, num_rays_on_hel: int,
-               ) -> Tuple[torch.Tensor, torch.Tensor]:
+    def sample(self,
+                num_rays_on_hel: int,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Sample rays from a given distribution.
+
+        Raises
+        ------
+        NotImplementedError
+            Whenever called (abstract base class method).
+        """
         raise NotImplementedError("Must Be Overridden!")
+
 
     def compute_rays(self,
                      planeNormal: torch.Tensor,
@@ -17,5 +34,13 @@ class ALightSource(torch.nn.Module):
                      hel_in_field: torch.Tensor,
                      xi: torch.Tensor,
                      yi: torch.Tensor,
-                     ) -> torch.Tensor:
+    ) -> torch.Tensor:
+        """
+        Compute the scattered rays for points on a surface.
+
+        Raises
+        ------
+        NotImplementedError
+            Whenever called (abstract base class method).
+        """
         raise NotImplementedError("Must Be Overridden!")
