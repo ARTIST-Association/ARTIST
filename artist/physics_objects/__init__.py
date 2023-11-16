72b7365cd9d00abfd0989fc0519e54217a4c6c8f
Kaleb Phipps <kaleb.phipps@kit.edu>
2023-11-16 08:33:04 +0100

    Include imports in init for physical_objects package

---
+++ artist/physics_objects/__init__.py
@@ -0,0 +1,16 @@
+"""
+This package bundles all classes that are used as physics_objects in ARTIST.
+"""
+
+from .module import (
+    AModule,
+)
+
+from .parameter import (
+    AParameter,
+)
+
+__all__ = [
+    "AModule",
+    "AParameter",
+]
