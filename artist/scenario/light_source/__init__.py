2ed7dac67fe418d0cfc12649ee52cdf229d1fa2e
Kaleb Phipps <kaleb.phipps@kit.edu>
2023-11-16 08:33:15 +0100

    Include imports in init for light_source package

---
+++ artist/scenario/light_source/__init__.py
@@ -0,0 +1,16 @@
+"""
+This package bundles all classes that are used as light_sources in ARTIST.
+"""
+
+from .light_source import (
+    ALightSource,
+)
+
+from .sun import (
+    Sun,
+)
+
+__all__ = [
+    "ALightSource",
+    "Sun",
+]
