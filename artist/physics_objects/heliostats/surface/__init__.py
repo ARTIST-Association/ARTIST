09949320a380883b7beec2a121f030a8b84f9f08
Kaleb Phipps <kaleb.phipps@kit.edu>
2023-11-16 08:32:39 +0100

    Include imports in init for surface package

---
+++ artist/physics_objects/heliostats/surface/__init__.py
@@ -0,0 +1,11 @@
+"""
+This package bundles all classes that are used for surfaces in ARTIST.
+"""
+
+from .concentrator import (
+    ConcentratorModule,
+)
+
+__all__ = [
+    "ConcentratorModule",
+]
