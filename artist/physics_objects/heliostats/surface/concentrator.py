bbaf337ed44457a3b39c5e2babac7a7244ac2e7a
Busch <marlene.busch@dlr.de>
2023-11-17 10:08:28 +0100

    add nurbs surface to concentrator

--- artist/physics_objects/heliostats/surface/concentrator.py
+++ artist/physics_objects/heliostats/surface/concentrator.py
@@ -1,7 +1,15 @@
+from typing import Tuple
+
+import torch
 from ...module import AModule
 
 
 class ConcentratorModule(AModule):
     def __init__(self):
         super().__init__()
+        self.nurbs_surface = NURBSSurface()
         pass
+
+    def get_surface() -> Tuple[torch.Tensor, torch.Tensor]:
+        surface_points, surface_normals = nurbs_surface._calc_normals_and_surface()
+        return surface_points, surface_normals
