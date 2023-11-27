4cd76f7becb757e0f0eb2373d8f98b645a1e2c92
Busch <marlene.busch@dlr.de>
2023-11-27 16:40:20 +0100

    concentrator not working

--- artist/physics_objects/heliostats/surface/concentrator.py
+++ artist/physics_objects/heliostats/surface/concentrator.py
@@ -1,16 +1,21 @@
 from typing import Tuple
 
 import torch
-
+from yacs.config import CfgNode
 from artist.physics_objects.module import AModule
-from artist.physics_objects.heliostats.surface.nurbs.nurbs_surface import NURBSSurface
 
 class ConcentratorModule(AModule):
-    def __init__(self):
+    def __init__(self,
+                 surface_config: CfgNode,
+                 nurbs_config: CfgNode,
+                 device: torch.device,
+                 position_on_field: torch.Tensor,
+                 receiver_center: torch.Tensor,
+                 sun_directions: torch.Tensor):
         super().__init__()
-        self.nurbs_surface = NURBSSurface()
-        pass
+        
+
 
     def get_surface(self) -> Tuple[torch.Tensor, torch.Tensor]:
-        surface_points, surface_normals = self.nurbs_surface.calc_normals_and_surface()
+        surface_points, surface_normals = self.multi_nurbs_surface._discrete_points()
         return surface_points, surface_normals
