c6a1d22b6a637312da68fc1d95143662b93155ba
Busch <marlene.busch@dlr.de>
2023-11-29 10:27:53 +0100

    add abstract facet module

--- artist/physics_objects/heliostats/surface/facets/facets.py
+++ artist/physics_objects/heliostats/surface/facets/facets.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 import torch
 
 from artist.physics_objects.heliostats.surface.nurbs.canting import CantingAlgorithm
@@ -21,40 +21,11 @@ class AFacetModule(torch.nn.Module):
     _canting_algo: Optional[CantingAlgorithm]
     cant_rots: torch.Tensor
 
-    @property
-    def raw_discrete_points(self) -> List[torch.Tensor]:
-        return self._discrete_points
-
-    @raw_discrete_points.setter
-    def raw_discrete_points(
-            self,
-            new_discrete_points: List[torch.Tensor],
-    ) -> None:
-        self._discrete_points = new_discrete_points
-
-    @property
-    def raw_discrete_points_ideal(self) -> List[torch.Tensor]:
-        return self._discrete_points_ideal
-
-    @raw_discrete_points_ideal.setter
-    def raw_discrete_points_ideal(
-            self,
-            new_discrete_points_ideal: List[torch.Tensor],
-    ) -> None:
-        self._discrete_points_ideal = new_discrete_points_ideal
-
-    @property
-    def raw_normals(self) -> List[torch.Tensor]:
-        return self._normals
-
-    @raw_normals.setter
-    def raw_normals(self, new_normals: List[torch.Tensor]) -> None:
-        self._normals = new_normals
-
-    @property
-    def raw_normals_ideal(self) -> List[torch.Tensor]:
-        return self._normals_ideal
-
-    @raw_normals_ideal.setter
-    def raw_normals_ideal(self, new_normals_ideal: List[torch.Tensor]) -> None:
-        self._normals_ideal = new_normals_ideal
+    def discrete_points_and_normals(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        return NotImplementedError('Please overwrite')
+    
+    def facetted_discrete_points_and_normals(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        return NotImplementedError('Please overwrite')
+    
+    def get_facet_surface(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        raise NotImplementedError('Please overwrite')
