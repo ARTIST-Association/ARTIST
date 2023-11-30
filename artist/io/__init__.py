bd94c67dc85b8538b60c61714bbb039805708951
Marie Weiel <marie.weiel@kit.edu>
2023-11-30 09:44:56 +0100

    remove not functional data generator and loader for now

--- artist/io/__init__.py
+++ artist/io/__init__.py
@@ -2,13 +2,9 @@
 This package bundles all classes that are used for IO in ARTIST.
 """
 
-from artist.io.data_generator import DataGenerator
 from artist.io.datapoint import HeliostatDataPointLabel, HeliostatDataPoint
-from artist.io.dataset_loader import DataLoader
 
 __all__ = [
-    "DataGenerator",
     "HeliostatDataPointLabel",
     "HeliostatDataPoint",
-    "DataLoader",
 ]
--- artist/io/data_generator.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-
-class DataGenerator:
-    def generate(self, filename, target_heliostat, light_source):
-        data = {"targetHeliostat": target_heliostat, "lightSource": light_source}
-        with open(filename, "wb") as file:
-            pickle.dump(data, file)
