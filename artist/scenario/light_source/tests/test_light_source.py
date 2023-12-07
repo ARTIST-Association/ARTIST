d69fcc47d9755d2b582e2a5d04c2000fdeece689
Busch <marlene.busch@dlr.de>
2023-12-07 14:58:57 +0100

    change unit test to pytest

--- artist/scenario/light_source/tests/test_light_source.py
+++ artist/scenario/light_source/tests/test_light_source.py
@@ -1,173 +1,136 @@
-import math
+
+import os
 from matplotlib import pyplot as plt
-import unittest
-import numpy
-import pandas as pd
-import torch
-from artist import ARTIST_ROOT
 import numpy as np
+import torch
+import pytest
+from artist import ARTIST_ROOT
 
 from artist.scenario.light_source.sun import Sun
 from artist.io.datapoint import HeliostatDataPoint, HeliostatDataPointLabel
 from artist.physics_objects.heliostats.alignment.alignment import AlignmentModule
 
 
-class TestASunModule(unittest.TestCase):
-    def setUp(self):
-        torch.manual_seed(7)
-        self.light_direction = torch.tensor([0.0, 1.0, 1.0])
-        self.heliostat_position = torch.tensor([0.0, 5.0, 0.0])
-        self.receiver_center = torch.tensor([0.0, -10.0, 0.0])
+def generate_sun_module_data(light_direction, expected_value):
+    heliostat_position = torch.tensor([0.0, 5.0, 0.0])
+    receiver_center = torch.tensor([0.0, -10.0, 0.0])
 
-        cov = 1e-12       #4.3681e-06
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.sun = Sun(
-            "Normal", 300, [0, 0], [[cov, 0], [0, cov]], device
-            #"Normal", 1, [0, 0], [[1, 0], [0, 1]], device
-        )
+    cov = 1e-12  # 4.3681e-06
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    sun = Sun(
+        "Normal", 300, [0, 0], [[cov, 0], [0, cov]], device
+    )
 
-        self.surface_normals = torch.tensor(
-            [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]]
-        )
-        self.surface_points = torch.tensor(
-            [[-1, -1, 0], [1, 1, 0], [-1, 1, 0], [1, -1, 0], [0, 0, 0]]
-        )
-        self.alignment_model = AlignmentModule(position=self.heliostat_position)
-        self.datapoint = HeliostatDataPoint(
-            point_id=1,
-            light_directions=self.light_direction,
-            desired_aimpoint=self.receiver_center,
-            label=HeliostatDataPointLabel(),
-        )
-        (
-            self.aligned_surface_points,
-            self.aligned_surface_normals,
-        ) = self.alignment_model.align_surface(
-            datapoint=self.datapoint,
-            surface_points=self.surface_points,
-            surface_normals=self.surface_normals,
-        )
+    surface_normals = torch.tensor(
+        [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]]
+    )
+    surface_points = torch.tensor(
+        [[-1, -1, 0], [1, 1, 0], [-1, 1, 0], [1, -1, 0], [0, 0, 0]]
+    )
+    alignment_model = AlignmentModule(position=heliostat_position)
 
-    def test_compute_rays(self):
-        receiver_plane_normal = torch.tensor([0.0, 1.0, 0.0])
-        receiver_center = self.receiver_center
-        receiver_plane_x = 10
-        receiver_plane_y = 10
-        receiver_resolution_x = 256
-        receiver_resolution_y = 256
-        sun_position = self.light_direction
+    datapoint = HeliostatDataPoint(
+        point_id=1,
+        light_directions=torch.tensor(light_direction),
+        desired_aimpoint=receiver_center,
+        label=HeliostatDataPointLabel(),
+    )
 
-        ray_directions = self.sun.reflect_rays_(
-            -sun_position, self.aligned_surface_normals
-        )
-        
-        xi, yi = self.sun.sample(len(ray_directions))
+    aligned_surface_points, aligned_surface_normals = alignment_model.align_surface(
+        datapoint=datapoint,
+        surface_points=surface_points,
+        surface_normals=surface_normals,
+    )
 
-        rays = self.sun.compute_rays(
-            receiver_plane_normal,
-            receiver_center,
-            ray_directions,
-            self.aligned_surface_points,
-            xi,
-            yi,
-        )
+    return {
+        'sun': sun,
+        'aligned_surface_points': aligned_surface_points,
+        'aligned_surface_normals': aligned_surface_normals,
+        'receiver_center': receiver_center,
+        'light_direction': torch.tensor(light_direction),
+        'expected_value': expected_value
+    }
 
-        intersections = self.sun.line_plane_intersections(
-            receiver_plane_normal, receiver_center, rays, self.aligned_surface_points
-        )
-
-        dx_ints = intersections[:, :, 0] + receiver_plane_x / 2 - receiver_center[0]
-
-        dy_ints = intersections[:, :, 2] + receiver_plane_y / 2 - receiver_center[2]
-
-        indices = (
-                (-1 <= dx_ints)
-                & (dx_ints < receiver_plane_x + 1)
-                & (-1 <= dy_ints)
-                & (dy_ints < receiver_plane_y + 1)
-        )
-
-        total_bitmap = self.sun.sample_bitmap(
-            dx_ints,
-            dy_ints,
-            indices,
-            receiver_plane_x,
-            receiver_plane_y,
-            receiver_resolution_x,
-            receiver_resolution_y,
-        )
-
-        total_bitmap = self.sun.normalize_bitmap(
-            total_bitmap,
-            xi.numel(),
-            receiver_plane_x,
-            receiver_plane_y,
-        )
-
-        total_bitmap = total_bitmap.T
-        
-        # torch.set_printoptions(threshold=10_000)
-        # print(total_bitmap)
-
-        # stral_df = pd.read_csv("artist\physics_objects\heliostats\\tests\\fdm_max_marlene.txt", delimiter="\t", header=1)
-        # stral_df.to_csv("artist\physics_objects\heliostats\\tests\\fdm_max_marlene.csv", encoding='utf-8', index=False)
-
-        # stral_df = pd.read_csv("artist\physics_objects\heliostats\\tests\\fdm_max_marlene.csv")
-        # # # plt.imshow(stral_df, cmap="jet")
-        # # # plt.show()
-        
-        # stral_df_rotated = numpy.rot90(stral_df, 0)
-        # # plt.imshow(stral_df_rotated, cmap="jet")
-        # # plt.show()
-
-        # stral_tensor = torch.Tensor(stral_df_rotated.copy())#[:, 1:]
-        # stral_tensor_rotated_normalized = self.sun.normalize_bitmap(
-        #     stral_tensor,
-        #     xi.numel(),
-        #     receiver_plane_x,
-        #     receiver_plane_y,
-        # )
-
-        # tick_positions = np.linspace(0, 150, 11)  # Define 11 tick positions between 0 and 256
-        # tick_labels = np.linspace(-1.5, 1.5, 11)     # Define corresponding labels from 0 to 10
-
-        # # Set the ticks and labels for both x and y axes
-        # plt.xticks(tick_positions, tick_labels)
-        # plt.yticks(tick_positions, tick_labels) 
-        # plt.imshow((stral_tensor_rotated_normalized), cmap="jet")
-        # plt.grid(True)
-        # plt.show()
+@pytest.fixture(params=[
+    ([0.0, 0.0, 1.0], "above.pt"),
+    ([1.0, 0.0, 0.0], "east.pt"),
+    ([-1.0, 0.0, 0.0], "west.pt"),
+    ([0.0, -1.0, 0.0], "south.pt"),
+], name="sun_module_data")
+def sun_module_data(request):
+    return generate_sun_module_data(*request.param)
 
 
-        fig, ax = plt.subplots(figsize=(6,6))
-        tick_positions = np.linspace(0, 256, 11)  # Define 11 tick positions between 0 and 256
-        tick_labels = np.linspace(5, 5, 11)     # Define corresponding labels from 0 to 10
+def test_compute_rays(sun_module_data):
+    torch.manual_seed(7)
+    sun = sun_module_data['sun']
+    aligned_surface_points = sun_module_data['aligned_surface_points']
+    aligned_surface_normals = sun_module_data['aligned_surface_normals']
+    receiver_center = sun_module_data['receiver_center']
+    light_direction = sun_module_data['light_direction']
+    expected_value = sun_module_data['expected_value']
 
-        # Set the ticks and labels for both x and y axes
-        plt.xticks(tick_positions, tick_labels)
-        plt.yticks(tick_positions, tick_labels) 
-        plt.imshow(total_bitmap.detach().numpy(), aspect="equal", cmap="jet")
-        plt.grid(True)
-        plt.show()
+    receiver_plane_normal = torch.tensor([0.0, 1.0, 0.0])
+    receiver_plane_x = 10
+    receiver_plane_y = 10
+    receiver_resolution_x = 256
+    receiver_resolution_y = 256
+    sun_position = light_direction
+    
+    ray_directions = sun.reflect_rays_(
+        -sun_position, aligned_surface_normals
+    )
 
-        torch.save(total_bitmap, 'artist\scenario\light_source\\tests\\bitmaps\\north_above.pt')
+    xi, yi = sun.sample(len(ray_directions))
 
-        # loss = torch.nn.L1Loss()
-        # l = loss(total_bitmap, stral_tensor_rotated_normalized)
-        # print(l)
-        # plt.xticks(tick_positions, tick_labels)
-        # plt.yticks(tick_positions, tick_labels) 
-        # plt.imshow(total_bitmap - stral_tensor_rotated_normalized)
-        # plt.grid(True)
-        # plt.show()
+    rays = sun.compute_rays(
+        receiver_plane_normal,
+        receiver_center,
+        ray_directions,
+        aligned_surface_points,
+        xi,
+        yi,
+    )
+
+    intersections = sun.line_plane_intersections(
+        receiver_plane_normal, receiver_center, rays, aligned_surface_points
+    )
+
+    dx_ints = intersections[:, :, 0] + receiver_plane_x / 2 - receiver_center[0]
+    dy_ints = intersections[:, :, 2] + receiver_plane_y / 2 - receiver_center[2]
+
+    indices = (
+        (-1 <= dx_ints)
+        & (dx_ints < receiver_plane_x + 1)
+        & (-1 <= dy_ints)
+        & (dy_ints < receiver_plane_y + 1)
+    )
+
+    total_bitmap = sun.sample_bitmap(
+        dx_ints,
+        dy_ints,
+        indices,
+        receiver_plane_x,
+        receiver_plane_y,
+        receiver_resolution_x,
+        receiver_resolution_y,
+    )
+
+    total_bitmap = sun.normalize_bitmap(
+        total_bitmap,
+        xi.numel(),
+        receiver_plane_x,
+        receiver_plane_y,
+    )
+
+    total_bitmap = total_bitmap.T
+    
+    expected_path = os.path.join(
+        ARTIST_ROOT, "artist", "scenario", "light_source", "tests", "bitmaps", expected_value
+    )
+
+    expected = torch.load(expected_path)
+
+    torch.testing.assert_close(total_bitmap, expected)
 
 
-        # expected = torch.load(
-        #     f"{ARTIST_ROOT}/artist/scenario/light_source/tests/bitmaps/testMap.pt"
-        # )
-
-        #torch.testing.assert_close(total_bitmap, expected)
-
-
-if __name__ == "__main__":
-    unittest.main()
