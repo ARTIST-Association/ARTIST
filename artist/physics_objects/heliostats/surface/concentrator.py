413e9c94d6117af54e525110da07ba6aabdb11c5
Busch <marlene.busch@dlr.de>
2023-12-19 11:13:52 +0100

    add docstrings, type hints and black formatting

--- artist/physics_objects/heliostats/surface/concentrator.py
+++ artist/physics_objects/heliostats/surface/concentrator.py
@@ -7,17 +7,50 @@ from artist.physics_objects.heliostats.surface.facets.facets import AFacetModule
 
 from artist.physics_objects.module import AModule
 
+
 class ConcentratorModule(AModule):
-    def __init__(self,
-                 facets: List[AFacetModule]
-    ):
+    """
+    Implementation of the concentrator module.
+
+    Attributes
+    ----------
+    facets : List[AFacetModule]
+        The facets of the concentrator.
+
+    Methods
+    -------
+    get_surface()
+        Compute the surface points and surface normals of the concentrator.
+
+    See also
+    --------
+    :class:AModule : The parent class.
+    """
+
+    def __init__(self, facets: List[AFacetModule]) -> None:
+        """
+        Initialize the concentrator.
+
+        Parameters
+        ----------
+        facets: List[AFacetModule]
+            The facets of the concentrator.
+        """
         super().__init__()
         self.facets = facets
 
     def get_surface(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Compute the surface points and surface normals of the concentrator.
+
+        Returns
+        -------
+        Tuple[torch.Tensor, torch.Tensor]
+            Return the surface points and the surface normals.
+        """
         surface_points_list = []
         surface_normals_list = []
-        
+
         for facet in self.facets:
             surface_points_list.append(facet[0])
             surface_normals_list.append(facet[1])
