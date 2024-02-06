83eb6626ccc5babd7c570623088639e445f182da
Busch <marlene.busch@dlr.de>
2024-02-06 16:00:30 +0100

    revert pytest to correct version

--- artist/physics_objects/heliostats/alignment/neural_network_rigid_body_fusion.py
+++ artist/physics_objects/heliostats/alignment/neural_network_rigid_body_fusion.py
@@ -556,27 +556,26 @@ class NeuralNetworkRigidBodyFusion(AKinematicModule):
             orientation = self.compute_orientation_from_steps(
                 actuator_1_steps=actuator_steps, actuator_2_steps=actuator_steps
             )
-            concentrator_normals = (
-                orientation @ torch.tensor([0, -1, 0, 0], dtype=torch.float32)
-            )[:1, :3]
-            concentrator_origins = (
-                orientation @ torch.tensor([0, 0, 0, 1], dtype=torch.float32)
-            )[:1, :3]
 
-            orientation[0][:, 1] = -orientation[0][:, 1]
+            orientation[0][:, 1] = orientation[0][:, 2]
             orientation[0][:3, 2] = torch.cross(
                 orientation[0][:3, 0], orientation[0][:3, 1]
             )
-
+            
+            concentrator_normals = (
+                                           orientation @ torch.tensor([0, 0, 1, 0], dtype=torch.float32)
+                                   )[:1, :3]
+            concentrator_origins = (
+                                           orientation @ torch.tensor([0, 0, 0, 1], dtype=torch.float32)
+                                   )[:1, :3]
+            
             # Compute desired normal.
             desired_reflect_vec = data_point.desired_aimpoint - concentrator_origins
             desired_reflect_vec /= desired_reflect_vec.norm()
             data_point.light_directions /= data_point.light_directions.norm()
             desired_concentrator_normal = (
-                data_point.light_directions + desired_reflect_vec
+                    data_point.light_directions + desired_reflect_vec
             )
-            desired_concentrator_normal /= desired_concentrator_normal.norm()
-            desired_concentrator_normal -= concentrator_origins
             desired_concentrator_normal /= desired_concentrator_normal.norm()
             #desired_concentrator_normal -= concentrator_origins
             #desired_concentrator_normal /= desired_concentrator_normal.norm()
