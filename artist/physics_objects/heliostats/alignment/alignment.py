eb6c623bda91e2d709be85e9ba0d112db9134607
Busch <marlene.busch@dlr.de>
2024-02-07 09:30:40 +0100

    removed commented out code sections

--- artist/physics_objects/heliostats/alignment/alignment.py
+++ artist/physics_objects/heliostats/alignment/alignment.py
@@ -11,7 +11,10 @@ from artist.physics_objects.heliostats.alignment.neural_network_rigid_body_fusio
     NeuralNetworkRigidBodyFusion,
 )
 from artist.physics_objects.module import AModule
-from artist.physics_objects.heliostats.alignment.neural_network_rigid_body_fusion import NeuralNetworkRigidBodyFusion
+from artist.physics_objects.heliostats.alignment.neural_network_rigid_body_fusion import (
+    NeuralNetworkRigidBodyFusion,
+)
+
 
 class AlignmentModule(AModule):
     """
@@ -75,15 +78,15 @@ class AlignmentModule(AModule):
             Tuple containing the aligned surface points and normals.
         """
         orientation = self.align(datapoint=datapoint)
-        normal_vec = (orientation @ torch.tensor([0.0, 0.0, 1.0, 0.0], dtype=torch.float32))[:1, :3]
+        normal_vec = (
+            orientation @ torch.tensor([0.0, 0.0, 1.0, 0.0], dtype=torch.float32)
+        )[:1, :3]
         alignment = torch.stack(
             self.heliostat_coord_system(
                 self.position,
                 datapoint.light_directions,
                 datapoint.desired_aimpoint,
                 normal_vec,
-                # torch.Tensor([0.0, 0.0, 0.0]),
-                # torch.Tensor([0.0, 0.0, 0.0]),
             )
         )
         align_origin = throt.Rotate(alignment, dtype=alignment.dtype)
@@ -117,22 +120,20 @@ class AlignmentModule(AModule):
             The orientation matrix.
         """
         return self.kinematic_model.compute_orientation_from_aimpoint(datapoint)
-    
+
     def heliostat_coord_system(
-            self,
-            Position: torch.Tensor,
-            Sun: torch.Tensor,
-            Aimpoint: torch.Tensor,
-            ideal_normal: torch.Tensor,
-            # disturbance_angles: List[torch.Tensor],
-            # rotation_offset: torch.Tensor
+        self,
+        Position: torch.Tensor,
+        Sun: torch.Tensor,
+        Aimpoint: torch.Tensor,
+        ideal_normal: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """
         Construct the heliostat coordination system.
 
         Parameters
         ----------
-        position : torch.tensor
+        position : torch.Tensor
             The position of the heliostat.
         sun : torch.Tensor
             The sun vector / direction.
@@ -148,13 +149,9 @@ class AlignmentModule(AModule):
         """
         dtype = Position.dtype
         device = Position.device
-        #rotation_offset = torch.deg2rad(rotation_offset)
         pSun = Sun
         pPosition = Position
         pAimpoint = Aimpoint
-        # print(pSun,pPosition,pAimpoint)
-        # Berechnung Idealer Heliostat
-        # 0. Iteration
         z = pAimpoint - pPosition
         z = z / torch.linalg.norm(z)
         z = pSun + z
@@ -173,15 +170,4 @@ class AlignmentModule(AModule):
         x /= torch.linalg.norm(x)
         y = torch.cross(z, x)
 
-        # # Add heliostat rotation error/disturbance.
-        # x_err_rot = utils.axis_angle_rotation(x, disturbance_angles[0])
-        # y_err_rot = utils.axis_angle_rotation(y, disturbance_angles[1])
-        # z_err_rot = utils.axis_angle_rotation(z, disturbance_angles[2]+rotation_offset)
-        # # print(Position)
-        # # print(disturbance_angles)
-        # full_rot = z_err_rot @ y_err_rot @ x_err_rot
-
-        # x = full_rot @ x
-        # y = full_rot @ y
-        # z = full_rot @ z
         return x, y, z
