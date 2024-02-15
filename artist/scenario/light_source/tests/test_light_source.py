a9857ec08c3fa1996227cb7f815065f4a3a89d8b
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-02-15 11:29:04 +0100

    fix typing for python 3.8

--- artist/scenario/light_source/tests/test_light_source.py
+++ artist/scenario/light_source/tests/test_light_source.py
@@ -2,6 +2,7 @@
 This pytest tests the correctness of the light source.
 """
 
+from typing import Dict
 import pathlib
 import pytest
 import torch
@@ -14,7 +15,7 @@ from artist.scenario.light_source.sun import Sun
 
 def generate_data(
     light_direction: torch.Tensor, expected_value: torch.Tensor
-) -> dict[str, torch.Tensor]:
+) -> Dict[str, torch.Tensor]:
     """
     Generate all the relevant data for this test.
 
@@ -32,7 +33,7 @@ def generate_data(
 
     Returns
     -------
-    dict[str, torch.Tensor]
+    Dict[str, torch.Tensor]
         A dictionary containing all the data.
     """
     heliostat_position = torch.tensor([0.0, 5.0, 0.0])
@@ -98,7 +99,7 @@ def data(request):
     return generate_data(*request.param)
 
 
-def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
+def test_compute_bitmaps(environment_data: Dict[str, torch.Tensor]) -> None:
     """
     Compute resulting flux density distribution (bitmap) for the given test case.
 
@@ -109,7 +110,7 @@ def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
 
     Parameters
     ----------
-    environment_data : dict[str, torch.Tensor]
+    environment_data : Dict[str, torch.Tensor]
         The dictionary containing all the data to compute the bitmaps.
     """
     torch.set_printoptions(precision=10)
