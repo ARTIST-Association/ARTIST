6caa411cda0be5e1964bfe17e0c8699da2bca9e3
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-02-28 15:54:18 +0100

    add first version of scenario generator

---
+++ artist/util/scenario_generator.py
@@ -0,0 +1,148 @@
+"""A util script for generating scenario h5 files to be used in Artist."""
+
+from collections.abc import MutableMapping
+from typing import Dict
+
+import h5py
+
+from artist import ARTIST_ROOT
+from artist.util.heliostat_configurations import _ideal_heliostat
+
+# The following configurations can be adapted to define the required scenario.
+
+# The following parameter is the name of the scenario.
+name = "test"
+
+# The following parameters refer to the receiver.
+receiver_params = {
+    "center": [0.0, -3.23, 35.89],
+    "plane_normal": [0.0, 1.0, 0.0],
+    "plane": [8.629666667, 7.0],
+    "resolution": [64.0, 64.0],
+}
+
+# The following parameters refer to the sun.
+sun_params = {
+    "distribution_parameters": {
+        "distribution_type": str("normal"),
+        "mean": 0.0,
+        "covariance": 4.3681e-06,
+    },
+    "redraw_random_variables": False,
+}
+
+# The following parameters refer to the heliostat list.
+heliostats = {
+    "AA39": {
+        "position": [0.0, 3.0],
+        "parameters": _ideal_heliostat,
+        "surface_data": False,
+        "alignment_parameters": False,
+    },
+    "AA40": {
+        "position": [3.0, 3.0],
+        "parameters": _ideal_heliostat,
+        "surface_data": False,
+        "alignment_parameters": False,
+    },
+    "AA50": {
+        "position": [5.0, 5.0],
+        "parameters": _ideal_heliostat,
+        "surface_data": False,
+        "alignment_parameters": False,
+    },
+}
+
+
+def flatten_dict(
+    dictionary: MutableMapping, parent_key: str = "", sep: str = "/"
+) -> Dict:
+    """
+    Flattens nested dictionaries to first level keys.
+
+    Parameters
+    ----------
+    dictionary : MutableMapping
+        Original nested dictionary to flatten.
+
+    parent_key: str
+        The parent key of nested dictionaries. Should be empty upon initialisation.
+
+    sep: str
+        The separator used to separate keys in nested dictionaries.
+
+    Returns
+    -------
+    Dict
+        A flattened version of the original dictionary
+    """
+    return dict(_flatten_dict_gen(dictionary, parent_key, sep))
+
+
+def _flatten_dict_gen(d: MutableMapping, parent_key: str, sep: str):
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            yield from flatten_dict(v, new_key, sep=sep).items()
+        else:
+            yield new_key, v
+
+
+def include_parameters(file: h5py.File, prefix: str, parameters: dict) -> None:
+    """
+    Includes the parameters from the sun parameter dict.
+
+    Parameters
+    ----------
+    file: h5py.File
+        The h5 file to write to.
+
+    prefix: str
+        The prefix used for naming the parameters.
+
+    parameters: dict
+        The parameters to be included into the h5 file.
+
+    """
+    for key, value in parameters.items():
+        file[f"{prefix}/{key}"] = value
+
+
+def generate_scenario(scenario_name: str, version: float = 0.1):
+    """
+    Generate the scenario according to the given parameters.
+
+    Parameters
+    ----------
+    scenario_name: str
+        The name of the scenario being generated.
+
+    version : float
+        The current version of the scenario generator being used. This must be updated if the names of
+        the parameters are altered.
+
+    """
+    with h5py.File(f"{ARTIST_ROOT}/scenarios/{scenario_name}.h5", "w") as f:
+        # Set scenario version as attribute
+        f.attrs["version"] = version
+
+        # Include parameters for the receiver
+        include_parameters(
+            file=f, prefix="receiver", parameters=flatten_dict(receiver_params)
+        )
+
+        # Include parameters for the sun
+        include_parameters(file=f, prefix="sun", parameters=flatten_dict(sun_params))
+
+        # Include heliostat parameters
+        include_parameters(
+            file=f, prefix="heliostats", parameters=flatten_dict(heliostats)
+        )
+
+
+if __name__ == "__main__":
+    """
+    The main method that generates the scenario using the parameters defined above.
+
+    """
+    generate_scenario(scenario_name=name)
