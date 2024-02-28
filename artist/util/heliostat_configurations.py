dcda1afdbffbb8b09a0ba198cb28684e3eed76ee
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-02-28 15:54:18 +0100

    add first version of scenario generator

---
+++ artist/util/heliostat_configurations.py
@@ -0,0 +1,25 @@
+"""This file contains bibliographies for different heliostat prototypes."""
+
+# Parameters for an ideal heliostat
+_ideal_heliostat = {
+    "aim_point": [0.0, 0.0, 0.0],
+    "columns": 32,
+    "rows": 32,
+    "height": 4.0,
+    "width": 4.0,
+    "position_on_fields": [0.0, 0.0, 0.0],
+    "normal_vectors": [0.0, 0.0, 1.0],
+    "disturbance_rotation_angles": [0.0, 0.0, 0.0],
+    "facets": {
+        "number": 4,
+        "shape": 0,
+        "positions": [
+            [1.0, -1.0, 0.0],
+            [-1.0, 1.0, 0.0],
+            [1.0, -1.0, 0.0],
+            [-1.0, 1.0, 0.0],
+        ],
+        "spans_east": [-1.0, 0.0, 0.0],
+        "spans_north": [0.0, 1.0, 0.0],
+    },
+}
