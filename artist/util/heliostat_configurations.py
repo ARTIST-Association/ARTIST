183c8f5690fff8350666ed208e3d19cd49b55098
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-02-29 13:09:06 +0100

    update heliostat config

--- artist/util/heliostat_configurations.py
+++ artist/util/heliostat_configurations.py
@@ -2,16 +2,12 @@
 
 # Parameters for an ideal heliostat
 _test_heliostat = {
-    "columns": 32,
-    "rows": 32,
     "height": 4.0,
     "width": 4.0,
-    "position_on_fields": [0.0, 0.0, 0.0],
     "normal_vectors": [0.0, 0.0, 1.0],
     "disturbance_rotation_angles": [0.0, 0.0, 0.0],
     "facets": {
         "number": 4,
-        "shape": 0,
         "positions": [
             [1.0, -1.0, 0.0],
             [-1.0, 1.0, 0.0],
