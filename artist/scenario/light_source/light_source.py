4a9445562aff790e8fa1e5d47495e2778dd215e3
Busch <marlene.busch@dlr.de>
2024-03-04 10:02:19 +0100

    rename sample function

--- artist/scenario/light_source/light_source.py
+++ artist/scenario/light_source/light_source.py
@@ -15,31 +15,12 @@ class ALightSource(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def sample(
+    def sample_distortions(
         self,
         num_rays_on_hel: int,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Sample rays from a given distribution.
-
-        Raises
-        ------
-        NotImplementedError
-            Whenever called (abstract base class method).
-        """
-        raise NotImplementedError("Must Be Overridden!")
-
-    def compute_rays(
-        self,
-        plane_normal: torch.Tensor,
-        plane_point: torch.Tensor,
-        ray_directions: torch.Tensor,
-        hel_in_field: torch.Tensor,
-        xi: torch.Tensor,
-        yi: torch.Tensor,
-    ) -> torch.Tensor:
-        """
-        Compute the scattered rays for points on a surface.
 
         Raises
         ------
