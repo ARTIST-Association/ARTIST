3bb517304013f2054a7c89ebe1c8cd6bbbda673a
Busch <marlene.busch@dlr.de>
2024-03-04 09:21:45 +0100

    change Dict to dict

--- artist/scenario/light_source/tests/test_light_source.py
+++ artist/scenario/light_source/tests/test_light_source.py
@@ -13,7 +13,7 @@ from artist.scenario.light_source.sun import Sun
 
 def generate_data(
     light_direction: torch.Tensor, expected_value: torch.Tensor
-) -> Dict[str, torch.Tensor]:
+) -> dict[str, torch.Tensor]:
     """
     Generate all the relevant data for this test.
 
@@ -93,7 +93,7 @@ def generate_data(
     ],
     name="environment_data",
 )
-def data(request) -> Dict[str, torch.Tensor]:
+def data(request) -> dict[str, torch.Tensor]:
     """
     [INSERT DESCRIPTION HERE!].
 
@@ -110,7 +110,7 @@ def data(request) -> Dict[str, torch.Tensor]:
     return generate_data(*request.param)
 
 
-def test_compute_bitmaps(environment_data: Dict[str, torch.Tensor]) -> None:
+def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
     """
     Compute resulting flux density distribution (bitmap) for the given test case.
 
