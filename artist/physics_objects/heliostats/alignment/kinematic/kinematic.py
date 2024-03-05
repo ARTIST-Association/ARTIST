ffb9d103b3c17b2ead1da68ecd837d87a77e839f
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-05 12:22:08 +0100

    continue refactoring

--- artist/physics_objects/heliostats/alignment/kinematic/kinematic.py
+++ artist/physics_objects/heliostats/alignment/kinematic/kinematic.py
@@ -1,8 +1,9 @@
-"""Kinematic modules in ARTIST."""
+"""
+Kinematic modules in ARTIST.
+"""
+
 import typing
-
 import torch
-
 from artist.physics_objects.module import AModule
 
 
@@ -19,12 +20,8 @@ class AKinematicModule(AModule):
     -------
     compute_orientation()
         Compute the orientation matrix to align the heliostat.
-    compute_reflection()
-        Compute the aim points starting from the orientation of the heliostat.
     forward()
         Implement the forward kinematics.
-    to_dict()
-        Convert to dictionary.
 
     See Also
     --------
@@ -33,15 +30,13 @@ class AKinematicModule(AModule):
 
     def __init__(self, position: torch.Tensor) -> None:
         """
-        Initialize a kinematic model.
-
         Parameters
         ----------
         position : torch.Tensor
             The position.
         """
         super().__init__()
-        self._position = position
+        self.position = position
 
     def compute_orientation(
         self, data_point_tensor: torch.Tensor
@@ -61,28 +56,6 @@ class AKinematicModule(AModule):
         """
         raise NotImplementedError("Must be overridden!")
 
-    def compute_reflection(self, data_points: torch.Tensor) -> torch.Tensor:
-        """
-        Compute the aim points starting from the orientation of the heliostat.
-
-        Parameters
-        ----------
-        data_points : torch.Tensor
-            Contains the information about the heliostat, light source, receiver.
-
-        Returns
-        -------
-        torch.Tensor
-            The derived aim points.
-        """
-        normal_vectors, normal_origins = self.compute_orientation(
-            data_point_tensor=data_points
-        )
-        aim_vectors = 2 * (data_points[:, 2:5] @ normal_vectors) - data_points[:, 2:5]
-        return (
-            self._position + aim_vectors * (data_points[:, 5:] - self._position).norm()
-        )  # Return aim points.
-
     def forward(self, data_points: torch.Tensor) -> torch.Tensor:
         """
         Implement the forward kinematics.
@@ -92,14 +65,9 @@ class AKinematicModule(AModule):
         data_points : torch.Tensor
             Contains the information about the heliostat, light source, receiver.
 
-        Returns
-        -------
-        torch.Tensor
-            The derived aim points.
-
+        Raises
+        ------
+        NotImplementedError
+            Whenever called (abstract base class method).
         """
-        return self.compute_reflection(data_points)
-
-    def to_dict(self):
-        """Convert to dictionary."""
-        return self.state_dict()
+        raise NotImplementedError("Must be overridden!")
