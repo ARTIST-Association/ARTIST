bab1a17d4b6cf813f8ec735198ff2946bb143955
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-06 17:18:09 +0100

    include config dictionary

---
+++ artist/util/config_dictionary.py
@@ -0,0 +1,44 @@
+# Dict for loading data
+load_normals_key = "surface_normals"
+load_points_key = "surface_points"
+measurement_location = "measurement_data"
+
+# Dict for heliostats
+heliostat_prefix = "heliostats"
+general_surface_points = "general_surface_points"
+general_surface_normals = "general_surface_normals"
+alignment_type_key = "alignment_type"
+actuator_type_key = "actuator_type"
+facets_type_key = "facets_type"
+heliostats_list = "heliostats_list"
+heliostat_id = "id"
+heliostat_aim_point = "aim_point"
+heliostat_position = "position"
+heliostat_parameters = "parameters"
+heliostat_individual_surface_points = "individual_surface_points"
+heliostat_individual_surface_normals = "individual_surface_normals"
+heliostat_height = "height"
+heliostat_width = "width"
+
+facets_list = "facets_list"
+facet_position = "position"
+facet_span_north = "span_north"
+facet_span_east = "span_east"
+
+# Dict for receiver
+receiver_prefix = "receiver"
+receiver_center = "center"
+
+# Dict for sun
+sun_prefix = "sun"
+sun_distribution_parameters = "distribution_parameters"
+sun_distribution_type = "distribution_type"
+sun_mean = "mean"
+sun_covariance = "covariance"
+sun_number_of_rays = "number_of_rays"
+
+
+# Dict for type_mapping
+rigid_body_key = "rigid_body"
+point_cloud_facet_key = "point_cloud_facet"
+ideal_actuator_key = "ideal_actuator"
