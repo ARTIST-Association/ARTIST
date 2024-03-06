c9a825296d3705cb176d7a4fd85fcbdd392bba56
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-06 17:18:59 +0100

    build scenario with config dict and convert to 4D

--- artist/util/heliostat_configurations.py
+++ artist/util/heliostat_configurations.py
@@ -1,32 +1,86 @@
 """This file contains bibliographies for different heliostat prototypes."""
 
+from artist.util import config_dictionary
+
 #### Dump from binp for test
 _test_heliostat = {
-    "height": 2.559999942779541,
-    "width": 3.2200000286102295,
-    "facets_type": "point_cloud",
-    "alignment_type": "rigid_body",
-    "actuator_type": "ideal",
-    "facets_list": {
+    config_dictionary.heliostat_height: 2.559999942779541,
+    config_dictionary.heliostat_width: 3.2200000286102295,
+    config_dictionary.facets_list: {
         "1": {
-            "position": [[-0.8075], [0.6425], [0.04019837]],
-            "span_north": [[-0.0], [0.8024845], [-0.00498457]],
-            "span_east": [[6.3749224e-01], [1.9569216e-05], [3.1505227e-03]],
+            config_dictionary.facet_position: [
+                [-0.8075],
+                [0.6425],
+                [0.04019837],
+                [1.0],
+            ],
+            config_dictionary.facet_span_north: [
+                [-0.0],
+                [0.8024845],
+                [-0.00498457],
+                [1.0],
+            ],
+            config_dictionary.facet_span_east: [
+                [6.3749224e-01],
+                [1.9569216e-05],
+                [3.1505227e-03],
+                [1.0],
+            ],
         },
         "2": {
-            "position": [[0.8075], [0.6425], [0.04019837]],
-            "span_north": [[-0.0], [0.8024845], [-0.00498457]],
-            "span_east": [[6.3749224e-01], [1.9569216e-05], [3.1505227e-03]],
+            config_dictionary.facet_position: [[0.8075], [0.6425], [0.04019837], [1.0]],
+            config_dictionary.facet_span_north: [
+                [-0.0],
+                [0.8024845],
+                [-0.00498457],
+                [1.0],
+            ],
+            config_dictionary.facet_span_east: [
+                [6.3749224e-01],
+                [1.9569216e-05],
+                [3.1505227e-03],
+                [1.0],
+            ],
         },
         "3": {
-            "position": [[-0.8075], [-0.6425], [0.04019837]],
-            "span_north": [[-0.0], [0.8024845], [-0.00498457]],
-            "span_east": [[6.3749224e-01], [1.9569216e-05], [3.1505227e-03]],
+            config_dictionary.facet_position: [
+                [-0.8075],
+                [-0.6425],
+                [0.04019837],
+                [1.0],
+            ],
+            config_dictionary.facet_span_north: [
+                [-0.0],
+                [0.8024845],
+                [-0.00498457],
+                [1.0],
+            ],
+            config_dictionary.facet_span_east: [
+                [6.3749224e-01],
+                [1.9569216e-05],
+                [3.1505227e-03],
+                [1.0],
+            ],
         },
         "4": {
-            "position": [[0.8075], [-0.6425], [0.04019837]],
-            "span_north": [[-0.0], [0.8024845], [-0.00498457]],
-            "span_east": [[6.3749224e-01], [1.9569216e-05], [3.1505227e-03]],
+            config_dictionary.facet_position: [
+                [0.8075],
+                [-0.6425],
+                [0.04019837],
+                [1.0],
+            ],
+            config_dictionary.facet_span_north: [
+                [-0.0],
+                [0.8024845],
+                [-0.00498457],
+                [1.0],
+            ],
+            config_dictionary.facet_span_east: [
+                [6.3749224e-01],
+                [1.9569216e-05],
+                [3.1505227e-03],
+                [1.0],
+            ],
         },
     },
 }
