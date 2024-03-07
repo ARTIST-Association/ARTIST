a40c30e2669fa97918cb1e6a188515e98117048e
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-07 14:28:44 +0100

    simplify scenario structure

--- artist/util/artist_type_mapping_dict.py
+++ artist/util/artist_type_mapping_dict.py
@@ -14,16 +14,3 @@ alignment_type_mapping = {config_dictionary.rigid_body_key: RigidBodyModule}
 actuator_type_mapping = {config_dictionary.ideal_actuator_key: IdealActuator}
 
 facet_type_mapping = {config_dictionary.point_cloud_facet_key: PointCloudFacetModule}
-
-
-## HERE IS AN EXAMPLE OF HOW WE CAN USE THESE DICTS
-# alignment_class = alignment_type_mapping.get(alignment_type)
-#
-# if alignment_class:
-#     self.kinematic_model = alignment_class(
-#         heliostat_name=heliostat_name, config_file=config_file
-#     )
-# else:
-#     raise NotImplementedError(
-#         "ARTIST currently only supports RigidBody Kinematic models."
-#     )
