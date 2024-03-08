7a457acf6b4adea547f26500c8f5a89e86f77fb7
Busch <marlene.busch@dlr.de>
2024-03-08 15:33:56 +0100

    fix soa, which might have been unnecessary

--- artist/tests/physics_objects/heliostats/test_load_surface_stral_data.py
+++ artist/tests/physics_objects/heliostats/test_load_surface_stral_data.py
@@ -124,27 +124,27 @@ def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
     # Beispiel ray_count = 2 -> ray_directions shape: (8, 3)
     #
 
-    selected_indices = np.arange(0, 161512, 300)
-    selected_points = aligned_surface_points[:, selected_indices]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    # ax.quiver(0, -50, 0, receiver_plane_normal[0], receiver_plane_normal[1], receiver_plane_normal[2], color='r')
+    # selected_indices = np.arange(0, 161512, 300)
+    # selected_points = aligned_surface_points[selected_indices, :].T
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection="3d")
+    # # ax.quiver(0, -50, 0, receiver_plane_normal[0], receiver_plane_normal[1], receiver_plane_normal[2], color='r')
     # ax.quiver(0, 0, 0, -incident_ray_direction[0], -incident_ray_direction[1], -incident_ray_direction[2], color='g')
-    ax.scatter(
-        selected_points[0].detach().numpy(),
-        selected_points[1].detach().numpy(),
-        selected_points[2].detach().numpy(),
-        c="b",
-        marker="o",
-    )
-    ax.set_xlabel("E")
-    ax.set_ylabel("N")
-    ax.set_zlabel("U")
-    ax.set_xlim(-3, 3)
-    # ax.set_ylim(-50, 3)
-    ax.set_ylim(-3, 3)
-    ax.set_zlim(-3, 3)
-    plt.show()
+    # ax.scatter(
+    #     selected_points[0].detach().numpy(),
+    #     selected_points[1].detach().numpy(),
+    #     selected_points[2].detach().numpy(),
+    #     c="b",
+    #     marker="o",
+    # )
+    # ax.set_xlabel("E")
+    # ax.set_ylabel("N")
+    # ax.set_zlabel("U")
+    # ax.set_xlim(-3, 3)
+    # # ax.set_ylim(-50, 3)
+    # ax.set_ylim(-3, 3)
+    # ax.set_zlim(-3, 3)
+    # plt.show()
 
     preferred_ray_directions = sun.get_preferred_reflection_direction(
         -incident_ray_direction, aligned_surface_normals
@@ -162,8 +162,8 @@ def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
         receiver_plane_normal, receiver_center, rays, aligned_surface_points
     )
 
-    dx_ints = intersections[:, :, 0] + receiver_plane_x / 2 - receiver_center[0]
-    dy_ints = intersections[:, :, 2] + receiver_plane_y / 2 - receiver_center[2]
+    dx_ints = intersections[:, 0, :] + receiver_plane_x / 2 - receiver_center[0]
+    dy_ints = intersections[:, 2, :] + receiver_plane_y / 2 - receiver_center[2]
 
     indices = (
         (-1 <= dx_ints)
@@ -189,8 +189,8 @@ def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
         receiver_plane_y,
     )
 
-    # plt.imshow(total_bitmap.T.detach().numpy(), origin="lower", cmap="jet" )
-    # plt.show()
+    plt.imshow(total_bitmap.T.detach().numpy(), origin="lower", cmap="jet" )
+    plt.show()
 
     expected_path = (
         pathlib.Path(ARTIST_ROOT)
@@ -200,4 +200,4 @@ def test_compute_bitmaps(environment_data: dict[str, torch.Tensor]) -> None:
 
     expected = torch.load(expected_path)
 
-    # torch.testing.assert_close(total_bitmap, expected)
+    torch.testing.assert_close(total_bitmap, expected)
