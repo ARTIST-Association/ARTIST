d33de3790ad240c3c04c88501a007192fe4e5724
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-14 16:15:59 +0100

    add test_data

--- artist/util/utils.py
+++ artist/util/utils.py
@@ -159,3 +159,102 @@ def rotate_align_new(v1, v2):
     dot_product = np.clip(dot_product, -1.0, 1.0)
     angle_radians = np.arccos(dot_product)
     return rotate_axis_angle(axis, angle_radians)
+
+
+def rotate_enu(
+    e: torch.Tensor,
+    n: torch.Tensor,
+    u: torch.Tensor,
+):
+    """
+    Rotate around the east, then the north, then the up axis in this very order in a right handed east north up
+    coordinate system. Positive angles result in a rotation in the mathematical direction of rotation, i.e.
+    counter-clockwise. Points need to be multiplied as column vectors from the right hand side with the
+    resulting rotation matrix. Note that the order is fixed due to the non-commutative property of matrix-matrix
+    multiplication.
+
+    Parameters
+    ----------
+    e : torch.Tensor
+        East rotation angle in radians.
+    n : torch.Tensor
+        North rotation angle in radians
+    u : torch.Tensor
+        Up rotation angle in radians.
+
+    Returns
+    -------
+    torch.Tensor
+        Corresponding rotation matrix.
+    """
+    cos_e = torch.cos(e)
+    sin_e = torch.sin(e)
+    cos_n = torch.cos(n)
+    sin_n = torch.sin(n)
+    cos_u = torch.cos(u)
+    sin_u = torch.sin(u)
+    zeros = torch.zeros(e.shape)
+    ones = torch.ones(e.shape)
+
+    return torch.stack(
+        [
+            torch.stack(
+                [
+                    cos_u * cos_n,
+                    -sin_u * cos_e + cos_u * sin_n * sin_e,
+                    -sin_u * sin_e - cos_u * sin_n * cos_e,
+                    zeros,
+                ],
+                dim=1,
+            ),
+            torch.stack(
+                [
+                    sin_u * cos_n,
+                    cos_u * cos_e + sin_u * sin_n * sin_e,
+                    cos_u * sin_e - sin_u * sin_n * cos_e,
+                    zeros,
+                ],
+                dim=1,
+            ),
+            torch.stack([sin_n, -cos_n * sin_e, cos_n * cos_e, zeros], dim=1),
+            torch.stack([zeros, zeros, zeros, ones], dim=1),
+        ],
+        dim=1,
+    )
+
+
+def translate_enu(
+    e: torch.Tensor,
+    n: torch.Tensor,
+    u: torch.Tensor,
+):
+    """
+    Translate a given point in the east, north and up direction. Note that the point must be multiplied as a column
+    vector from the right hand side of the resulting matrix.
+
+    Parameters
+    ----------
+    e : torch.Tensor
+        East translation.
+    n : torch.Tensor
+        North translation
+    u : torch.Tensor
+        Up translation.
+
+    Returns
+    -------
+    torch.Tensor
+        Corresponding rotation matrix.
+    """
+    zeros = torch.zeros(e.shape)
+    ones = torch.ones(e.shape)
+
+    return torch.stack(
+        [
+            torch.stack([ones, zeros, zeros, e], dim=1),
+            torch.stack([zeros, ones, zeros, n], dim=1),
+            torch.stack([zeros, zeros, ones, u], dim=1),
+            torch.stack([zeros, zeros, zeros, ones], dim=1),
+        ],
+        dim=1,
+    )
