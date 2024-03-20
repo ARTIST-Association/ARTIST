d8529fbd33ee2b4b7ac59162335af8133dfd2189
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-20 15:00:42 +0100

    add init

---
+++ artist/environment/__init__.py
@@ -0,0 +1,11 @@
+"""
+This package bundles all classes that represent the environment in ARTIST.
+"""
+
+from artist.environment.light_source import ALightSource
+from artist.environment.sun import Sun
+
+__all__ = [
+    "ALightSource",
+    "Sun",
+]
