c59ca64ee938bd1d2da749da102dc19962ef6e32
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-21 11:59:14 +0100

    remove extra imports

--- artist/__init__.py
+++ artist/__init__.py
@@ -2,8 +2,4 @@
 
 import os
 
-from artist import physics_objects, raytracing, scene, util
-
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
-
-__all__ = ["scene", "physics_objects", "raytracing", "util"]
