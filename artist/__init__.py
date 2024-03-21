1683d0fda74c4e3d235bf220c952e00dc25d84d4
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-21 14:04:36 +0100

    break things

--- artist/__init__.py
+++ artist/__init__.py
@@ -2,14 +2,14 @@
 
 import os
 
-from . import physics_objects, raytracing, scene, util
+# from . import physics_objects, raytracing, scene, util
 
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
 
-__all__ = [
-    "physics_objects",
-    "raytracing",
-    "scene",
-    "util",
-    "ARTIST_ROOT",
-]
+# __all__ = [
+#     "physics_objects",
+#     "raytracing",
+#     "scene",
+#     "util",
+#     "ARTIST_ROOT",
+# ]
