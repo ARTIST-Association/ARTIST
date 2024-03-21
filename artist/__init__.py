1b8ab78ab069a84a8032020c9a259616528f6f6b
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-21 11:20:07 +0100

    fix init files

--- artist/__init__.py
+++ artist/__init__.py
@@ -4,7 +4,6 @@ import os
 
 from artist import environment, physics_objects, raytracing, util
 
-__all__ = ["environment", "physics_objects", "raytracing", "util"]
-
-
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
+
+__all__ = ["environment", "physics_objects", "raytracing", "util", "ARTIST_ROOT"]
