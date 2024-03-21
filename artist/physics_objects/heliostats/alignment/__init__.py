f85def41ec42804c8b2ae58f4f0072262983505f
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-03-21 11:13:09 +0100

    fix init files

--- artist/physics_objects/heliostats/alignment/__init__.py
+++ artist/physics_objects/heliostats/alignment/__init__.py
@@ -1,7 +1,9 @@
 """This package bundles all classes that are used as alignment modules in ARTIST."""
 
+from artist.physics_objects.heliostats.alignment import kinematic
 from artist.physics_objects.heliostats.alignment.alignment import AlignmentModule
 
 __all__ = [
     "AlignmentModule",
+    "kinematic",
 ]
