e59e729ffb4a5034d8b4db474bf2d7b142ba6868
Busch <marlene.busch@dlr.de>
2024-03-22 09:20:45 +0100

    remove not used functions and include Docstrings

--- artist/physics_objects/module.py
+++ artist/physics_objects/module.py
@@ -3,41 +3,9 @@ import torch
 
 class AModule(torch.nn.Module):
     """
-    [INSERT DESCRIPTION HERE!].
-
-    Methods
-    -------
-    get_optimizer()
-        [INSERT DESCRIPTION HERE!]
-    forward()
-        [INSERT DESCRIPTION HERE!]
+    This is an abstract base class for all modules.
     """
 
     def __init__(self):
         super().__init__()
 
-    def get_optimizer_config(self):
-        """
-        [INSERT DESCRIPTION HERE!].
-
-        Returns
-        -------
-        [INSERT DESCRIPTION HERE!]
-        """
-        return self._optimizer_cfg
-
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        """
-        [INSERT DESCRIPTION HERE!].
-
-        Parameters
-        ----------
-        inputs : torch.Tensor
-            [INSERT DESCRIPTION HERE!]
-
-        Returns
-        -------
-        torch.Tensor
-            [INSERT DESCRIPTION HERE!]
-        """
-        raise NotImplementedError("Must be overwritten!")
