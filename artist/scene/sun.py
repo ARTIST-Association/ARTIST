96741426c82cba2a07bce3a9367ec5c731a92cb1
MarleneBusch <145541950+MarleneBusch@users.noreply.github.com>
2024-03-22 13:32:29 +0100

    fix typo
    Co-authored-by: Marie Weiel <marie.weiel@kit.edu>

--- artist/scene/sun.py
+++ artist/scene/sun.py
@@ -230,7 +230,7 @@ class Sun(ALightSource):
         ndotu = ray_directions @ plane_normal
         assert (
             torch.abs(ndotu) >= epsilon
-        ).all(), "No intersection or line is within plane"
+        ).all(), "No intersection or line is within plane."
         ds = (plane_point - surface_points) @ plane_normal / ndotu
 
         return surface_points + ray_directions * ds.unsqueeze(-1)
