3040765f2174d98c598f5d97218b70033be922ca
MarleneBusch <145541950+MarleneBusch@users.noreply.github.com>
2024-03-22 13:17:38 +0100

    type hints
    Co-authored-by: Marie Weiel <marie.weiel@kit.edu>

--- artist/physics_objects/actuator_ideal.py
+++ artist/physics_objects/actuator_ideal.py
@@ -29,7 +29,7 @@ class IdealActuator(AActuatorModule):
 
         Parameters
         ----------
-        motor_steps
+        motor_steps : torch.Tensor
             The motor steps.
 
         Returns
