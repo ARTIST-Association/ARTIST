f6a20af8d127ca06d043e310a6152b713415d563
Busch <marlene.busch@dlr.de>
2024-03-25 11:24:15 +0100

    handle actuator parameters

--- artist/physics_objects/actuator.py
+++ artist/physics_objects/actuator.py
@@ -11,6 +11,16 @@ class AActuatorModule(torch.nn.Module):
         Descriptor (number) of the joint.
     clockwise : bool
         Turning direction of the joint.
+    increment : torch.Tensor
+        The increment of an actuator.
+    initial_stroke_length : torch.Tensor
+        The initial_stroke_length.
+    actuator_offset : torch.Tensor
+        The actuator offset.
+    joint_radius : toorch.Tensor
+        The joint radius.
+    phi_0 : torch.Tensor
+        An initial angle.
 
     Methods
     -------
@@ -22,10 +32,20 @@ class AActuatorModule(torch.nn.Module):
         self,
         joint_number: int,
         clockwise: bool,
+        increment: torch.Tensor,
+        initial_stroke_length: torch.Tensor,
+        actuator_offset: torch.Tensor,
+        joint_radius: torch.Tensor,
+        phi_0: torch.Tensor,
     ) -> None:
         super().__init__()
         self.joint_number = joint_number
         self.clockwise = clockwise
+        self.increment = increment
+        self.initial_stroke_length = initial_stroke_length
+        self.actuator_offset = actuator_offset
+        self.joint_radius = joint_radius
+        self.phi_0 = phi_0
 
     def forward(self) -> torch.Tensor:
         """
