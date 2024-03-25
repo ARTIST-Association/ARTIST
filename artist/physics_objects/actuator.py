790b12c7423d312aa34757a1e3488b6f1d0ac303
Busch <marlene.busch@dlr.de>
2024-03-25 13:20:44 +0100

    fix typos

--- artist/physics_objects/actuator.py
+++ artist/physics_objects/actuator.py
@@ -17,7 +17,7 @@ class AActuatorModule(torch.nn.Module):
         The initial_stroke_length.
     actuator_offset : torch.Tensor
         The actuator offset.
-    joint_radius : toorch.Tensor
+    radius : torch.Tensor
         The joint radius.
     phi_0 : torch.Tensor
         An initial angle.
@@ -35,7 +35,7 @@ class AActuatorModule(torch.nn.Module):
         increment: torch.Tensor,
         initial_stroke_length: torch.Tensor,
         actuator_offset: torch.Tensor,
-        joint_radius: torch.Tensor,
+        radius: torch.Tensor,
         phi_0: torch.Tensor,
     ) -> None:
         super().__init__()
@@ -44,7 +44,7 @@ class AActuatorModule(torch.nn.Module):
         self.increment = increment
         self.initial_stroke_length = initial_stroke_length
         self.actuator_offset = actuator_offset
-        self.joint_radius = joint_radius
+        self.radius = radius
         self.phi_0 = phi_0
 
     def forward(self) -> torch.Tensor:
