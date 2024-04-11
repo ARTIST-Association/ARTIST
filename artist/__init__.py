2a9c0df032c3b17e57efa66d72bf79e73c0bd2ed
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-04-11 17:19:50 +0200

    include scenario object

--- artist/__init__.py
+++ artist/__init__.py
@@ -2,9 +2,12 @@
 
 import os
 
+from scenario import Scenario
+
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
 
 __all__ = [
+    "Scenario",
     "field",
     "raytracing",
     "scene",
