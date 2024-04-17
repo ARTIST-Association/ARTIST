1ba7e86184f627beca91c0005f85a138d1539b10
Busch <marlene.busch@dlr.de>
2024-04-17 16:34:14 +0200

    bug fixes, normals might work now, test is missing

--- artist/field/nurbs_differentiable.py
+++ artist/field/nurbs_differentiable.py
@@ -13,8 +13,12 @@ class NURBSSurface(torch.nn.Module):
         self._dimension = dimension
         self.degree_x = degree_x
         self.degree_y = degree_y
-        self.evaluation_points_x = torch.linspace(1e-5, 1.0-1e-5, steps=output_dimension_x, dtype=torch.float32)
-        self.evaluation_points_y = torch.linspace(1e-5, 1.0-1e-5, steps=output_dimension_y, dtype=torch.float32)
+        self.evaluation_points_rows_x = torch.linspace(1e-5, 1.0-1e-5, steps=output_dimension_x, dtype=torch.float32)
+        self.evaluation_points_columns_y = torch.linspace(1e-5, 1.0-1e-5, steps=output_dimension_y, dtype=torch.float32)
+        self.evaluation_points =  torch.cartesian_prod(self.evaluation_points_rows_x, self.evaluation_points_columns_y)
+        self.evaluation_points_x = self.evaluation_points[:, 0]
+        self.evaluation_points_y = self.evaluation_points[:, 1]
+
 
     def forward(self, input):
         control_points, knot_vector_x, knot_vector_y = input
@@ -22,11 +26,11 @@ class NURBSSurface(torch.nn.Module):
 
         # find span indices x direction (based on A2.1) 
         # add -1
-        span_indices_x = torch.empty(len(self.evaluation_points_x), dtype=torch.int64)
+        span_indices_x = torch.zeros(len(self.evaluation_points_x), dtype=torch.int64)
         for i, evaluation_point in enumerate(self.evaluation_points_x):
             if torch.isclose(evaluation_point, knot_vector_x[:, control_points.shape[1] + 1], atol=1e-5, rtol=1e-5):
                 span_indices_x[i] = control_points.shape[1] - 1
-                break
+                continue
             low = self.degree_x
             high = control_points.shape[1] + 1
             mid = (low + high) // 2
@@ -41,15 +45,15 @@ class NURBSSurface(torch.nn.Module):
         evaluation_points_x = self.evaluation_points_x.squeeze(0)
 
         # find basis values x direction (based on A2.2)
-        basis_values_x = BasisFunction.apply(evaluation_points_x, knot_vector_x, span_indices_x.unsqueeze(-1), self.degree_x).unsqueeze(2).unsqueeze(-1).unsqueeze(-1)
+        basis_values_x = BasisFunction.apply(evaluation_points_x, knot_vector_x, span_indices_x.unsqueeze(-1), self.degree_x)
 
         # find span indices y direction (based on A2.1)
         # add - 1
-        span_indices_y = torch.empty(len(self.evaluation_points_y), dtype=torch.int64)
+        span_indices_y = torch.zeros(len(self.evaluation_points_y), dtype=torch.int64)
         for i, evaluation_point in enumerate(self.evaluation_points_y):
             if torch.isclose(evaluation_point, knot_vector_x[:, control_points.shape[2] + 1], atol=1e-5, rtol=1e-5):
                 span_indices_y[i] = control_points.shape[2] - 1
-                break
+                continue
             low = self.degree_y
             high = control_points.shape[2] + 1
             mid = (low + high) // 2
@@ -64,29 +68,30 @@ class NURBSSurface(torch.nn.Module):
         evaluation_points_y = self.evaluation_points_y.squeeze(0)
         
         # find basis values y direction (based on A2.2)
-        basis_values_y = BasisFunction.apply(evaluation_points_y, knot_vector_y, span_indices_y.unsqueeze(-1), self.degree_y).unsqueeze(1).unsqueeze(-1).unsqueeze(-3)
+        basis_values_y = BasisFunction.apply(evaluation_points_y, knot_vector_y, span_indices_y.unsqueeze(-1), self.degree_y)
 
-        # added parentheses to make it work
-        pts = torch.stack([
-            torch.stack([
-                torch.stack([
-                    control_points[s,(span_indices_x.unsqueeze(0)[s,:]-(self.degree_x+l)),:,:][:,(span_indices_y.unsqueeze(0)[s,:]-(self.degree_y+r)),:]
-                    for r in range(self.degree_y+1)
-                ])
-                for l in range(self.degree_x+1)
-            ]) 
-            for s in range(knot_vector_x.size(0))
-        ])
+        # # added parentheses to make it work
+        # pts = torch.stack([
+        #     torch.stack([
+        #         torch.stack([
+        #             control_points[s,(span_indices_x.unsqueeze(0)[s,:]-(self.degree_x+l)),:,:][:,(span_indices_y.unsqueeze(0)[s,:]-(self.degree_y+r)),:]
+        #             for r in range(self.degree_y+1)
+        #         ])
+        #         for l in range(self.degree_x+1)
+        #     ]) 
+        #     for s in range(knot_vector_x.size(0))
+        # ])
 
-        surfaces = torch.sum((basis_values_x*pts)*basis_values_y, (1,2))
-        surfaces = surfaces[:,:,:,:self._dimension]
+        # surfaces = torch.sum((basis_values_x*pts)*basis_values_y, (1,2))
+        # surfaces = surfaces[:,:,:,:self._dimension]
+
+        control_point_weights = torch.zeros((control_points.shape[1],control_points.shape[2]) + (1,))
+        control_points = torch.cat([control_points.squeeze(0), control_point_weights], dim=-1)
 
         # find derivatives of basis values in x and y direction (based on A2.3)
         nth_derivative = 1
-        derivatives = torch.empty(len(evaluation_points_x), nth_derivative + 1, nth_derivative + 1, control_points.shape[-1])
-        basis_values_derivatives_x = BasisFunctionDerivatives.apply(evaluation_points_x, knot_vector_x.squeeze(0), span_indices_x, self.degree_x, nth_derivative)
-        basis_values_derivatives_y = BasisFunctionDerivatives.apply(evaluation_points_y, knot_vector_y.squeeze(0), span_indices_y, self.degree_y, nth_derivative)
-        
+        derivatives = torch.zeros(len(evaluation_points_x), nth_derivative + 1, nth_derivative + 1, control_points.shape[-1])
+
         # find surface normals based on A3.6
         dx = min(nth_derivative, self.degree_x)
         for k in range(self.degree_x + 1, nth_derivative + 1):
@@ -96,23 +101,25 @@ class NURBSSurface(torch.nn.Module):
         for l in range(self.degree_y + 1, nth_derivative + 1):
             for k in range(nth_derivative - l + 1):
                 derivatives[:, k, l] = 0
-        temp = [torch.empty((len(evaluation_points_x), control_points.shape[-1])) for _ in range(self.degree_y + 1)]
+        
+        basis_values_derivatives_x = BasisFunctionDerivatives.apply(evaluation_points_x, knot_vector_x.squeeze(0), span_indices_x, self.degree_x, dx)
+        basis_values_derivatives_y = BasisFunctionDerivatives.apply(evaluation_points_y, knot_vector_y.squeeze(0), span_indices_y, self.degree_y, dy)
+        
+        temp = [torch.zeros((len(evaluation_points_x), control_points.shape[-1])) for _ in range(self.degree_y + 1)]
         for k in range(dx + 1): 
             for s in range(self.degree_y + 1):
                 temp[s] = torch.zeros_like(temp[s])
                 for r in range(self.degree_x + 1):
-                    temp[s] += (basis_values_derivatives_x[k][r].squeeze(0).squeeze(0).unsqueeze(-1) * control_points.squeeze(0)[span_indices_x - self.degree_x + r, span_indices_y - self.degree_y + s])
+                    temp[s] += (basis_values_derivatives_x[k][r].squeeze(0).squeeze(0).unsqueeze(-1) * control_points[span_indices_x - self.degree_x + r, span_indices_y - self.degree_y + s])
             dd = min(nth_derivative - k, dy)
             for l in range(dd + 1):
                 derivatives[:, k, l] = 0
                 for s in range(self.degree_y + 1):
-                    derivatives[:, k, l] += ((basis_values_derivatives_y[l][s].squeeze(0).squeeze(0).unsqueeze(-1) * temp[s])).squeeze(0).squeeze(0)
-        
-        normals = []
-        for i in derivatives:
-            normals.append(torch.linalg.cross(i[0][1][:3], i[1][1][:3]))
-        torch.stack(normals)
-        return surfaces, normals
+                    derivatives[:, k, l] += ((basis_values_derivatives_y[l][s].unsqueeze(-1) * temp[s])).squeeze(0).squeeze(0)
+
+        cross_prod = torch.linalg.cross(derivatives[:, 1, 1, :3], derivatives[:, 1, 1, :3])
+
+        return derivatives[:, 0, 0], cross_prod
 
 
 class BasisFunction(torch.autograd.Function):
@@ -141,47 +148,14 @@ class BasisFunction(torch.autograd.Function):
         ctx.basis_values = basis
         return basis
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        knot_vector = ctx.saved_tensors[0]
-        span_indices = ctx.span_indices
-        degree = ctx.degree
-        basis_values = ctx.basis_values
-        evaluation_points = ctx.evaluation_points
-
-        UList = torch.stack([knot_vector[s, span_indices[s,:]] for s in range(knot_vector.size(0))])
-
-        derivatives_basis_values = [grad_output[:,0,:]*0 for i in range(degree+1)]
-        derivatives_basis_values[0] = grad_output[:,0,:]*0 + 0.5*UList*basis_values[:,0,:]
-
-        for k in reversed(range(1,degree+1)):
-            temp = derivatives_basis_values[k]*grad_output[:,k,:]
-            for r in reversed(range(k)):
-                left = torch.stack([knot_vector[s, span_indices[s,:] + r + 1] for s in range(knot_vector.size(0))])
-                right = torch.stack([knot_vector[s, span_indices[s,:] + 1 - k + r] for s in range(knot_vector.size(0))])
-                temp = temp*(evaluation_points-right)
-                derivatives_basis_values[r] += (left - evaluation_points)*temp
-                derivatives_basis_values[r] = derivatives_basis_values[r]/((left - evaluation_points) + (evaluation_points - right))
-
-        derivatives = torch.stack(derivatives_basis_values).permute(1,0,2)
-
-        dU = knot_vector*0
-        for s in range(knot_vector.size(0)):
-            for k in range(0, degree + 1):
-                dU[s, :].scatter_(-1, (span_indices[s,:] + k).type_as(span_indices), derivatives[s, k, :], reduce='add')
-        dU = dU*knot_vector
-
-        return Variable(knot_vector*0), Variable(dU), Variable(knot_vector*0), None
-    
-
 class BasisFunctionDerivatives(torch.autograd.Function):
     @staticmethod
     def forward(ctx, evaluation_points, knot_vector, span, degree, nth_derivative=1):
         num_evaluation_points = len(evaluation_points)
-        ndu = [[torch.empty(num_evaluation_points) for _ in range(degree + 1)] for _ in range(degree + 1)]
+        ndu = [[torch.zeros(num_evaluation_points) for _ in range(degree + 1)] for _ in range(degree + 1)]
         ndu[0][0] = torch.ones_like(ndu[0][0])
-        left = [torch.empty(num_evaluation_points) for _ in range(degree + 1)]
-        right = [torch.empty(num_evaluation_points) for _ in range(degree + 1)]
+        left = [torch.zeros(num_evaluation_points) for _ in range(degree + 1)]
+        right = [torch.zeros(num_evaluation_points) for _ in range(degree + 1)]
         for j in range(1, degree + 1):
             left[j] = evaluation_points - knot_vector[span + 1 - j]
             right[j] = knot_vector[span + j] - evaluation_points
@@ -192,10 +166,10 @@ class BasisFunctionDerivatives(torch.autograd.Function):
                 ndu[r][j] = saved + right[r + 1] * tmp
                 saved = left[j - r] * tmp
             ndu[j][j] = saved
-        ders = [[torch.empty(num_evaluation_points) for _ in range(degree + 1)] for _ in range(nth_derivative + 1)]
+        ders = [[torch.zeros(num_evaluation_points) for _ in range(degree + 1)] for _ in range(nth_derivative + 1)]
         for j in range(degree + 1):
             ders[0][j] = ndu[j][degree]
-        a = [[torch.empty(num_evaluation_points) for _ in range(degree + 1)] for _ in range(2)]
+        a = [[torch.zeros(num_evaluation_points) for _ in range(degree + 1)] for _ in range(2)]
         for r in range(degree + 1):
             s1 = 0
             s2 = 1
@@ -230,28 +204,57 @@ class BasisFunctionDerivatives(torch.autograd.Function):
         for k in range(1, nth_derivative + 1):
             for j in range(degree + 1):
                 ders[k][j] *= r
-            r *= degree - k
+            r *= (degree - k)
         return ders
-            
 
-num_control_points_x = 4
-num_control_points_y = 4
-control_points_shape = (num_control_points_x, num_control_points_y)                        
-control_points = torch.empty(control_points_shape + (3,))
-degree_x = 2
-degree_y = 2
 
-knots_x = torch.zeros(control_points_shape[0] + degree_x + 1)                                                           
-knots_x[(degree_x+1):-(degree_x+1)] = 0.5                                               
-knots_x[-(degree_x+1):] = 1
 
-knots_y = torch.zeros(control_points_shape[1] + degree_y + 1)                                                           
-knots_y[(degree_y+1):-(degree_y+1)] = 0.5                                               
-knots_y[-(degree_y+1):] = 1
+# receiver_plane_x = 8.629666667
+# receiver_plane_y = 7.0
 
-nurbs = NURBSSurface(degree_x, degree_y, 10, 10)
-input = (control_points, knots_x.unsqueeze(0), knots_y.unsqueeze(0))
-surface_points, surface_normals = nurbs(input)
+# degree_x = 2
+# degree_y = 2
+# num_control_points_x = 7
+# num_control_points_y = 7
 
-print('points', surface_points)
-print(surface_normals)
+# next_degree_x = degree_x + 1                                                          
+# next_degree_y = degree_y + 1 
+
+# origin = torch.tensor([0.0, 5.0, 0.0]) # heliostat position in field                                                                                    
+
+# control_points_shape = (num_control_points_x, num_control_points_y)                       
+# control_points = torch.zeros(
+#     control_points_shape + (3,),                                                  
+# )
+
+# origin_offsets_x = torch.linspace(
+#     -receiver_plane_x / 2, receiver_plane_x / 2, num_control_points_x)
+# origin_offsets_y = torch.linspace(
+#     -receiver_plane_y / 2, receiver_plane_y / 2, num_control_points_y)
+# origin_offsets = torch.cartesian_prod(origin_offsets_x, origin_offsets_y)
+# origin_offsets = torch.hstack((
+#     origin_offsets,
+#     torch.zeros((len(origin_offsets), 1)),
+# ))
+# control_points = (origin + origin_offsets).reshape(control_points.shape)                      
+
+# knots_x = torch.zeros(num_control_points_x + next_degree_x)                                                                                              
+# num_knot_vals = len(knots_x[degree_x:-degree_x])
+# knot_vals = torch.linspace(0, 1, num_knot_vals)
+# knots_x[:degree_x] = 0
+# knots_x[degree_x:-degree_x] = knot_vals
+# knots_x[-degree_x:] = 1
+
+# knots_y = torch.zeros(num_control_points_y + next_degree_y)                                                                                        
+# num_knot_vals = len(knots_y[degree_y:-degree_y])
+# knot_vals = torch.linspace(0, 1, num_knot_vals)
+# knots_y[:degree_y] = 0
+# knots_y[degree_y:-degree_y] = knot_vals
+# knots_y[-degree_y:] = 1
+
+# nurbs = NURBSSurface(degree_x, degree_y, 2, 2)
+# input = (control_points, knots_x.unsqueeze(0), knots_y.unsqueeze(0))
+# surface_points, surface_normals = nurbs(input)
+
+# print('points', surface_points)
+# print(surface_normals)
--- tests/physics_objects/test_nurbs.py
+++ tests/physics_objects/test_nurbs.py
@@ -1,13 +1,15 @@
 import datetime
 import math
+import pathlib
 from typing import Dict
 
+from matplotlib import pyplot as plt
 import pytest
 import torch
 import h5py
 
 from artist import ARTIST_ROOT
-from artist.field.nurbs_diff import NURBSSurface
+from artist.field.nurbs_differentiable import NURBSSurface
 from artist.field.heliostat import Heliostat
 from artist.scene.sun import Sun
 from artist.util import config_dictionary
@@ -94,50 +96,143 @@ def data(request):
 def test_nurbs(environment_data: Dict[str, torch.Tensor]):
     torch.manual_seed(7)
     sun = environment_data["sun"]
+    receiver_center = environment_data["receiver_center"]
+    incident_ray_direction = environment_data["incident_ray_direction"]
+    expected_value = environment_data["expected_value"]
     aligned_surface_points = environment_data["aligned_surface_points"]
-
-    # build control point vector from aligned surface points
-    # remove some elements
-    #control_points = torch.reshape(aligned_surface_points[:160801], (401, -1, 4)).unsqueeze(0)
-    
     aligned_surface_normals = environment_data["aligned_surface_normals"]
 
-    degree_x = 3
-    degree_y = 3
-    num_control_points_x = 401
-    num_control_points_y = 401
+    receiver_plane_normal = torch.tensor([0.0, 1.0, 0.0, 0.0])
+    receiver_plane_x = 8.629666667
+    receiver_plane_y = 7.0
+    receiver_resolution_x = 256
+    receiver_resolution_y = 256
 
-    next_degree_x = degree_x + 1                                                                # == Order
-    next_degree_y = degree_y + 1                                                                # == Order
-    assert num_control_points_x > degree_x, \
-        f'need at least {next_degree_x} control points in x direction'                          # num_control_points > degree + 1       
-    assert num_control_points_y > degree_y, \
-        f'need at least {next_degree_y} control points in y direction'                          # num_control_points > degree + 1
+    degree_x = 2
+    degree_y = 2
+    num_control_points_x = 7
+    num_control_points_y = 7
+
+    origin = torch.tensor([0.0, 5.0, 0.0]) # heliostat position in field                                                                                    
     
-    control_points_real = torch.reshape(aligned_surface_points[:160801], (num_control_points_x, num_control_points_y, 4))
-    control_points_shape = (num_control_points_x, num_control_points_y)                         # grid of control points
-    control_points = torch.empty(
-        control_points_shape + (4,),                                                            # each control point is 3 dimensional
-    )   
-    knots_x = torch.zeros(num_control_points_x + next_degree_x)                                 # num_control_points + order == num_control_points + degree + 1                                                             # fill knot vector with full multiplicity knots ranging from 0 to 1
-    knots_x[next_degree_x:-next_degree_x] = 0.5                                                 # Why full multiplicity knots in the middle?
-    knots_x[-next_degree_x:] = 1
+    control_points_shape = (num_control_points_x, num_control_points_y)                       
+    control_points = torch.zeros(
+        control_points_shape + (3,),                                                  
+    )
 
-    knots_y = torch.zeros(num_control_points_y + next_degree_y)                                 # num_control_points + order == num_control_points + degree + 1                                                           # fill knot vector with full multiplicity knots ranging from 0 to 1
-    knots_y[next_degree_y:-next_degree_y] = 0.5                                                 # Why full multiplicity knots in the middle?
-    knots_y[-next_degree_y:] = 1
+    origin_offsets_x = torch.linspace(
+        -receiver_plane_x / 2, receiver_plane_x / 2, num_control_points_x)
+    origin_offsets_y = torch.linspace(
+        -receiver_plane_y / 2, receiver_plane_y / 2, num_control_points_y)
+    origin_offsets = torch.cartesian_prod(origin_offsets_x, origin_offsets_y)
+    origin_offsets = torch.hstack((
+        origin_offsets,
+        torch.zeros((len(origin_offsets), 1)),
+    ))
+    control_points = torch.nn.parameter.Parameter((origin + origin_offsets).reshape(control_points.shape))
+
+    # num_control_points_x = 401
+    # num_control_points_y = 401
+    # control_points_real = torch.reshape(aligned_surface_points[:160801], (num_control_points_x, num_control_points_y, 4))
+    # control_points_real = control_points_real[:,:, :3]
+    # control_points_shape = (num_control_points_x, num_control_points_y)                         
+  
+    knots_x = torch.zeros(num_control_points_x + degree_x + 1)                                                                                              
+    num_knot_vals = len(knots_x[degree_x:-degree_x])
+    knot_vals = torch.linspace(0, 1, num_knot_vals)
+    knots_x[:degree_x] = 0
+    knots_x[degree_x:-degree_x] = knot_vals
+    knots_x[-degree_x:] = 1
+
+    knots_y = torch.zeros(num_control_points_y + degree_y + 1)                                                                                        
+    num_knot_vals = len(knots_y[degree_y:-degree_y])
+    knot_vals = torch.linspace(0, 1, num_knot_vals)
+    knots_y[:degree_y] = 0
+    knots_y[degree_y:-degree_y] = knot_vals
+    knots_y[-degree_y:] = 1
 
     surface = NURBSSurface(degree_x,
                            degree_y,
-                           401,
-                           401,
-                           4)
+                           2,
+                           2,)
     
-    input = (control_points_real, knots_x.unsqueeze(0), knots_y.unsqueeze(0))
-    output = surface(input)
-    print(output)
-    print(output.grad_fn)
+    optimizer = torch.optim.Adam([control_points])
+    
+    input = (control_points, knots_x.unsqueeze(0), knots_y.unsqueeze(0))
 
-    loss = (output-control_points_real).sum()
-    print(loss)
-    
+    for epoch in range(10):
+        points, normals = surface(input)
+        print(points)
+        print(normals)
+
+        optimizer.zero_grad()
+
+        normals = torch.cat((normals, torch.zeros(normals.shape[0], 1)), dim = 1)
+
+        preferred_ray_directions = sun.get_preferred_reflection_direction(
+            -incident_ray_direction, normals
+        )
+
+        distortions_n, distortions_u = sun.sample(preferred_ray_directions.shape[0])
+
+        rays = sun.scatter_rays(
+            preferred_ray_directions,
+            distortions_n,
+            distortions_u,
+        )
+
+        intersections = sun.line_plane_intersections(
+            receiver_plane_normal, receiver_center, rays, points
+        )
+
+        dx_ints = intersections[:, :, 0] + receiver_plane_x / 2 - receiver_center[0]
+        dy_ints = intersections[:, :, 2] + receiver_plane_y / 2 - receiver_center[2]
+
+        indices = (
+            (-1 <= dx_ints)
+            & (dx_ints < receiver_plane_x + 1)
+            & (-1 <= dy_ints)
+            & (dy_ints < receiver_plane_y + 1)
+        )
+
+        total_bitmap = sun.sample_bitmap(
+            dx_ints,
+            dy_ints,
+            indices,
+            receiver_plane_x,
+            receiver_plane_y,
+            receiver_resolution_x,
+            receiver_resolution_y,
+        )
+
+        total_bitmap = sun.normalize_bitmap(
+            total_bitmap,
+            distortions_n.numel(),
+            receiver_plane_x,
+            receiver_plane_y,
+        )
+
+        plt.imshow(total_bitmap.detach())
+        plt.show()
+
+        expected_path = (
+            pathlib.Path(ARTIST_ROOT)
+            / "tests/physics_objects/test_bitmaps_load_surface_stral"
+            / expected_value
+        )
+
+        expected = torch.load(expected_path)
+
+        plt.imshow(expected.detach())
+        plt.show()
+
+        loss = total_bitmap - expected
+        loss.abs().sum().backward()
+
+        optimizer.step()
+
+        print(loss.abs().sum())
+
+        #loss = (output-control_points_real).sum()
+        #print(loss)
+        
