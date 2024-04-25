295ea88aad8851f13c8ec93581bc43f90cd7b857
Kaleb Phipps <58810809+kalebphipps@users.noreply.github.com>
2024-04-25 15:41:28 +0200

    fix docstring
    Co-authored-by: Marie Weiel <marie.weiel@kit.edu>

--- artist/raytracing/heliostat_tracing.py
+++ artist/raytracing/heliostat_tracing.py
@@ -29,7 +29,7 @@ class DistortionsDataset(Dataset):
         random_seed: Optional[int] = 7,
     ) -> None:
         """
-        Initialize the dataset.
+        Initialize the data set.
 
         Parameters
         ----------
