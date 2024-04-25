8c45a57a0688bd79f69e869256f26aa8562fbd98
Kaleb Phipps <58810809+kalebphipps@users.noreply.github.com>
2024-04-25 16:24:52 +0200

    remove line
    Co-authored-by: Marie Weiel <marie.weiel@kit.edu>

--- scenarios/generate_test_scenario.py
+++ scenarios/generate_test_scenario.py
@@ -94,7 +94,6 @@ heliostat_1 = SingleHeliostatConfig(
     actuator_deviations=actuator_deviations,
 )
 
-
 # Create a list of all the heliostats -- in this case only one
 all_heliostats = [heliostat_1]
 
