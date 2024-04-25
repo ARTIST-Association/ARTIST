47d6a71f28ac2392f6d40586c8011aedceb63fe0
Kaleb Phipps <58810809+kalebphipps@users.noreply.github.com>
2024-04-25 15:39:23 +0200

    fix docstring
    Co-authored-by: Marie Weiel <marie.weiel@kit.edu>

--- artist/raytracing/heliostat_tracing.py
+++ artist/raytracing/heliostat_tracing.py
@@ -19,7 +19,7 @@ class DistortionsDataset(Dataset):
     distortions_e : torch.Tensor
         The distortions in the east direction.
     number_of_heliostats : int
-        The number of heliostats in the scenario
+        The number of heliostats in the scenario.
     """
 
     def __init__(
