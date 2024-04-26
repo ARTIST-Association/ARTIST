29f4f770b95b5ce3b9346c4ae44051656217660b
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-04-26 19:24:02 +0200

    remove reference to point cloud facet

--- artist/util/artist_type_mapping_dict.py
+++ artist/util/artist_type_mapping_dict.py
@@ -3,16 +3,14 @@
 from artist.field.actuator_ideal import (
     IdealActuator,
 )
-from artist.field.facets_point_cloud import (
-    PointCloudFacet,
-)
 from artist.field.kinematic_rigid_body import (
     RigidBody,
 )
+from artist.scene import Sun
 from artist.util import config_dictionary
 
-alignment_type_mapping = {config_dictionary.rigid_body_key: RigidBody}
+light_source_type_mapping = {config_dictionary.sun_key: Sun}
+
+kinematic_type_mapping = {config_dictionary.rigid_body_key: RigidBody}
 
 actuator_type_mapping = {config_dictionary.ideal_actuator_key: IdealActuator}
-
-facet_type_mapping = {config_dictionary.point_cloud_facet_key: PointCloudFacet}
