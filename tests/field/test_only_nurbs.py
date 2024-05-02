21c9cbd9b376ef971b8e4267faae69e7fcdc55d0
Busch <marlene.busch@dlr.de>
2024-05-02 16:02:42 +0200

    add docstrings

--- tests/field/test_only_nurbs.py
+++ tests/field/test_only_nurbs.py
@@ -4,40 +4,44 @@ from artist.field.nurbs import NURBSSurface
 
 
 def test_nurbs():
+    """Test the NURBS surface only, without raytracing."""
     torch.manual_seed(7)
     x_range = torch.linspace(-5, 5, 40)
     y_range = torch.linspace(-5, 5, 40)
 
-    X, Y = torch.meshgrid(x_range, y_range, indexing='ij')
+    x, y = torch.meshgrid(x_range, y_range, indexing="ij")
 
-    z_range = torch.linspace(-5, 5, 40) 
+    z_range = torch.linspace(-5, 5, 40)
 
-    Z1, Z2 = torch.meshgrid(z_range, z_range, indexing='ij')
+    z1, z2 = torch.meshgrid(z_range, z_range, indexing="ij")
 
-    Z = torch.stack((Z1, Z2), dim=-1)
+    z = torch.stack((z1, z2), dim=-1)
 
     def generate_random_coefficients():
         return torch.randn(6)
 
     factor = 0.1
+
     def random_surface(x, y, z, coefficients):
         a, b, c, d, e, f = coefficients
-        return (factor * a * torch.sin(x) + factor *b * torch.sin(y) + factor*c * torch.sin(z[..., 0]) +
-                factor * d * torch.cos(x) + factor *e * torch.cos(y) + factor*f * torch.cos(z[..., 1]))
+        return (
+            factor * a * torch.sin(x)
+            + factor * b * torch.sin(y)
+            + factor * c * torch.sin(z[..., 0])
+            + factor * d * torch.cos(x)
+            + factor * e * torch.cos(y)
+            + factor * f * torch.cos(z[..., 1])
+        )
 
     surface_coefficients = generate_random_coefficients()
 
-    surface = random_surface(X, Y, Z, surface_coefficients)
-    surface_points = torch.stack((X.flatten(), Y.flatten(), surface.flatten()), dim=-1)
+    surface = random_surface(x, y, z, surface_coefficients)
+    surface_points = torch.stack((x.flatten(), y.flatten(), surface.flatten()), dim=-1)
     ones = torch.ones(surface_points.shape[0], 1)
     surface_points = torch.cat((surface_points, ones), dim=1)
 
-    x = surface_points[:, 0]
-    y = surface_points[:, 1]
-    z = surface_points[:, 2]
-
-    evaluation_points_x = torch.linspace(1e-5, 1-1e-5, 40)
-    evaluation_points_y = torch.linspace(1e-5, 1-1e-5, 40)
+    evaluation_points_x = torch.linspace(1e-5, 1 - 1e-5, 40)
+    evaluation_points_y = torch.linspace(1e-5, 1 - 1e-5, 40)
     evaluation_points = torch.cartesian_prod(evaluation_points_x, evaluation_points_y)
     evaluation_points_x = evaluation_points[:, 0]
     evaluation_points_y = evaluation_points[:, 1]
@@ -47,24 +51,28 @@ def test_nurbs():
 
     degree_x = 2
     degree_y = 2
-    
-    control_points_shape = (num_control_points_x, num_control_points_y)                       
+
+    control_points_shape = (num_control_points_x, num_control_points_y)
     control_points = torch.zeros(
-        control_points_shape + (3,),                                                  
+        control_points_shape + (3,),
     )
-    origin_offsets_x = torch.linspace(
-        -5, 5, num_control_points_x)
-    origin_offsets_y = torch.linspace(
-        -5, 5, num_control_points_y)
+    origin_offsets_x = torch.linspace(-5, 5, num_control_points_x)
+    origin_offsets_y = torch.linspace(-5, 5, num_control_points_y)
     origin_offsets = torch.cartesian_prod(origin_offsets_x, origin_offsets_y)
-    origin_offsets = torch.hstack((
-        origin_offsets,
-        torch.zeros((len(origin_offsets), 1)),
-    ))
+    origin_offsets = torch.hstack(
+        (
+            origin_offsets,
+            torch.zeros((len(origin_offsets), 1)),
+        )
+    )
 
-    control_points = torch.nn.parameter.Parameter((origin_offsets).reshape(control_points.shape)) 
+    control_points = torch.nn.parameter.Parameter(
+        (origin_offsets).reshape(control_points.shape)
+    )
 
-    nurbs = NURBSSurface(degree_x, degree_y, evaluation_points_x, evaluation_points_y, control_points)
+    nurbs = NURBSSurface(
+        degree_x, degree_y, evaluation_points_x, evaluation_points_y, control_points
+    )
 
     optimizer = torch.optim.Adam([control_points], lr=5e-3)
 
@@ -73,7 +81,7 @@ def test_nurbs():
 
         optimizer.zero_grad()
 
-        loss =  points - surface_points
+        loss = points - surface_points
         loss.abs().mean().backward()
 
         optimizer.step()
@@ -81,4 +89,3 @@ def test_nurbs():
         print(loss.abs().mean())
 
     torch.testing.assert_close(points, surface_points, atol=1e-2, rtol=1e-2)
-
