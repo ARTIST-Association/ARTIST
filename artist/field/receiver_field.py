825b62f66c77cd88203cf49adc1eaa6b6663d4d2
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-05-02 17:39:49 +0200

    include functionality for multiple receivers and light sources

---
+++ artist/field/receiver_field.py
@@ -0,0 +1,57 @@
+from typing import List
+
+import h5py
+import torch.nn
+from typing_extensions import Self
+
+from artist.field.receiver import Receiver
+from artist.util import config_dictionary
+
+
+class ReceiverField(torch.nn.Module):
+    """
+    This class wraps the list of receivers as a torch.nn.Module to allow gradient calculation.
+
+    Attributes
+    ----------
+    receiver_list : List[Receiver]
+        A list of receivers included in the scenario.
+
+    Methods
+    -------
+    from_hdf5()
+        Load the list of receivers from an HDF5 file.
+    """
+
+    def __init__(self, receiver_list: List[Receiver]):
+        """
+        Initialize the heliostat field.
+
+        Parameters
+        ----------
+        receiver_list : List[Heliostat]
+            The list of heliostats included in the scenario.
+        """
+        super(ReceiverField, self).__init__()
+        self.receiver_list = receiver_list
+
+    @classmethod
+    def from_hdf5(cls, config_file: h5py.File) -> Self:
+        """
+        Load a receiver field from an HDF5 file.
+
+        Parameters
+        ----------
+        config_file : h5py.File
+            The HDF5 file containing the configuration to be loaded.
+
+        Returns
+        -------
+        ReceiverField
+            The receiver field loaded from the HDF5 file.
+        """
+        receiver_field = [
+            Receiver.from_hdf5(config_file=config_file, receiver_key=receiver_name)
+            for receiver_name in config_file[config_dictionary.receiver_key].keys()
+        ]
+        return cls(receiver_list=receiver_field)
