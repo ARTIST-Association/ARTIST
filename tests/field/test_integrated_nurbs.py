5be18fe7de1811791dc791df8899a2f51f2d24b3
Busch <marlene.busch@dlr.de>
2024-05-02 16:02:20 +0200

    add docstrings

--- tests/field/test_integrated_nurbs.py
+++ tests/field/test_integrated_nurbs.py
@@ -1,14 +1,13 @@
 import pathlib
-from matplotlib import pyplot as plt
-import torch
-import h5py
-import pytest
 import warnings
 
-from artist import ARTIST_ROOT
-from artist.util import nurbs_converters
-from artist import Scenario
+import h5py
+import pytest
+import torch
+
+from artist import ARTIST_ROOT, Scenario
 from artist.raytracing.heliostat_tracing import HeliostatRayTracer
+from artist.util import nurbs_converters
 
 warnings.filterwarnings("always")
 
@@ -33,39 +32,77 @@ else:
 
 
 @pytest.fixture(scope="module")
-def common_setup():
+def common_setup() -> Scenario:
+    """
+    Create the scenario to use across different tests and learn the NURBS.
+
+    Returns
+    -------
+    Scenario
+        The loaded scenario.
+    """
     with h5py.File(f"{ARTIST_ROOT}/scenarios/test_scenario.h5", "r") as config_h5:
         scenario = Scenario.load_scenario_from_hdf5(scenario_file=config_h5)
-    
+
     width = 2
     height = 2
-    nurbs_surface = nurbs_converters.deflectometry_to_nurbs(scenario.heliostats.heliostat_list[0].concentrator.facets.surface_points[::100], scenario.heliostats.heliostat_list[0].concentrator.facets.surface_normals[::100], width, height)
-    
-    surface_points, surface_normals = nurbs_surface.calculate_surface_points_and_normals()
+    nurbs_surface = nurbs_converters.deflectometry_to_nurbs(
+        scenario.heliostats.heliostat_list[0].concentrator.facets.surface_points[::100],
+        scenario.heliostats.heliostat_list[0].concentrator.facets.surface_normals[
+            ::100
+        ],
+        width,
+        height,
+    )
 
-    scenario.heliostats.heliostat_list[0].concentrator.facets.surface_points = surface_points
-    scenario.heliostats.heliostat_list[0].concentrator.facets.surface_normals = surface_normals
+    (
+        surface_points,
+        surface_normals,
+    ) = nurbs_surface.calculate_surface_points_and_normals()
+
+    scenario.heliostats.heliostat_list[
+        0
+    ].concentrator.facets.surface_points = surface_points
+    scenario.heliostats.heliostat_list[
+        0
+    ].concentrator.facets.surface_normals = surface_normals
 
     return scenario
 
 
 @pytest.mark.parametrize(
-        "incident_ray_direction, expected_value",
-        [
-            (torch.tensor([0.0, -1.0, 0.0, 0.0]), "south.pt"),
-            (torch.tensor([1.0, 0.0, 0.0, 0.0]), "east.pt"),
-            (torch.tensor([-1.0, 0.0, 0.0, 0.0]), "west.pt"),
-            (torch.tensor([0.0, 0.0, 1.0, 0.0]), "above.pt"),
-        ]
+    "incident_ray_direction, expected_value",
+    [
+        (torch.tensor([0.0, -1.0, 0.0, 0.0]), "south.pt"),
+        (torch.tensor([1.0, 0.0, 0.0, 0.0]), "east.pt"),
+        (torch.tensor([-1.0, 0.0, 0.0, 0.0]), "west.pt"),
+        (torch.tensor([0.0, 0.0, 1.0, 0.0]), "above.pt"),
+    ],
 )
-def test_nurbs(common_setup, incident_ray_direction, expected_value):
+def test_nurbs(
+    common_setup: Scenario, incident_ray_direction: torch.Tensor, expected_value: str
+):
+    """
+    Test the nurbs surface and the raytracing process.
+
+    Parameters
+    ----------
+    common_setup : Scenario
+        The scenario shared across different tests.
+    incident_ray_direction : torch.Tensor
+        The incident ray direction.
+    expected_value : str
+        Path to the expected Bitmap.
+    """
     torch.manual_seed(7)
 
     scenario = common_setup
-    
+
     scenario.heliostats.heliostat_list[0].set_aligned_surface(incident_ray_direction)
 
-    scenario.heliostats.heliostat_list[0].set_preferred_reflection_direction(-incident_ray_direction)
+    scenario.heliostats.heliostat_list[0].set_preferred_reflection_direction(
+        -incident_ray_direction
+    )
     raytracer = HeliostatRayTracer(
         scenario=scenario, world_size=world_size, rank=rank, batch_size=5
     )
@@ -85,5 +122,3 @@ def test_nurbs(common_setup, incident_ray_direction, expected_value):
         expected = torch.load(expected_path)
 
         torch.testing.assert_close(final_bitmap.T, expected, atol=5e-4, rtol=5e-4)
-
-
