85929c543092f77cccaf5307c701e77329837fc9
Busch <marlene.busch@dlr.de>
2024-05-03 13:25:52 +0200

    change x to e and y to n

--- artist/util/nurbs_converters.py
+++ artist/util/nurbs_converters.py
@@ -33,26 +33,26 @@ def deflectometry_to_nurbs(
 
     # normalize evaluation points and shift them so that they correspond to the knots.
     # -> The evaluation points must also lie between 0 and 1 (like the knots).
-    evaluation_points_x = (
+    evaluation_points_e = (
         evaluation_points[:, 0] - min(evaluation_points[:, 0]) + 1e-5
     ) / max((evaluation_points[:, 0] - min(evaluation_points[:, 0])) + 2e-5)
-    evaluation_points_y = (
+    evaluation_points_n = (
         evaluation_points[:, 1] - min(evaluation_points[:, 1]) + 1e-5
     ) / max((evaluation_points[:, 1] - min(evaluation_points[:, 1])) + 2e-5)
 
-    num_control_points_x = 7
-    num_control_points_y = 7
+    num_control_points_e = 7
+    num_control_points_n = 7
 
-    degree_x = 2
-    degree_y = 2
+    degree_e = 2
+    degree_n = 2
 
-    control_points_shape = (num_control_points_x, num_control_points_y)
+    control_points_shape = (num_control_points_e, num_control_points_n)
     control_points = torch.zeros(
         control_points_shape + (3,),
     )
-    origin_offsets_x = torch.linspace(-width / 2, width / 2, num_control_points_x)
-    origin_offsets_y = torch.linspace(-height / 2, height / 2, num_control_points_y)
-    origin_offsets = torch.cartesian_prod(origin_offsets_x, origin_offsets_y)
+    origin_offsets_e = torch.linspace(-width / 2, width / 2, num_control_points_e)
+    origin_offsets_n = torch.linspace(-height / 2, height / 2, num_control_points_n)
+    origin_offsets = torch.cartesian_prod(origin_offsets_e, origin_offsets_n)
     origin_offsets = torch.hstack(
         (
             origin_offsets,
@@ -65,7 +65,7 @@ def deflectometry_to_nurbs(
     )
 
     nurbs_surface = NURBSSurface(
-        degree_x, degree_y, evaluation_points_x, evaluation_points_y, control_points
+        degree_e, degree_n, evaluation_points_e, evaluation_points_n, control_points
     )
 
     optimizer = torch.optim.Adam([control_points], lr=1e-2)
