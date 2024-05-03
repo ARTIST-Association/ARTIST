a5394e73915646dfa1d7b8834d7cb9fac0a50947
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-05-03 22:35:56 +0200

    fix circular imports

--- artist/__init__.py
+++ artist/__init__.py
@@ -2,12 +2,9 @@
 
 import os
 
-from .scenario import Scenario
-
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
 
 __all__ = [
-    "Scenario",
     "field",
     "raytracing",
     "scene",
