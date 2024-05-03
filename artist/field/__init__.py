71948b1994c17e105d2e1123a799e01b8444dcf6
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-05-03 22:35:56 +0200

    fix circular imports

--- artist/field/__init__.py
+++ artist/field/__init__.py
@@ -1,23 +1,29 @@
 """This package bundles all classes that represent physical objects in ARTIST."""
 
 from .actuator import Actuator
+from .actuator_array import ActuatorArray
 from .actuator_ideal import IdealActuator
-from .alignment import Alignment
-from .concentrator import Concentrator
-from .facets import Facet
-from .facets_point_cloud import PointCloudFacet
+from .actuator_linear import LinearActuator
+from .facets_nurbs import NurbsFacet
 from .heliostat import Heliostat
+from .heliostat_field import HeliostatField
 from .kinematic import Kinematic
 from .kinematic_rigid_body import RigidBody
+from .receiver import Receiver
+from .receiver_field import ReceiverField
+from .surface import Surface
 
 __all__ = [
     "Actuator",
     "IdealActuator",
-    "Alignment",
-    "Concentrator",
-    "Facet",
-    "PointCloudFacet",
+    "LinearActuator",
+    "ActuatorArray",
+    "Surface",
+    "NurbsFacet",
     "Heliostat",
+    "HeliostatField",
+    "Receiver",
+    "ReceiverField",
     "Kinematic",
     "RigidBody",
 ]
