b6aa12bf586403b154a3ef898c99325204bb9b1c
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-05-06 17:11:02 +0200

    update to include new nurbs config

--- artist/field/surface.py
+++ artist/field/surface.py
@@ -26,15 +26,16 @@ class Surface(torch.nn.Module):
         super(Surface, self).__init__()
         self.facets = [
             NurbsFacet(
-                control_points_e=facet_config.control_points_e,
-                control_points_u=facet_config.control_points_u,
-                knots_e=facet_config.knots_e,
-                knots_u=facet_config.knots_u,
+                control_points=facet_config.control_points,
+                degree_e=facet_config.degree_e,
+                degree_n=facet_config.degree_n,
+                number_eval_points_e=facet_config.number_eval_points_e,
+                number_eval_points_n=facet_config.number_eval_points_n,
                 width=facet_config.width,
                 height=facet_config.height,
                 position=facet_config.position,
                 canting_e=facet_config.canting_e,
-                canting_u=facet_config.canting_u,
+                canting_n=facet_config.canting_n,
             )
             for facet_config in surface_config.facets_list
         ]
