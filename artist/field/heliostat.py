3d97eb3b03b12135fcc68bc17ea370bb4c9738bc
Busch <marlene.busch@dlr.de>
2024-05-15 08:49:22 +0200

    remove plot

--- artist/field/heliostat.py
+++ artist/field/heliostat.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import h5py
-from matplotlib import pyplot as plt
 import torch
 from typing_extensions import Self
 
@@ -552,18 +551,6 @@ class Heliostat(torch.nn.Module):
         )
         self.is_aligned = True
         
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
-        # for facet in surface_points:
-        #     ax.scatter(facet[:, 0], facet[:, 1], facet[:, 2])
-        # plt.show()
-        
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
-        # for facet in self.current_aligned_surface_points:
-        #     ax.scatter(facet[:, 0].detach().numpy(), facet[:, 1].detach().numpy(), facet[:, 2].detach().numpy())
-        # plt.show()
-
     def set_preferred_reflection_direction(self, rays: torch.Tensor) -> None:
         """
         Reflect incoming rays according to a normal vector.
