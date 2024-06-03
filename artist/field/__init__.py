3f80ca6ea7ce4cc1ca995205994262bcda64d701
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-06-03 18:22:17 +0200

    try stuff

--- artist/field/__init__.py
+++ artist/field/__init__.py
@@ -1,29 +1,29 @@
 """Bundles all classes that represent physical objects in ARTIST."""
 
-from .actuator import Actuator
-from .actuator_array import ActuatorArray
-from .actuator_ideal import IdealActuator
-from .actuator_linear import LinearActuator
-from .facets_nurbs import NurbsFacet
-from .heliostat import Heliostat
-from .heliostat_field import HeliostatField
-from .kinematic import Kinematic
-from .kinematic_rigid_body import RigidBody
-from .receiver import Receiver
-from .receiver_field import ReceiverField
-from .surface import Surface
-
-__all__ = [
-    "Actuator",
-    "IdealActuator",
-    "LinearActuator",
-    "ActuatorArray",
-    "Surface",
-    "NurbsFacet",
-    "Heliostat",
-    "HeliostatField",
-    "Receiver",
-    "ReceiverField",
-    "Kinematic",
-    "RigidBody",
-]
+# from .actuator import Actuator
+# from .actuator_array import ActuatorArray
+# from .actuator_ideal import IdealActuator
+# from .actuator_linear import LinearActuator
+# from .facets_nurbs import NurbsFacet
+# from .heliostat import Heliostat
+# from .heliostat_field import HeliostatField
+# from .kinematic import Kinematic
+# from .kinematic_rigid_body import RigidBody
+# from .receiver import Receiver
+# from .receiver_field import ReceiverField
+# from .surface import Surface
+#
+# __all__ = [
+#     "Actuator",
+#     "IdealActuator",
+#     "LinearActuator",
+#     "ActuatorArray",
+#     "Surface",
+#     "NurbsFacet",
+#     "Heliostat",
+#     "HeliostatField",
+#     "Receiver",
+#     "ReceiverField",
+#     "Kinematic",
+#     "RigidBody",
+# ]
