736fd9ab093c74df86ec0d7cf3d77532f25e52c4
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-06-04 14:26:21 +0200

    absolute imports

--- artist/__init__.py
+++ artist/__init__.py
@@ -2,10 +2,7 @@
 
 import os
 
-print("TOP LEVEL IMPORT")
 from . import field, raytracing, scenario, scene, util
-
-print("TOP LEVEL IMPORT FINISHED")
 
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
 
