e3ccd5bd3042a08a27cd3342bc8c9d096abfa141
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-06-04 17:21:23 +0200

    document artist root

--- artist/__init__.py
+++ artist/__init__.py
@@ -5,6 +5,7 @@ import os
 from . import field, raytracing, scenario, scene, util
 
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
+"""Reference to the root directory of ARTIST."""
 
 __all__ = [
     "field",
