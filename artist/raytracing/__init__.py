c06d2fe2ff05bd92576e1309d46e833f94722650
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-06-04 14:26:21 +0200

    absolute imports

--- artist/raytracing/__init__.py
+++ artist/raytracing/__init__.py
@@ -1,8 +1,6 @@
 """Bundles all classes responsible for raytracing in ARTIST."""
 
-print("RAYTRACING IMPORT")
-from .rays import Rays
+from artist.raytracing.heliostat_tracing import DistortionsDataset, HeliostatRayTracer
+from artist.raytracing.rays import Rays
 
-print("RAYTRACING IMPORT FINISHED")
-
-__all__ = ["Rays", "raytracing_utils"]
+__all__ = ["HeliostatRayTracer", "DistortionsDataset", "Rays", "raytracing_utils"]
