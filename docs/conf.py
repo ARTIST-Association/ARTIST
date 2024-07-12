362e43ce8ba32b9837e00ab87acbb0dc6124cbbd
Kaleb Phipps <58810809+kalebphipps@users.noreply.github.com>
2024-07-12 13:46:33 +0200

    repair config for read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 from datetime import datetime
 
-from artist import ARTIST_ROOT
-
 project = "ARTIST"
 copyright = f"{datetime.now().year}, ARTIST consortium"
 author = "ARTIST Consortium"
@@ -41,7 +39,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static/"]
 html_css_files = ["custom.css"]
-html_logo = f"{ARTIST_ROOT}/logos/artist_logo.svg"
+html_logo = f"../logos/artist_logo.svg"
 
 html_theme_options = {
     "logo_only": True,
