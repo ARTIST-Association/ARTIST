cfc989a458184104ed6cbbe0cf7d471828206c53
MarleneBusch <marlene.busch@dlr.de>
2024-12-12 15:00:57 +0100

    add tests for ValueErrors

--- tests/field/test_rigid_body.py
+++ tests/field/test_rigid_body.py
@@ -12,7 +12,25 @@ from artist.util.configuration_classes import (
 
 
 @pytest.fixture
-def actuator_configuration() -> ActuatorListConfig:
+def actuator_configuration_1() -> ActuatorListConfig:
+    """
+    Define actuator parameters used in tests.
+
+    Returns
+    -------
+    ActuatorListConfig
+        A List containing parameters for each actuator.
+    """
+    actuator1_config = ActuatorConfig(
+        key="",
+        type=config_dictionary.ideal_actuator_key,
+        clockwise_axis_movement=False,
+    )
+    return ActuatorListConfig(actuator_list=[actuator1_config])
+
+
+@pytest.fixture
+def actuator_configuration_2() -> ActuatorListConfig:
     """
     Define actuator parameters used in tests.
 
@@ -32,6 +50,36 @@ def actuator_configuration() -> ActuatorListConfig:
         clockwise_axis_movement=True,
     )
     return ActuatorListConfig(actuator_list=[actuator1_config, actuator2_config])
+
+
+@pytest.fixture
+def actuator_configuration_3() -> ActuatorListConfig:
+    """
+    Define actuator parameters used in tests.
+
+    Returns
+    -------
+    ActuatorListConfig
+        A List containing parameters for each actuator.
+    """
+    actuator1_config = ActuatorConfig(
+        key="",
+        type=config_dictionary.ideal_actuator_key,
+        clockwise_axis_movement=False,
+    )
+    actuator2_config = ActuatorConfig(
+        key="",
+        type=config_dictionary.ideal_actuator_key,
+        clockwise_axis_movement=True,
+    )
+    actuator3_config = ActuatorConfig(
+        key="",
+        type=config_dictionary.ideal_actuator_key,
+        clockwise_axis_movement=True,
+    )
+    return ActuatorListConfig(
+        actuator_list=[actuator1_config, actuator2_config, actuator3_config]
+    )
 
 
 @pytest.fixture
@@ -74,7 +122,7 @@ def initial_orientation_up(device: torch.device) -> torch.Tensor:
 
 @pytest.fixture
 def kinematic_model_1(
-    actuator_configuration: ActuatorListConfig,
+    actuator_configuration_2: ActuatorListConfig,
     initial_orientation_south: torch.Tensor,
     device: torch.device,
 ) -> RigidBody:
@@ -83,7 +131,7 @@ def kinematic_model_1(
 
     Parameters
     ----------
-    actuator_configuration : ActuatorListConfig
+    actuator_configuration_2 : ActuatorListConfig
         The configuration of the actuators.
     initial_orientation_south : torch.Tensor
         The kinematic initial orientation.
@@ -100,7 +148,7 @@ def kinematic_model_1(
     return RigidBody(
         position=position,
         aim_point=aim_point,
-        actuator_config=actuator_configuration,
+        actuator_config=actuator_configuration_2,
         initial_orientation=initial_orientation_south,
         device=device,
     )
@@ -108,7 +156,7 @@ def kinematic_model_1(
 
 @pytest.fixture
 def kinematic_model_2(
-    actuator_configuration: ActuatorListConfig,
+    actuator_configuration_2: ActuatorListConfig,
     initial_orientation_south: torch.Tensor,
     device: torch.device,
 ) -> RigidBody:
@@ -117,7 +165,7 @@ def kinematic_model_2(
 
     Parameters
     ----------
-    actuator_configuration : ActuatorListConfig
+    actuator_configuration_2 : ActuatorListConfig
         The configuration of the actuators.
     initial_orientation_south : torch.Tensor
         The kinematic initial orientation.
@@ -134,7 +182,7 @@ def kinematic_model_2(
     return RigidBody(
         position=position,
         aim_point=aim_point,
-        actuator_config=actuator_configuration,
+        actuator_config=actuator_configuration_2,
         initial_orientation=initial_orientation_south,
         device=device,
     )
@@ -142,7 +190,7 @@ def kinematic_model_2(
 
 @pytest.fixture
 def kinematic_model_3(
-    actuator_configuration: ActuatorListConfig,
+    actuator_configuration_2: ActuatorListConfig,
     initial_orientation_up: torch.Tensor,
     device: torch.device,
 ) -> RigidBody:
@@ -151,7 +199,7 @@ def kinematic_model_3(
 
     Parameters
     ----------
-    actuator_configuration : ActuatorListConfig
+    actuator_configuration_2 : ActuatorListConfig
         The configuration of the actuators.
     initial_orientation_up : torch.Tensor
         The kinematic initial orientation offsets.
@@ -168,18 +216,87 @@ def kinematic_model_3(
     return RigidBody(
         position=position,
         aim_point=aim_point,
-        actuator_config=actuator_configuration,
+        actuator_config=actuator_configuration_2,
+        initial_orientation=initial_orientation_up,
+        device=device,
+    )
+
+
+@pytest.fixture
+def kinematic_model_4(
+    actuator_configuration_1: ActuatorListConfig,
+    initial_orientation_up: torch.Tensor,
+    device: torch.device,
+) -> RigidBody:
+    """
+    Create a kinematic model to use in the test.
+
+    Parameters
+    ----------
+    actuator_configuration_1 : ActuatorListConfig
+        The configuration of the actuators.
+    initial_orientation_up : torch.Tensor
+        The kinematic initial orientation offsets.
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Returns
+    -------
+    RigidBody
+        The kinematic model.
+    """
+    position = torch.tensor([0.0, 0.0, 0.0, 1.0], device=device)
+    aim_point = torch.tensor([0.0, -10.0, 0.0, 1.0], device=device)
+    return RigidBody(
+        position=position,
+        aim_point=aim_point,
+        actuator_config=actuator_configuration_1,
+        initial_orientation=initial_orientation_up,
+        device=device,
+    )
+
+
+@pytest.fixture
+def kinematic_model_5(
+    actuator_configuration_3: ActuatorListConfig,
+    initial_orientation_up: torch.Tensor,
+    device: torch.device,
+) -> RigidBody:
+    """
+    Create a kinematic model to use in the test.
+
+    Parameters
+    ----------
+    actuator_configuration_3 : ActuatorListConfig
+        The configuration of the actuators.
+    initial_orientation_up : torch.Tensor
+        The kinematic initial orientation offsets.
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Returns
+    -------
+    RigidBody
+        The kinematic model.
+    """
+    position = torch.tensor([0.0, 0.0, 0.0, 1.0], device=device)
+    aim_point = torch.tensor([0.0, -10.0, 0.0, 1.0], device=device)
+    return RigidBody(
+        position=position,
+        aim_point=aim_point,
+        actuator_config=actuator_configuration_3,
         initial_orientation=initial_orientation_up,
         device=device,
     )
 
 
 @pytest.mark.parametrize(
-    "kinematic_model_fixture, incident_ray_direction, expected",
+    "kinematic_model_fixture, input, method, expected",
     [
         (
             "kinematic_model_1",
             torch.tensor([0.0, 0.0, 1.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [1, 0, 0, 0],
@@ -202,6 +319,7 @@ def kinematic_model_3(
         (
             "kinematic_model_1",
             torch.tensor([1.0, 0.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [
@@ -224,6 +342,7 @@ def kinematic_model_3(
         (
             "kinematic_model_1",
             torch.tensor([0.0, -1.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [1.0, 0.0, 0.0, 0.0],
@@ -236,6 +355,7 @@ def kinematic_model_3(
         (
             "kinematic_model_1",
             torch.tensor([-1.0, 0.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [
@@ -258,6 +378,7 @@ def kinematic_model_3(
         (
             "kinematic_model_1",
             torch.tensor([0.0, -1.0, 1.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [1.0, 0.0, 0.0, 0.0],
@@ -280,6 +401,7 @@ def kinematic_model_3(
         (
             "kinematic_model_2",
             torch.tensor([0.0, 0.0, 1.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [1.0, 0.0, 0.0, 0.0],
@@ -302,6 +424,7 @@ def kinematic_model_3(
         (
             "kinematic_model_3",
             torch.tensor([0.0, 0.0, 1.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [1.0, 0.0, 0.0, 0.0],
@@ -324,6 +447,7 @@ def kinematic_model_3(
         (
             "kinematic_model_3",
             torch.tensor([0.0, -1.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [1.0, 0.0, 0.0, 0.0],
@@ -346,6 +470,7 @@ def kinematic_model_3(
         (
             "kinematic_model_3",
             torch.tensor([1.0, 0.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [
@@ -368,6 +493,7 @@ def kinematic_model_3(
         (
             "kinematic_model_3",
             torch.tensor([-1.0, 0.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
             torch.tensor(
                 [
                     [
@@ -387,12 +513,76 @@ def kinematic_model_3(
                 ]
             ),
         ),
+        (
+            "kinematic_model_4",
+            torch.tensor([-1.0, 0.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
+            None,
+        ),
+        (
+            "kinematic_model_5",
+            torch.tensor([-1.0, 0.0, 0.0, 0.0]),
+            "incident_ray_direction_to_orientation",
+            None,
+        ),
+        (
+            "kinematic_model_1",
+            torch.tensor([0.0, 0.0]),
+            "motor_positions_to_orientation",
+            torch.tensor(
+                [
+                    [1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0],
+                ]
+            ),
+        ),
+        (
+            "kinematic_model_2",
+            torch.tensor([5.0, 1000.0]),
+            "motor_positions_to_orientation",
+            torch.tensor(
+                [
+                    [0.562379062176, -0.826879560947, 0.000000000000, 0.000000000000],
+                    [0.234554469585, 0.159525677562, 0.958924293518, 1.000000000000],
+                    [-0.792914927006, -0.539278924465, 0.283662199974, 0.000000000000],
+                    [0.000000000000, 0.000000000000, 0.000000000000, 1.000000000000],
+                ]
+            ),
+        ),
+        (
+            "kinematic_model_3",
+            torch.tensor([10.0, 40.0]),
+            "motor_positions_to_orientation",
+            torch.tensor(
+                [
+                    [-6.6694e-01, 3.2570e-08, 7.4511e-01, 0.0000e00],
+                    [-6.2520e-01, 5.4402e-01, -5.5961e-01, 0.0000e00],
+                    [-4.0536e-01, -8.3907e-01, -3.6283e-01, 0.0000e00],
+                    [0.0000e00, 0.0000e00, 0.0000e00, 1.0000e00],
+                ]
+            ),
+        ),
+        (
+            "kinematic_model_4",
+            torch.tensor([0.0, 0.0]),
+            "motor_positions_to_orientation",
+            None,
+        ),
+        (
+            "kinematic_model_5",
+            torch.tensor([0.0, 0.0]),
+            "motor_positions_to_orientation",
+            None,
+        ),
     ],
 )
 def test_orientation_matrix(
     request: pytest.FixtureRequest,
     kinematic_model_fixture: str,
-    incident_ray_direction: torch.Tensor,
+    input: torch.Tensor,
+    method,
     expected: torch.Tensor,
     device: torch.device,
 ) -> None:
@@ -405,10 +595,12 @@ def test_orientation_matrix(
         The pytest fixture used to consider different test cases.
     kinematic_model_fixture : str
         The kinematic model fixture used to select the kinematic model used in the test case.
-    incident_ray_direction : torch.Tensor
-        The incident ray direction considered.
+    input : torch.Tensor
+
+    method
+
     expected : torch.Tensor
-        The expected orientation matrix.
+        The expected orientation matrix or ``None`` if an error is expected.
     device : torch.device
         The device on which to initialize tensors.
 
@@ -417,9 +609,16 @@ def test_orientation_matrix(
     AssertionError
         If test does not complete as expected.
     """
-    orientation_matrix = request.getfixturevalue(
-        kinematic_model_fixture
-    ).incident_ray_direction_to_orientation(
-        incident_ray_direction.to(device), device=device
-    )
-    torch.testing.assert_close(orientation_matrix, expected.to(device))
+    # Check if the ValueError is thrown as expected.
+    get_orientation = getattr(request.getfixturevalue(kinematic_model_fixture), method)
+    if expected is None:
+        with pytest.raises(ValueError) as exc_info:
+            get_orientation(input.to(device), device=device)
+        assert (
+            f"The rigid body kinematic requires exactly two actuators but {len(request.getfixturevalue(kinematic_model_fixture).actuators.actuator_list)} were specified, please check the configuration!"
+            in str(exc_info.value)
+        )
+    else:
+        # Check if the orientation matrix is correct.
+        orientation_matrix = get_orientation(input.to(device), device=device)
+        torch.testing.assert_close(orientation_matrix, expected.to(device))
---
+++ tests/scene/test_load_light_source_array.py
@@ -0,0 +1,57 @@
+import h5py
+import pytest
+import torch
+from pytest_mock import MockerFixture
+
+from artist.scene.light_source_array import LightSourceArray
+from artist.util import config_dictionary
+
+
+def test_light_source_array_load_from_hdf5_errors(
+    mocker: MockerFixture,
+    device: torch.device,
+) -> None:
+    """
+    Test the heliostat load from hdf5 method.
+
+    Parameters
+    ----------
+    mocker : MockerFixture
+        A pytest-mocker fixture used to create mock objects.
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Raises
+    ------
+    AssertionError
+        If test does not complete as expected.
+    """
+    mock_h5_file = mocker.MagicMock(spec=h5py.File)
+
+    mock_level_3 = mocker.MagicMock()
+    mock_level_3.__getitem__.return_value = b"laser"
+
+    mock_level_2 = mocker.MagicMock()
+    mock_level_2.__getitem__.side_effect = lambda key: {
+        config_dictionary.light_source_type: mock_level_3
+    }[key]
+    mock_level_2.keys.return_value = [config_dictionary.light_source_type]
+
+    mock_level_1 = mocker.MagicMock()
+    mock_level_1.__getitem__.side_effect = lambda key: {
+        config_dictionary.sun_key: mock_level_2
+    }[key]
+    mock_level_1.keys.return_value = [config_dictionary.sun_key]
+
+    mock_h5_file.__getitem__.side_effect = lambda key: {
+        config_dictionary.light_source_key: mock_level_1
+    }[key]
+
+    with pytest.raises(KeyError) as exc_info:
+        LightSourceArray.from_hdf5(
+            config_file=mock_h5_file,
+            device=device,
+        )
+    assert "Currently the selected light source: laser is not supported." in str(
+        exc_info.value
+    )
