06dba706deab3093501505893991d95dfd579c08
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-12-16 10:32:02 +0100

    add converter to test surface from points

--- tests/util/test_surface_converter.py
+++ tests/util/test_surface_converter.py
@@ -28,29 +28,50 @@ def test_surface_converter(device: torch.device) -> None:
     deflectometry_file_path = pathlib.Path(ARTIST_ROOT) / "tests/data/deflectometry.h5"
     stral_file_path = pathlib.Path(ARTIST_ROOT) / "tests/data/stral_test_data.binp"
 
-    surface_converter = SurfaceConverter(
+    surface_converter_normals = SurfaceConverter(
         step_size=5000,
         number_eval_points_e=10,
         number_eval_points_n=10,
+        conversion_method="deflectometry",
         number_control_points_e=5,
         number_control_points_n=5,
         max_epoch=1,
     )
 
-    surface_config_paint = surface_converter.generate_surface_config_from_paint(
+    surface_converter_points = SurfaceConverter(
+        step_size=5000,
+        number_eval_points_e=10,
+        number_eval_points_n=10,
+        conversion_method="point_cloud",
+        number_control_points_e=5,
+        number_control_points_n=5,
+        max_epoch=1,
+    )
+
+    surface_config_paint = surface_converter_normals.generate_surface_config_from_paint(
         heliostat_file_path=heliostat_file_path,
         deflectometry_file_path=deflectometry_file_path,
         device=device,
     )
 
-    surface_config_stral = surface_converter.generate_surface_config_from_stral(
+    surface_config_stral = surface_converter_normals.generate_surface_config_from_stral(
         stral_file_path=stral_file_path,
         device=device,
     )
+
+    surface_config_stral_points = (
+        surface_converter_points.generate_surface_config_from_stral(
+            stral_file_path=stral_file_path,
+            device=device,
+        )
+    )
+
     assert isinstance(surface_config_paint, list)
     assert isinstance(surface_config_stral, list)
+    assert isinstance(surface_config_stral_points, list)
     assert all(isinstance(obj, FacetConfig) for obj in surface_config_paint)
     assert all(isinstance(obj, FacetConfig) for obj in surface_config_stral)
+    assert all(isinstance(obj, FacetConfig) for obj in surface_config_stral_points)
 
 
 def test_fit_nurbs_conversion_method_error(device: torch.device) -> None:
