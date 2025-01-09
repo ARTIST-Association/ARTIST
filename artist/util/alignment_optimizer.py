d101997e6edded5b26155aaab2146821061f74af
MarleneBusch <marlene.busch@dlr.de>
2025-01-09 18:09:33 +0100

    adapt to distributed computing

--- artist/util/alignment_optimizer.py
+++ artist/util/alignment_optimizer.py
@@ -30,6 +30,14 @@ class AlignmentOptimizer:
         The optimizer.
     scheduler : Union[_LRScheduler, ReduceLROnPlateau]
         The learning rate scheduler.
+    world_size : int
+        The world size i.e., the overall number of processors / ranks (default: 1).
+    rank : int
+        The rank, i.e., individual process ID (default: 0).
+    batch_size : int
+        The batch size used for raytracing (default: 100).
+    is_distributed : bool
+        Distributed mode enabled (default: False).
 
     Methods
     -------
@@ -42,6 +50,10 @@ class AlignmentOptimizer:
         scenario: Scenario,
         optimizer: Optimizer,
         scheduler: Union[_LRScheduler, ReduceLROnPlateau],
+        world_size: int = 1,
+        rank: int = 0,
+        batch_size: int = 100,
+        is_distributed: bool = False,
     ) -> None:
         """
         Initialize the alignment optimizer.
@@ -54,11 +66,23 @@ class AlignmentOptimizer:
             The optimizer.
         scheduler : Union[_LRScheduler, ReduceLROnPlateau]
             The learning rate scheduler.
+        world_size : int
+            The world size i.e., the overall number of processors / ranks (default: 1).
+        rank : int
+            The rank, i.e., individual process ID (default: 0).
+        batch_size : int
+            The batch size used for raytracing (default: 100).
+        is_distributed : bool
+            Distributed mode enabled (default: False).
         """
         log.info("Create alignment optimizer.")
         self.scenario = scenario
         self.optimizer = optimizer
         self.scheduler = scheduler
+        self.world_size = world_size
+        self.rank = rank
+        self.batch_size = batch_size
+        self.is_distributed = is_distributed
 
     def optimize(
         self,
@@ -66,6 +90,7 @@ class AlignmentOptimizer:
         max_epoch: int,
         center_calibration_image: torch.Tensor,
         incident_ray_direction: torch.Tensor,
+        calibration_target_name: Optional[str] = None,
         motor_positions: Optional[torch.Tensor] = None,
         num_log: int = 3,
         device: Union[torch.device, str] = "cuda",
@@ -83,6 +108,8 @@ class AlignmentOptimizer:
             The center of the calibration flux density.
         incident_ray_direction : torch.Tensor
             The incident ray direction specified in the calibration.
+        calibration_target_name : Optional[str]
+            The name of the calibration target.
         motor_positions : Optional[torch.Tensor]
             The motor positions specified in the calibration (default is ``None``).
         num_log : int
@@ -112,11 +139,12 @@ class AlignmentOptimizer:
                     device=device,
                 )
             )
-        else:
+        elif calibration_target_name is not None:
             optimized_parameters, self.scenario = (
                 self._optimize_kinematic_parameters_with_raytracing(
                     tolerance=tolerance,
                     max_epoch=max_epoch,
+                    calibration_target_name=calibration_target_name,
                     center_calibration_image=center_calibration_image,
                     incident_ray_direction=incident_ray_direction,
                     num_log=num_log,
@@ -226,6 +254,7 @@ class AlignmentOptimizer:
         self,
         tolerance: float,
         max_epoch: int,
+        calibration_target_name: str,
         center_calibration_image: torch.Tensor,
         incident_ray_direction: torch.Tensor,
         num_log: int = 3,
@@ -244,6 +273,8 @@ class AlignmentOptimizer:
             The optimzer tolerance.
         max_epoch : int
             The maximum number of optimization epochs.
+        calibration_target_name : str
+            The name of the calibration target or tower area.
         center_calibration_image : torch.Tensor
             The center of the calibration flux density.
         incident_ray_direction : torch.Tensor
@@ -265,6 +296,27 @@ class AlignmentOptimizer:
         loss = torch.inf
         epoch = 0
 
+        # Since the default heliostat aim point and thereby kinematic aim point is in the receiver center, the aim point
+        # needs to be updated to the center of the calibration target.
+        calibration_target = next(
+            (
+                area
+                for area in self.scenario.target_areas.target_area_list
+                if area.name == calibration_target_name
+            ),
+            None,
+        )
+
+        if calibration_target is None:
+            raise KeyError(
+                "The specified calibration target is not included in the scenario!"
+            )
+
+        self.scenario.heliostats.heliostat_list[0].aim_point = calibration_target.center
+        self.scenario.heliostats.heliostat_list[
+            0
+        ].kinematic.aim_point = calibration_target.center
+
         log_step = max_epoch // num_log
         while loss > tolerance and epoch <= max_epoch:
             # Align heliostat.
@@ -274,20 +326,31 @@ class AlignmentOptimizer:
                 incident_ray_direction=incident_ray_direction, device=device
             )
 
-            # Create raytracer.
-            raytracer = HeliostatRayTracer(scenario=self.scenario)
+            # Create raytracer
+            raytracer = HeliostatRayTracer(
+                scenario=self.scenario,
+                aim_point_area=calibration_target_name,
+                world_size=self.world_size,
+                rank=self.rank,
+                batch_size=self.batch_size,
+            )
 
             final_bitmap = raytracer.trace_rays(
                 incident_ray_direction=incident_ray_direction, device=device
             )
 
+            if self.is_distributed:
+                final_bitmap = torch.distributed.all_reduce(
+                    final_bitmap, op=torch.distributed.ReduceOp.SUM
+                )
+
             final_bitmap = raytracer.normalize_bitmap(final_bitmap)
 
             center = utils.get_center_of_mass(
                 bitmap=torch.flip(final_bitmap, dims=(0, 1)),
-                target_center=self.scenario.receivers.receiver_list[0].position_center,
-                plane_e=self.scenario.receivers.receiver_list[0].plane_e,
-                plane_u=self.scenario.receivers.receiver_list[0].plane_u,
+                target_center=calibration_target.center,
+                plane_e=calibration_target.plane_e,
+                plane_u=calibration_target.plane_u,
                 device=device,
             )
 
