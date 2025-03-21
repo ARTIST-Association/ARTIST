4c4b24e7b90b72199aac357fb06b1c43f1f0e615
MarleneBusch <marlene.busch@dlr.de>
2025-03-21 16:13:44 +0100

    add pytest for value error in load kinematic from hdf5

--- tests/util/test_utils_load_h5.py
+++ tests/util/test_utils_load_h5.py
@@ -1,20 +1,42 @@
 import logging
-from unittest.mock import MagicMock
 
 import h5py
 import pytest
 import torch
-from pytest_mock import mocker
+from pytest_mock import MockerFixture
 
 from artist.util import config_dictionary, utils_load_h5
 
 
 @pytest.mark.parametrize(
-    "kinematic_type, expected",
-    [()],
+    "kinematic_type",
+    [(
+        "invalid_kinematic_type"
+    )],
 )
-def test_load_kinematic_deviations(kinematic_type: str, device: torch.device):
-    scenario_file = MagicMock(spec=h5py.File)
+def test_load_kinematic_deviations(
+        mocker: MockerFixture,
+        kinematic_type: str, 
+        device: torch.device
+    ):
+    """
+    Test errors raised when loading kinematic deviations from an hdf5 file.
+
+    Parameters
+    ----------
+    mocker : MockerFixture
+        A pytest-mocker fixture used to create mock objects.
+    kinematic_type : str
+        The kinematic type to be tested.
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Raises
+    ------
+    AssertionError
+        If test does not complete as expected.
+    """
+    scenario_file = mocker.MagicMock(spec=h5py.File)
 
     mock_level_1 = mocker.MagicMock()
 
@@ -22,7 +44,7 @@ def test_load_kinematic_deviations(kinematic_type: str, device: torch.device):
         config_dictionary.heliostat_kinematic_key: mock_level_1
     }[key]
 
-    log = MagicMock(spec=logging.Logger)
+    log = mocker.MagicMock(spec=logging.Logger)
 
     with pytest.raises(ValueError) as exc_info:
         utils_load_h5.kinematic_deviations(
