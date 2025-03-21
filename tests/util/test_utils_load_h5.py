54fa865e78671b788ad3d757ed40296a47898659
MarleneBusch <marlene.busch@dlr.de>
2025-03-21 16:50:46 +0100

    add further test for value error check

--- tests/util/test_utils_load_h5.py
+++ tests/util/test_utils_load_h5.py
@@ -58,3 +58,74 @@ def test_load_kinematic_deviations(
     assert f"The kinematic type: {kinematic_type} is not yet implemented!" in str(
         exc_info.value
     )
+
+
+
+@pytest.mark.parametrize(
+    "actuator_type, error_message",
+    [
+        (
+            "invalid_actuator_type",
+            "The actuator type: invalid_actuator_type is not yet implemented!"
+        ),
+        (
+            "linear",
+            "This scenario file contains the wrong amount of actuators for this heliostat and its kinematic type. Expected 2 actuators, found 1 actuator(s)."
+        )
+    ],
+)
+def test_load_actuator_parameters(
+        mocker: MockerFixture,
+        actuator_type: str, 
+        error_message: str,
+        device: torch.device
+    ):
+    """
+    Test errors raised when loading actuator parameters from an hdf5 file.
+
+    Parameters
+    ----------
+    mocker : MockerFixture
+        A pytest-mocker fixture used to create mock objects.
+    actuator_type : str
+        The actuator type to be tested.
+    error_message : str
+        The expected error message.
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Raises
+    ------
+    AssertionError
+        If test does not complete as expected.
+    """
+    scenario_file = mocker.MagicMock(spec=h5py.File)
+
+    mock_level_2 = mocker.MagicMock()
+
+    mock_level_1 = mocker.MagicMock()
+    mock_level_1.__getitem__.side_effect = lambda key: {
+        config_dictionary.sun_key: mock_level_2
+    }[key]
+    mock_level_1.keys.return_value = [config_dictionary.sun_key]
+
+    scenario_file.__getitem__.side_effect = lambda key: {
+        config_dictionary.heliostat_actuator_key: mock_level_1
+    }[key]
+
+    log = mocker.MagicMock(spec=logging.Logger)
+
+    with pytest.raises(ValueError) as exc_info:
+        utils_load_h5.actuator_parameters(
+            prototype=False,
+            actuator_type=actuator_type,
+            scenario_file=scenario_file,
+            number_of_actuators=2,
+            initial_orientation=torch.tensor([0.0, -1.0, 0.0, 0.0]),
+            log=log,
+            device=device,
+        )
+    assert error_message in str(
+        exc_info.value
+    )
+
