5aa93357cd2a77b2f27d2d24dd3381ffdae216fe
MarleneBusch <marlene.busch@dlr.de>
2025-05-19 16:20:30 +0200

    add test for target area curvature

--- tests/field/test_load_target_area.py
+++ tests/field/test_load_target_area.py
@@ -1,4 +1,5 @@
 import h5py
+import pytest
 import torch
 from pytest_mock import MockerFixture
 
@@ -6,8 +7,10 @@ from artist.field.tower_target_areas import TowerTargetAreas
 from artist.util import config_dictionary
 
 
+@pytest.mark.parametrize("curvature", [True, False])
 def test_target_area_from_hdf5(
     mocker: MockerFixture,
+    curvature: bool,
     device: torch.device,
 ) -> None:
     """
@@ -17,6 +20,8 @@ def test_target_area_from_hdf5(
     ----------
     mocker : MockerFixture
         A pytest-mocker fixture used to create mock objects.
+    curvature : bool
+        Target area has a curvature or not.
     device : torch.device
         The device on which to initialize tensors.
 
@@ -50,15 +55,24 @@ def test_target_area_from_hdf5(
     mock_curvature_u.__getitem__.return_value = 2.0
 
     mock_level_receiver = mocker.MagicMock()
-    mock_level_receiver.__getitem__.side_effect = lambda key: {
-        config_dictionary.target_area_geometry: mock_geometry,
-        config_dictionary.target_area_position_center: mock_center,
-        config_dictionary.target_area_normal_vector: mock_normal,
-        config_dictionary.target_area_plane_e: mock_plane_e,
-        config_dictionary.target_area_plane_u: mock_plane_u,
-        config_dictionary.target_area_curvature_e: mock_curvature_e,
-        config_dictionary.target_area_curvature_u: mock_curvature_u,
-    }[key]
+    if curvature:
+        mock_level_receiver.__getitem__.side_effect = lambda key: {
+            config_dictionary.target_area_geometry: mock_geometry,
+            config_dictionary.target_area_position_center: mock_center,
+            config_dictionary.target_area_normal_vector: mock_normal,
+            config_dictionary.target_area_plane_e: mock_plane_e,
+            config_dictionary.target_area_plane_u: mock_plane_u,
+            config_dictionary.target_area_curvature_e: mock_curvature_e,
+            config_dictionary.target_area_curvature_u: mock_curvature_u,
+        }[key]
+    else:
+        mock_level_receiver.__getitem__.side_effect = lambda key: {
+            config_dictionary.target_area_geometry: mock_geometry,
+            config_dictionary.target_area_position_center: mock_center,
+            config_dictionary.target_area_normal_vector: mock_normal,
+            config_dictionary.target_area_plane_e: mock_plane_e,
+            config_dictionary.target_area_plane_u: mock_plane_u,
+        }[key]
 
     mock_level_target_areas = mocker.MagicMock()
     mock_level_target_areas.__getitem__.side_effect = lambda key: {
