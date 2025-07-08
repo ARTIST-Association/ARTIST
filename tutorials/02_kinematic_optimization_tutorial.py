97c4d8e135195ac3afda41d668a8991c1b76f098
MarleneBusch <marlene.busch@dlr.de>
2025-07-08 16:42:59 +0200

    add tutorial

--- tutorials/02_kinematic_optimization_tutorial.py
+++ tutorials/02_kinematic_optimization_tutorial.py
@@ -3,11 +3,12 @@ import pathlib
 import h5py
 import torch
 
+from artist.core.heliostat_ray_tracer import HeliostatRayTracer
 from artist.core.kinematic_optimizer import KinematicOptimizer
 from artist.data_loader import paint_loader
 from artist.scenario.scenario import Scenario
 from artist.util import set_logger_config
-from artist.util.environment_setup import get_device
+from artist.util.environment_setup import get_device, setup_distributed_environment
 
 torch.manual_seed(7)
 torch.cuda.manual_seed(7)
@@ -20,98 +21,238 @@ device = get_device()
 
 # Specify the path to your scenario.h5 file.
 scenario_path = pathlib.Path(
-    "please/insert/the/path/to/the/scenario/here/test_scenario_paint_four_heliostats.h5"
+    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/scenarios/test_scenario_paint_multiple_heliostat_groups.h5"
+)
+number_of_heliostat_groups = Scenario.get_number_of_heliostat_groups_from_hdf5(
+    scenario_path=scenario_path
 )
 
-# Also specify the heliostats to be calibrated and the paths to your calibration-properties.json files.
-# Please follow the following style: list[tuple[str, list[pathlib.Path]]]
-heliostat_calibration_mapping = [
-    (
-        "first_heliostat",
-        [
-            pathlib.Path(
-                "please/insert/the/path/to/the/paint/data/here/calibration-properties.json"
-            ),
-            # pathlib.Path(
-            #     "please/insert/the/path/to/the/paint/data/here/calibration-properties.json"
-            # ),
-            # ....
-        ],
-    ),
-    (
-        "second_heliostat",
-        [
-            pathlib.Path(
-                "please/insert/the/path/to/the/paint/data/here/calibration-properties.json"
-            ),
-            # pathlib.Path(
-            #      "please/insert/the/path/to/the/paint/data/here/calibration-properties.json"
-            # ),
-        ],
-    ),
-    # ...
-]
+with setup_distributed_environment(
+    number_of_heliostat_groups=number_of_heliostat_groups,
+    device=device,
+) as (
+    device,
+    is_distributed,
+    is_nested,
+    rank,
+    world_size,
+    process_subgroup,
+    groups_to_ranks_mapping,
+    heliostat_group_rank,
+    heliostat_group_world_size,
+    ):
 
-# Load the scenario.
-with h5py.File(scenario_path, "r") as scenario_file:
-    scenario = Scenario.load_scenario_from_hdf5(
-        scenario_file=scenario_file, device=device
-    )
+    # Also specify the heliostats to be calibrated and the paths to your calibration-properties.json files.
+    # Please follow the following style: list[tuple[str, list[pathlib.Path]]]
+    heliostat_calibration_mapping = [
+        (
+            "AA39",
+            [
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/202558-calibration-properties.json"
+                ),
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/209075-calibration-properties.json"
+                ),
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/218385-calibration-properties.json"
+                ),
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/246955-calibration-properties.json"
+                ),
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/275564-calibration-properties.json"
+                ),
+                # pathlib.Path(
+                #     "please/insert/the/path/to/the/paint/data/here/calibration-properties.json"
+                # ),
+                # ....
+            ],
+        ),
+        (
+            "AA31",
+            [
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA31/125284-calibration-properties.json"
+                ),
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA31/126372-calibration-properties.json"
+                ),
+                pathlib.Path(
+                    "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA31/219988-calibration-properties.json"
+                ),
+                # pathlib.Path(
+                #      "please/insert/the/path/to/the/paint/data/here/calibration-properties.json"
+                # ),
+            ],
+        ),
+        # ...
+    ]
 
-for heliostat_group_index, heliostat_group in enumerate(
-    scenario.heliostat_field.heliostat_groups
-):
-    # Load the calibration data.
-    (
-        focal_spots_calibration,
-        incident_ray_directions_calibration,
-        motor_positions_calibration,
-        heliostats_mask_calibration,
-        target_area_mask_calibration,
-    ) = paint_loader.extract_paint_calibration_data(
-        heliostat_calibration_mapping=[
-            (heliostat_name, paths)
-            for heliostat_name, paths in heliostat_calibration_mapping
-            if heliostat_name in heliostat_group.names
-        ],
-        heliostat_names=heliostat_group.names,
-        target_area_names=scenario.target_areas.names,
-        power_plant_position=scenario.power_plant_position,
+    # Load the scenario.
+    with h5py.File(scenario_path, "r") as scenario_file:
+        scenario = Scenario.load_scenario_from_hdf5(
+            scenario_file=scenario_file, device=device
+        )
+
+    scenario.light_sources.light_source_list[0].number_of_rays = 100
+
+    for heliostat_group_index, heliostat_group in enumerate(
+        scenario.heliostat_field.heliostat_groups
+    ):
+        # Load the calibration data.
+        (
+            focal_spots_calibration,
+            incident_ray_directions_calibration,
+            motor_positions_calibration,
+            heliostats_mask_calibration,
+            target_area_mask_calibration,
+        ) = paint_loader.extract_paint_calibration_data(
+            heliostat_calibration_mapping=[
+                (heliostat_name, paths)
+                for heliostat_name, paths in heliostat_calibration_mapping
+                if heliostat_name in heliostat_group.names
+            ],
+            heliostat_names=heliostat_group.names,
+            target_area_names=scenario.target_areas.names,
+            power_plant_position=scenario.power_plant_position,
+            device=device,
+        )
+        if heliostats_mask_calibration.sum() > 0:
+            # Set up optimizer and scheduler.
+            tolerance = 0.0005
+            max_epoch = 1000
+            initial_learning_rate = 0.0001
+
+            use_ray_tracing = True
+            if use_ray_tracing:
+                motor_positions_calibration = None
+                tolerance = 0.035
+                max_epoch = 600
+                initial_learning_rate = 0.0004
+
+            optimizer = torch.optim.Adam(
+                heliostat_group.kinematic.parameters(), lr=initial_learning_rate
+            )
+
+            # Create the kinematic optimizer.
+            kinematic_optimizer = KinematicOptimizer(
+                scenario=scenario,
+                heliostat_group=heliostat_group,
+                optimizer=optimizer,
+            )
+
+            # Calibrate the kinematic.
+            kinematic_optimizer.optimize(
+                focal_spots_calibration=focal_spots_calibration,
+                incident_ray_directions=incident_ray_directions_calibration,
+                active_heliostats_mask=heliostats_mask_calibration,
+                target_area_mask_calibration=target_area_mask_calibration,
+                motor_positions_calibration=motor_positions_calibration,
+                tolerance=tolerance,
+                max_epoch=max_epoch,
+                num_log=max_epoch,
+                device=device,
+            )
+
+    heliostat_target_light_source_mapping = [
+        ("AA31", "solar_tower_juelich_upper", torch.tensor([ 0.6083,  0.2826, -0.7417,  0.0000], device=device)),
+        ("AA31", "solar_tower_juelich_lower", torch.tensor([-0.5491,  0.3197, -0.7722,  0.0000] ,device=device)),
+        ("AA31", "solar_tower_juelich_lower", torch.tensor([-0.6905,  0.2223, -0.6884,  0.0000], device=device)),
+        ("AA39", "multi_focus_tower", torch.tensor([-0.6568,  0.3541, -0.6658,  0.0000] ,device=device)),
+        ("AA39", "solar_tower_juelich_lower", torch.tensor([-0.0947,  0.4929, -0.8649,  0.0000] ,device=device)),
+        ("AA39", "multi_focus_tower", torch.tensor([ 0.0619,  0.4641, -0.8836,  0.0000] ,device=device)),
+        ("AA39", "solar_tower_juelich_lower", torch.tensor([-0.5211,  0.5949, -0.6121,  0.0000], device=device)),
+        ("AA39", "multi_focus_tower", torch.tensor([-0.2741,  0.4399, -0.8552,  0.0000] ,device=device)),
+    ]
+
+    bitmap_resolution_e = 256
+    bitmap_resolution_u = 256
+    combined_bitmaps_per_target = torch.zeros(
+        (
+            scenario.target_areas.number_of_target_areas,
+            bitmap_resolution_e,
+            bitmap_resolution_u,
+        ),
         device=device,
     )
 
-    # Set up optimizer and scheduler.
-    tolerance = 0.0005
-    max_epoch = 1000
-    initial_learning_rate = 0.0001
+    for group_index in groups_to_ranks_mapping[rank]:
+        heliostat_group = scenario.heliostat_field.heliostat_groups[group_index]
 
-    use_ray_tracing = False
-    if use_ray_tracing:
-        motor_positions_calibration = None
-        tolerance = 0.035
-        max_epoch = 1000
-        initial_learning_rate = 0.0005
+        # If no mapping from heliostats to target areas to incident ray direction is provided, the scenario.index_mapping() method
+        # activates all heliostats. It is possible to then provide a default target area index and a default incident ray direction
+        # if those are not specified either all heliostats are assigned to the first target area found in the scenario with an
+        # incident ray direction "north" (meaning the light source position is directly in the south) for all heliostats.
+        (
+            active_heliostats_mask,
+            target_area_mask,
+            incident_ray_directions,
+        ) = scenario.index_mapping(
+            heliostat_group=heliostat_group,
+            string_mapping=heliostat_target_light_source_mapping,
+            device=device,
+        )
 
-    optimizer = torch.optim.Adam(
-        heliostat_group.kinematic.parameters(), lr=initial_learning_rate
-    )
+        # Align heliostats.
+        heliostat_group.align_surfaces_with_incident_ray_directions(
+            aim_points=scenario.target_areas.centers[target_area_mask],
+            incident_ray_directions=incident_ray_directions,
+            active_heliostats_mask=active_heliostats_mask,
+            device=device,
+        )
 
-    # Create the kinematic optimizer.
-    kinematic_optimizer = KinematicOptimizer(
-        scenario=scenario,
-        heliostat_group=heliostat_group,
-        optimizer=optimizer,
-    )
+        # Create a ray tracer.
+        ray_tracer = HeliostatRayTracer(
+            scenario=scenario,
+            heliostat_group=heliostat_group,
+            world_size=heliostat_group_world_size,
+            rank=heliostat_group_rank,
+            batch_size=4,
+            random_seed=heliostat_group_rank,
+            bitmap_resolution_e=bitmap_resolution_e,
+            bitmap_resolution_u=bitmap_resolution_u,
+        )
 
-    # Calibrate the kinematic.
-    kinematic_optimizer.optimize(
-        focal_spots_calibration=focal_spots_calibration,
-        incident_ray_directions=incident_ray_directions_calibration,
-        active_heliostats_mask=heliostats_mask_calibration,
-        target_area_mask_calibration=target_area_mask_calibration,
-        motor_positions_calibration=motor_positions_calibration,
-        tolerance=tolerance,
-        max_epoch=max_epoch,
-        num_log=max_epoch,
-        device=device,
-    )
+        # Perform heliostat-based ray tracing.
+        bitmaps_per_heliostat = ray_tracer.trace_rays(
+            incident_ray_directions=incident_ray_directions,
+            active_heliostats_mask=active_heliostats_mask,
+            target_area_mask=target_area_mask,
+            device=device,
+        )
+
+        import matplotlib.pyplot as plt
+        for i in range(bitmaps_per_heliostat.shape[0]):
+            fig, ax = plt.subplots()
+            ax.imshow(bitmaps_per_heliostat[i].cpu().detach(), cmap="gray")
+            ax.axis('off')
+            ax.margins(0)
+            ax.set_position([0, 0, 1, 1])
+            plt.savefig(f"heliostat_{i}_artist.png", bbox_inches='tight', pad_inches=0)
+        
+        torch.save(bitmaps_per_heliostat.cpu().detach(), "heliostat_bitmaps.pt")
+
+
+        bitmaps_per_target = ray_tracer.get_bitmaps_per_target(
+            bitmaps_per_heliostat=bitmaps_per_heliostat,
+            target_area_mask=target_area_mask,
+            device=device,
+        )
+
+        combined_bitmaps_per_target = combined_bitmaps_per_target + bitmaps_per_target
+    
+
+    if is_nested:
+        torch.distributed.all_reduce(
+            combined_bitmaps_per_target,
+            op=torch.distributed.ReduceOp.SUM,
+            group=process_subgroup,
+        )
+
+    if is_distributed:
+        torch.distributed.all_reduce(
+            combined_bitmaps_per_target, op=torch.distributed.ReduceOp.SUM
+        )
+
+
