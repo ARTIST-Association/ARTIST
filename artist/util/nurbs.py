9fc724258843e7ffd50d440b09df63e9db6e2e9c
MarleneBusch <marlene.busch@dlr.de>
2025-07-18 11:40:00 +0200

    fix docstrings and naming

--- artist/util/nurbs.py
+++ artist/util/nurbs.py
@@ -1,12 +1,15 @@
 import torch
 
-from artist.util import type_mappings
+from artist.util import config_dictionary
 from artist.util.environment_setup import get_device
 
 
 class NURBSSurfaces(torch.nn.Module):
     """
-    Implement differentiable NURBS for the heliostat surface.
+    Implement differentiable NURBS for surface representations.
+    
+    This implementation can be used to create multiple seperate NURBS surfaces at the same time
+    and they are handled in parallel.
 
     Attributes
     ----------
@@ -14,23 +17,23 @@ class NURBSSurfaces(torch.nn.Module):
         The spline degrees.
     control_points : torch.Tensor
         The control points.
-    knot_vector_e : torch.Tensor
-        The knot vector in east direction.
-    knot_vector_n : torch.Tensor
-        The knot vector in north direction.
+    knot_vectors_u : torch.Tensor
+        The knot vectors in the u direction.
+    knot_vectors_v : torch.Tensor
+        The knot vectors in the v direction.
 
     Methods
     -------
     calculate_knot_vector()
-        Calculate the knot vector in one dimension.
+        Calculate the knot vectors in one direction.
     find_span()
-        Determine the knot span index in one dimension.
-    basis_function_and_derivatives()
+        Determine the knot spans in one direction.
+    basis_functions_and_derivatives()
         Compute the nonzero derivatives of the basis functions up to the nth-derivative.
     calculate_surface_points_and_normals()
-        Calculate the surface points and normals of the NURBS surface.
+        Calculate the surface points and normals of the NURBS surfaces.
     forward()
-        Specify the forward operation of the actuator, i.e. caluclate the surface points and normals.
+        Specify the forward operation of the NURBS, i.e. caluclate the surface points and normals.
     """
 
     def __init__(
@@ -41,7 +44,7 @@ class NURBSSurfaces(torch.nn.Module):
         device: torch.device | None = None,
     ) -> None:
         """
-        Initialize a NURBS surface.
+        Initialize NURBS surfaces.
 
         NURBS stands for Non-Uniform Rational B-Spline. NURBS allow for an efficient and precise reconstruction
         of the imperfect heliostat surfaces in the digital twin. This implementation of the NURBS is
@@ -54,6 +57,8 @@ class NURBSSurfaces(torch.nn.Module):
             The spline degrees in east and north directions.
         control_points : torch.Tensor
             The control points.
+        uniform : bool
+            Indicates wether the NURBS are uniform or not (default is True (uniform)).
         device : torch.device | None
             The device on which to perform computations or load tensors and models (default is None).
             If None, ARTIST will automatically select the most appropriate
@@ -64,23 +69,20 @@ class NURBSSurfaces(torch.nn.Module):
         device = get_device(device=device)
 
         self.degrees = degrees
-        self.control_points = torch.nn.Parameter(control_points)
+        self.control_points = control_points
         self.uniform = uniform
         self.number_of_surfaces = self.control_points.shape[0]
         self.number_of_facets_per_surface = self.control_points.shape[1]
-        # self.number_of_control_points_u = self.control_points.shape[2]
-        # self.number_of_control_points_v = self.control_points.shape[3]
-        self.knot_vectors_u = self.calculate_knot_vectors(dimension="u", uniform=self.uniform, device=device)
-        self.knot_vectors_v = self.calculate_knot_vectors(dimension="v", uniform=self.uniform, device=device)
+        self.knot_vectors_u = self.calculate_uniform_knot_vectors(direction="u", uniform=self.uniform, device=device)
+        self.knot_vectors_v = self.calculate_uniform_knot_vectors(direction="v", uniform=self.uniform, device=device)
 
-    def calculate_knot_vectors(
+    def calculate_uniform_knot_vectors(
         self,
-        dimension: int,
-        uniform: bool = True,
+        direction: int,
         device: torch.device | None = None,
     ) -> torch.Tensor | None:
         """
-        Calculate the knot vectors for all surfaces in one dimension.
+        Calculate the knot vectors for all surfaces in one direction.
 
         For our application, only uniform knot vectors are required.
         The knots range from zero to one and are distributed uniformly.
@@ -90,68 +92,8 @@ class NURBSSurfaces(torch.nn.Module):
 
         Parameters
         ----------
-        dimension : int
-            The NURBS dimension.
-        device : torch.device | None
-            The device on which to perform computations or load tensors and models (default is None).
-            If None, ARTIST will automatically select the most appropriate
-            device (CUDA or CPU) based on availability and OS.
-
-        Returns
-        -------
-        torch.Tensor | None
-            The knot vectors.
-        """
-        device = get_device(device=device)
-
-        dimension=type_mappings.nurbs_dimension_mapping[dimension]
-        degree = self.degrees[dimension].item()
-
-        if uniform:
-            knot_vector = torch.zeros(
-                (
-                    self.control_points.shape[2 + dimension] + degree + 1
-                ),
-                device=device,
-            )
-            number_of_knot_values = knot_vector[degree : -degree].shape[0]
-            knot_vector[:degree] = 0
-            knot_vector[degree:-degree] = (
-                torch.linspace(0, 1, number_of_knot_values, device=device)
-            )
-            knot_vector[-degree:] = 1
-
-            knot_vectors = knot_vector.unsqueeze(0).repeat(self.number_of_surfaces, self.number_of_facets_per_surface, 1)
-
-        else:
-            raise ValueError("Non uniform knots not implemented.")
-
-        return knot_vectors
-
-    def find_spans(
-        self,
-        dimension: int,
-        evaluation_points: torch.Tensor,
-        knot_vectors: torch.Tensor,
-        device: torch.device | None = None,
-    ) -> torch.Tensor:
-        """
-        Determine the knot span index in one dimension.
-
-        To generate NURBS, the basis functions must be evaluated. However, some basis functions may be zero. To improve
-        computational efficiency, basis functions that are zero are not computed. Therefore, the knot span in which the
-        evaluation point lies is first computed using this function.
-        See `The NURBS Book` p. 68 for reference.
-        If the knot vector is uniform, the span indices can be computed more efficiently.
-
-        Parameters
-        ----------
-        dimension : int
-            The NURBS dimension.
-        evaluation_points : torch.Tensor
-            The evaluation points.
-        knot_vector : torch.Tensor
-            The knot vector for the NURBS surface in a single direction.
+        direction : int
+            The NURBS surface direction u or v.
         device : torch.device | None
             The device on which to perform computations or load tensors and models (default is None).
             If None, ARTIST will automatically select the most appropriate
@@ -160,19 +102,73 @@ class NURBSSurfaces(torch.nn.Module):
         Returns
         -------
         torch.Tensor
-            The knot span index.
+            The knot vectors.
         """
         device = get_device(device=device)
 
-        dimension=type_mappings.nurbs_dimension_mapping[dimension]
-        degree = self.degrees[dimension].item()
+        degree = self.degrees[direction].item()
+
+        knot_vector = torch.zeros(
+            (
+                self.control_points.shape[2 + direction] + degree + 1
+            ),
+            device=device,
+        )
+        number_of_knot_values = knot_vector[degree : -degree].shape[0]
+        knot_vector[:degree] = 0
+        knot_vector[degree:-degree] = (
+            torch.linspace(0, 1, number_of_knot_values, device=device)
+        )
+        knot_vector[-degree:] = 1
+
+        knot_vectors = knot_vector.unsqueeze(0).repeat(self.number_of_surfaces, self.number_of_facets_per_surface, 1)
+
+        return knot_vectors
+
+    def find_spans(
+        self,
+        direction: int,
+        evaluation_points: torch.Tensor,
+        knot_vectors: torch.Tensor,
+        device: torch.device | None = None,
+    ) -> torch.Tensor:
+        """
+        Determine the knot spans in one direction.
+
+        To generate NURBS, the basis functions must be evaluated. However, some basis functions may be zero. To improve
+        computational efficiency, basis functions that are zero are not computed. Therefore, the knot spans in which the
+        evaluation point lies is first computed using this function.
+        See `The NURBS Book` p. 68 for reference.
+        If the knot vector is uniform, the spans can be computed more efficiently.
+
+        Parameters
+        ----------
+        direction : int
+            The NURBS surface direction u or v.
+        evaluation_points : torch.Tensor
+            The evaluation points.
+        knot_vectors : torch.Tensor
+            The knot vector for the NURBS surfaces in a single direction.
+        device : torch.device | None
+            The device on which to perform computations or load tensors and models (default is None).
+            If None, ARTIST will automatically select the most appropriate
+            device (CUDA or CPU) based on availability and OS.
+
+        Returns
+        -------
+        torch.Tensor
+            The knot spans.
+        """
+        device = get_device(device=device)
+
+        degree = self.degrees[direction].item()
 
         if self.uniform:
             unique_knots = torch.unique(knot_vectors, dim=-1)
 
-            span_indices = (
-                torch.floor(evaluation_points[:, :, :, dimension] * (unique_knots.shape[2] - 1)).long()
-                + self.degrees[dimension]
+            spans = (
+                torch.floor(evaluation_points[:, :, :, direction] * (unique_knots.shape[2] - 1)).long()
+                + self.degrees[direction]
             )
         
         else:
@@ -189,14 +185,14 @@ class NURBSSurfaces(torch.nn.Module):
 
             in_span = (evaluation_points[:, :, :, 0].unsqueeze(-1) >= lefts.unsqueeze(2)) & (evaluation_points[:, :, :, 0].unsqueeze(-1) < rights.unsqueeze(2))
             is_last_knot = torch.isclose(evaluation_points[:, :, :, 0], knot_vectors[:, :, number_of_knots], atol=1e-5, rtol=1e-5)
-            span_indices = torch.argmax(in_span.to(torch.int), dim=-1) + degree
-            span_indices = torch.where(is_last_knot, torch.full_like(span_indices, number_of_knots - 1), span_indices)
+            spans = torch.argmax(in_span.to(torch.int), dim=-1) + degree
+            spans = torch.where(is_last_knot, torch.full_like(spans, number_of_knots - 1), spans)
 
-        return span_indices
+        return spans
 
     def basis_functions_and_derivatives(
         self,
-        dimension: str,
+        direction: int,
         evaluation_points: torch.Tensor,
         knot_vectors: torch.Tensor,
         spans: torch.Tensor,
@@ -210,14 +206,14 @@ class NURBSSurfaces(torch.nn.Module):
 
         Parameters
         ----------
-        degree : int
-            The NURBS degree.
+        direction : int
+            The NURBS surface direction u or v.
         evaluation_points : torch.Tensor
             The evaluation points.
-        knot_vector : torch.Tensor
-            Contains all the knots of the NURBS model.
-        span : torch.Tensor
-            The span indices.
+        knot_vectors : torch.Tensor
+            Contains all the knots of the NURBS surfaces.
+        spans : torch.Tensor
+            The knot spans.
         nth_derivative : int
             Specifies how many derivatives are calculated (default is 1).
         device : torch.device | None
@@ -228,14 +224,13 @@ class NURBSSurfaces(torch.nn.Module):
         Returns
         -------
         torch.Tensor
-            The derivatives of the basis function.
+            The derivatives of the basis functions.
         """
         device = get_device(device=device)
 
-        dimension = type_mappings.nurbs_dimension_mapping[dimension]
-        degree = self.degrees[dimension].item()
+        degree = self.degrees[direction].item()
 
-        evaluation_points = evaluation_points[:, :, :, dimension]
+        evaluation_points = evaluation_points[:, :, :, direction]
         num_evaluation_points = evaluation_points.shape[2]
 
         # Introduce `ndu` to store the basis functions (called "n" in The NURBS book) and the knot differences (du).
@@ -311,40 +306,47 @@ class NURBSSurfaces(torch.nn.Module):
         
         return derivatives
 
-    def batched_gather_control_points(self, control_points: torch.Tensor, index_u: torch.Tensor, index_v: torch.Tensor, device: torch.device | None = None) -> torch.Tensor:
+    def _batched_gather_control_points(
+        self, 
+        control_points: torch.Tensor, 
+        index_u: torch.Tensor, 
+        index_v: torch.Tensor, 
+        device: torch.device | None = None
+    ) -> torch.Tensor:
         """
         Gathers control points using batched 2D indices.
 
         Parameters
         ----------
         control_points : torch.Tensor
-            Tensor of shape [B, F, Nu, Nv, D]
-        idx_u : torch.Tensor
-            Indices in the u-direction [B, F, E]
-        idx_v : torch.Tensor
-            Indices in the v-direction [B, F, E]
+            The control points.
+        index_u : torch.Tensor
+            The indices in u direction.
+        index_v : torch.Tensor
+            The indices in v direction.
+        device : torch.device | None
+            The device on which to perform computations or load tensors and models (default is None).
+            If None, ARTIST will automatically select the most appropriate
+            device (CUDA or CPU) based on availability and OS.
 
         Returns
         -------
         torch.Tensor
-            Gathered control points of shape [B, F, E, D]
+            The gathered control points.
         """
         device = get_device(device=device)
-        number_of_evaluation_points = index_u.shape[2]
 
-        # Build batch and facet indices
         batch_index = torch.arange(
             self.number_of_surfaces, device=device
         ).view(self.number_of_surfaces, 1, 1).expand(
-            self.number_of_surfaces, self.number_of_facets_per_surface, number_of_evaluation_points
+            self.number_of_surfaces, self.number_of_facets_per_surface, index_u.shape[2]
         )
         facet_index = torch.arange(
             self.number_of_facets_per_surface, device=device
         ).view(1, self.number_of_facets_per_surface, 1).expand(
-            self.number_of_surfaces, self.number_of_facets_per_surface, number_of_evaluation_points
+            self.number_of_surfaces, self.number_of_facets_per_surface, index_u.shape[2]
         )
 
-        # Advanced indexing
         gathered = control_points[batch_index, facet_index, index_u, index_v]
         return gathered
 
@@ -355,7 +357,7 @@ class NURBSSurfaces(torch.nn.Module):
         device: torch.device | None = None,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         """
-        Calculate the surface points and normals of the NURBS surface.
+        Calculate the surface points and normals of the NURBS surfaces.
 
         Parameters
         ----------
@@ -377,17 +379,17 @@ class NURBSSurfaces(torch.nn.Module):
 
         nth_derivative = 1
 
-        # Find span indices e direction (based on A2.1, p. 68).
-        span_indices_u = self.find_spans(
-            dimension="u",
+        # Find the spans in u direction (based on A2.1, p. 68).
+        spans_u = self.find_spans(
+            direction=config_dictionary.nurbs_u_direction,
             evaluation_points=evaluation_points,
             knot_vectors=self.knot_vectors_u,
             device=device,
         )
 
-        # Find span indices n direction (based on A2.1, p. 68).
-        span_indices_v = self.find_spans(
-            dimension="v",
+        # Find the spans in v direction (based on A2.1, p. 68).
+        spans_v = self.find_spans(
+            direction=config_dictionary.nurbs_v_direction,
             evaluation_points=evaluation_points,
             knot_vectors=self.knot_vectors_v,
             device=device,
@@ -428,18 +430,18 @@ class NURBSSurfaces(torch.nn.Module):
 
         # Find derivatives of basis functions (based on A2.3, p. 72).
         basis_values_derivatives_u = self.basis_functions_and_derivatives(
-            dimension="u",
+            direction=config_dictionary.nurbs_u_direction,
             evaluation_points=evaluation_points,
             knot_vectors=self.knot_vectors_u,
-            spans=span_indices_u,
+            spans=spans_u,
             nth_derivative=du,
             device=device,
         )
         basis_values_derivatives_v = self.basis_functions_and_derivatives(
-            dimension="v",
+            direction=config_dictionary.nurbs_v_direction,
             evaluation_points=evaluation_points,
             knot_vectors=self.knot_vectors_v,
-            spans=span_indices_v,
+            spans=spans_v,
             nth_derivative=dv,
             device=device,
         )
@@ -458,9 +460,9 @@ class NURBSSurfaces(torch.nn.Module):
                 temp[s] = torch.zeros_like(temp[s], device=device)
                 for r in range(self.degrees[0] + 1):
                     bu = basis_values_derivatives_u[k][r].unsqueeze(-1)
-                    index_u = span_indices_u - self.degrees[0] + r
-                    index_v = span_indices_v - self.degrees[1] + s
-                    gathered_control_points = self.batched_gather_control_points(
+                    index_u = spans_u - self.degrees[0] + r
+                    index_v = spans_v - self.degrees[1] + s
+                    gathered_control_points = self._batched_gather_control_points(
                         control_points=control_points, 
                         index_u=index_u, 
                         index_v=index_v,
@@ -486,10 +488,12 @@ class NURBSSurfaces(torch.nn.Module):
         return derivatives[:, :, :, 0, 0], normals
 
     def forward(
-        self, evaluation_points: torch.Tensor, device: torch.device | None = None
+        self, 
+        evaluation_points: torch.Tensor, 
+        device: torch.device | None = None
     ) -> tuple[torch.Tensor, torch.Tensor]:
         """
-        Specify the forward operation of the actuator, i.e. caluclate the surface points and normals.
+        Specify the forward operation of the NURBS, i.e. caluclate the surface points and normals.
 
         Parameters
         ----------
