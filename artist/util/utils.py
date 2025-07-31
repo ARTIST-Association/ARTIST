fix bitmap normalization method by scaling the values

artist/util/utils.py
@@ -625,12 +625,16 @@ def normalize_bitmaps(
     Returns
     -------
     torch.Tensor
-        The normalized flux density distributions.
+        The normalized and scaled flux density distributions.
     """
     plane_areas = target_area_widths * target_area_heights
     num_pixels = flux_distributions.shape[1] * flux_distributions.shape[2]
     plane_area_per_pixel = plane_areas / num_pixels
 
-    return flux_distributions / (number_of_rays * plane_area_per_pixel).unsqueeze(
+    normalized_fluxes = flux_distributions / (number_of_rays * plane_area_per_pixel).unsqueeze(
         -1
     ).unsqueeze(-1)
+    
+    scaled_fluxes = ((normalized_fluxes - torch.mean(normalized_fluxes, dim=(1,2), keepdim=True)) / torch.std(normalized_fluxes, dim=(1,2), keepdim=True))
+
+    return scaled_fluxes