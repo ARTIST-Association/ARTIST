06dcbe539fa31e9baa6a032678cb0f654c9b4128
MarleneBusch <marlene.busch@dlr.de>
2025-08-14 13:25:15 +0200

    ruff formatting

--- tests/core/test_surface_reconstructor.py
+++ tests/core/test_surface_reconstructor.py
@@ -105,3 +105,136 @@ def test_surface_reconstructor(
         atol=5e-2,
         rtol=5e-2,
     )
+
+
+def test_fixate_control_points_on_outer_edges(
+    device: torch.device,
+) -> None:
+    """
+    Test the outer control points fixation function.
+
+    Parameters
+    ----------
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Raises
+    ------
+    AssertionError
+        If test does not complete as expected.
+    """
+    test_gradients = torch.zeros((1, 1, 4, 4, 3), device=device)
+    origin_offsets_e = torch.linspace(-5, 5, test_gradients.shape[2], device=device)
+    origin_offsets_n = torch.linspace(-5, 5, test_gradients.shape[3], device=device)
+
+    test_gradients_e, test_gradients_n = torch.meshgrid(
+        origin_offsets_e, origin_offsets_n, indexing="ij"
+    )
+
+    test_gradients[:, :, :, :, 0] = test_gradients_e
+    test_gradients[:, :, :, :, 1] = test_gradients_n
+    test_gradients[:, :, :, :, 2] = torch.full_like(test_gradients_e, 5, device=device)
+
+    fixed_gradients = SurfaceReconstructor.fixate_control_points_on_outer_edges(
+        gradients=test_gradients, device=device
+    )
+
+    expected_gradients = torch.tensor(
+        [
+            [
+                [
+                    [
+                        [0.0000, 0.0000, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                    ],
+                    [
+                        [0.0000, 0.0000, 5.0000],
+                        [-1.6667, -1.6667, 5.0000],
+                        [-1.6667, 1.6667, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                    ],
+                    [
+                        [0.0000, 0.0000, 5.0000],
+                        [1.6667, -1.6667, 5.0000],
+                        [1.6667, 1.6667, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                    ],
+                    [
+                        [0.0000, 0.0000, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                        [0.0000, 0.0000, 5.0000],
+                    ],
+                ]
+            ]
+        ],
+        device=device,
+    )
+
+    torch.testing.assert_close(
+        fixed_gradients,
+        expected_gradients,
+        atol=5e-2,
+        rtol=5e-2,
+    )
+
+
+def test_total_variation_loss(
+    device: torch.device,
+) -> None:
+    """
+    Test the total variation loss function.
+
+    Parameters
+    ----------
+    device : torch.device
+        The device on which to initialize tensors.
+
+    Raises
+    ------
+    AssertionError
+        If test does not complete as expected.
+    """
+    rows = torch.linspace(0, 4 * 3.1415, 120, device=device)
+    columns = torch.linspace(0, 4 * 3.1415, 120, device=device)
+    x, y = torch.meshgrid(rows, columns, indexing="ij")
+
+    # Smooth surface with waves.
+    z_values_smooth = 0.5 * torch.sin(x) + 0.5 * torch.cos(y)
+
+    # Irregular surface = smooth surface with waves and random noise.
+    noise = torch.randn_like(z_values_smooth, device=device) * 0.5
+    z_irregular = z_values_smooth + noise
+
+    coordinates_smooth = torch.stack(
+        [x.flatten(), y.flatten(), z_values_smooth.flatten()], dim=1
+    ).unsqueeze(0)
+    coordinates_irregular = torch.stack(
+        [x.flatten(), y.flatten(), z_irregular.flatten()], dim=1
+    ).unsqueeze(0)
+
+    surfaces = (
+        torch.cat([coordinates_smooth, coordinates_irregular], dim=0)
+        .unsqueeze(1)
+        .expand(2, 4, -1, 3)
+    )
+
+    # Calculate total variation loss
+    loss = SurfaceReconstructor.total_variation_loss_per_surface(
+        surfaces=surfaces, number_of_neighbors=10, radius=0.5, sigma=1.0, device=device
+    )
+
+    torch.testing.assert_close(
+        loss,
+        torch.tensor(
+            [
+                [0.043646000326, 0.043646000326, 0.043646000326, 0.043646000326],
+                [0.564661264420, 0.564661264420, 0.564661264420, 0.564661264420],
+            ],
+            device=device,
+        ),
+        atol=5e-2,
+        rtol=5e-2,
+    )
