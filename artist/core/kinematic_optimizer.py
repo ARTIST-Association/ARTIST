bug fixes in distributed gradient flow

artist/core/kinematic_optimizer.py
@@ -348,7 +348,7 @@ class KinematicOptimizer:
                     )
 
                     if self.ddp_setup["is_nested"]:
-                        torch.distributed.nn.functional.all_reduce(
+                        flux_distributions = torch.distributed.nn.functional.all_reduce(
                             flux_distributions,
                             group=self.ddp_setup["process_subgroup"],
                             op=torch.distributed.ReduceOp.SUM,
@@ -372,6 +372,17 @@ class KinematicOptimizer:
                     loss = (focal_spots - focal_spots_measured).abs().mean()
                     loss.backward()
 
+                    if self.ddp_setup["is_nested"]:
+                        # Reduce gradients within each heliostat group.
+                        for param_group in optimizer.param_groups:
+                            for param in param_group["params"]:
+                                if param.grad is not None:
+                                    param.grad = torch.distributed.nn.functional.all_reduce(
+                                        param.grad,
+                                        op=torch.distributed.ReduceOp.SUM,
+                                        group=self.ddp_setup["process_subgroup"],
+                                    )
+
                     optimizer.step()
 
                     if epoch % log_step == 0:
@@ -384,14 +395,9 @@ class KinematicOptimizer:
                 log.info(f"Rank: {rank}, kinematic parameters optimized.")
 
         if self.ddp_setup["is_distributed"]:
-            for heliostat_group in self.scenario.heliostat_field.heliostat_groups:
-                torch.distributed.nn.functional.all_reduce(
-                    heliostat_group.kinematic.deviation_parameters,
-                    op=torch.distributed.ReduceOp.SUM,
-                )
-                torch.distributed.nn.functional.all_reduce(
-                    heliostat_group.kinematic.actuators.actuator_parameters,
-                    op=torch.distributed.ReduceOp.SUM,
-                )
+            for index, heliostat_group in enumerate(self.scenario.heliostat_field.heliostat_groups):
+                source = self.ddp_setup['ranks_to_groups_mapping'][index]
+                torch.distributed.broadcast(heliostat_group.kinematic.deviation_parameters, src=source[0])
+                torch.distributed.broadcast(heliostat_group.kinematic.actuators.actuator_parameters, src=source[0])
 
-        log.info(f"Rank: {rank}, synchronised after kinematic calibration.")
+            log.info(f"Rank: {rank}, synchronised after kinematic calibration.")

tutorials/03_nurbs_surface_reconstruction.py
@@ -17,9 +17,6 @@ set_logger_config()
 # Set the device
 device = get_device()
 
-torch.autograd.set_detect_anomaly(True)
-
-
 # Specify the path to your scenario.h5 file.
 scenario_path = pathlib.Path("/workVERLEIHNIX/mb/ARTIST/tutorials/data/scenarios/test_scenario_paint_multiple_heliostat_groups_deflectometry.h5")
 
@@ -32,23 +29,23 @@ heliostat_data_mapping = [
             pathlib.Path(
                 "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/270398-calibration-properties.json"
             ),
-            # pathlib.Path(
-            #     "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/271633-calibration-properties.json"
-            # ),
-            # pathlib.Path(
-            #     "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/275564-calibration-properties.json"
-            # ),
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/271633-calibration-properties.json"
+            ),
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/275564-calibration-properties.json"
+            ),
         ],
         [
             pathlib.Path(
                 "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/270398-flux-centered.png"
             ),
-            # pathlib.Path(
-            #     "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/271633-flux-centered.png"
-            # ),
-            # pathlib.Path(
-            #     "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/275564-flux-centered.png"
-            # ),
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/271633-flux-centered.png"
+            ),
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AA39/275564-flux-centered.png"
+            ),
         ],
     ),
     # (
@@ -70,25 +67,25 @@ heliostat_data_mapping = [
     #         ),
     #     ],
     # ),
-    # (
-    #     "AC43",
-    #     [
-    #         pathlib.Path(
-    #             "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/62900-calibration-properties.json"
-    #         ),
-    #         pathlib.Path(
-    #             "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/72752-calibration-properties.json"
-    #         ),
-    #     ],
-    #     [
-    #         pathlib.Path(
-    #             "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/62900-flux-centered.png"
-    #         ),
-    #         pathlib.Path(
-    #             "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/72752-flux-centered.png"
-    #         ),
-    #     ],
-    # ),
+    (
+        "AC43",
+        [
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/62900-calibration-properties.json"
+            ),
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/72752-calibration-properties.json"
+            ),
+        ],
+        [
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/62900-flux-centered.png"
+            ),
+            pathlib.Path(
+                "/workVERLEIHNIX/mb/ARTIST/tutorials/data/paint/AC43/72752-flux-centered.png"
+            ),
+        ],
+    ),
 ]
 
 number_of_heliostat_groups = Scenario.get_number_of_heliostat_groups_from_hdf5(
@@ -108,30 +105,12 @@ with setup_distributed_environment(
         )
 
     # Set optimizer parameters.
-    scenario.light_sources.light_source_list[0].number_of_rays = 200
+    scenario.light_sources.light_source_list[0].number_of_rays = 20
     tolerance = 0.00005
-    max_epoch = 2000
-    initial_learning_rate = 1e-6
-    number_of_surface_points = torch.tensor([80, 80], device=device)
+    max_epoch = 10
+    initial_learning_rate = 1e-5
+    number_of_surface_points = torch.tensor([100, 100], device=device)
     resolution = torch.tensor([256, 256], device=device)
-
-    use_scheduler = "exponential" 
-    # Set up schedulers.
-    if use_scheduler == "exponential":
-        scheduler_parameters = {
-            "type": "exponential",
-            "gamma": [1.002] #[0.994, 0.996, 0.998]
-        }
-
-    if use_scheduler == "reduce_on_plateau":
-        scheduler_parameters = {
-            "type": "reduce_on_plateau",
-            "cooldown": [50],
-            "factor": [0.1, 0.5, 0.9],
-            "min_lr": [1e-9],
-            "patience": [50]
-        }
-
 
     # Create the surface reconstructor.
     surface_reconstructor = SurfaceReconstructor(
@@ -143,7 +122,6 @@ with setup_distributed_environment(
         initial_learning_rate=initial_learning_rate,
         tolerance=tolerance,
         max_epoch=max_epoch,
-        scheduler_parameters=scheduler_parameters,
         num_log=max_epoch,
         device=device,
     )