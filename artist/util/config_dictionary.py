28ea93a36808b28fe4cd6a924773d0907ab54d11
Filos1992 <max.pargmann@dlr.de>
2025-08-28 15:06:34 +0200

    added plot colors

--- artist/util/config_dictionary.py
+++ artist/util/config_dictionary.py
@@ -318,3 +318,18 @@ crop_target_width = 6
 """Key to crop the target size width."""
 crop_target_height = 6
 """Key to crop the target size height."""
+
+
+# Plot Settings.
+helmholtz_colors = {
+    "hgfblue": "#005AA0",
+    "hgfdarkblue": "#0A2D6E",
+    "hgfgreen": "#8CB423",
+    "hgfgray": "#5A696E",
+    "hgfaerospace": "#50C8AA",
+    "hgfearthandenvironment": "#326469",
+    "hgfenergy": "#FFD228",
+    "hgfhealth": "#D23264",
+    "hgfkeytechnologies": "#A0235A",
+    "hgfmatter": "#F0781E",
+}
