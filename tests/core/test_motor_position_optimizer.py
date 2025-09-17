406dff5d417c612cbb0008b7499b320fd94f2b3b
Kaleb Phipps <kaleb.phipps@kit.edu>
2025-09-17 17:42:52 +0200

    revert to correct tolerances

--- tests/core/test_motor_position_optimizer.py
+++ tests/core/test_motor_position_optimizer.py
@@ -162,6 +162,6 @@ def test_motor_positions_optimizer(
         torch.testing.assert_close(
             heliostat_group.kinematic.motor_positions,
             expected,
-            atol=1,
-            rtol=1,
+            atol=5e-3,
+            rtol=5e-3,
         )
