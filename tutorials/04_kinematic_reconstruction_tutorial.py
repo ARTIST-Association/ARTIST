9fcafddd79e5b6d82b5ffa30dd7a7a340b7d7fd5
MarleneBusch <marlene.busch@dlr.de>
2025-11-13 11:55:46 +0100

    adapt tutorial to changes in kinematic reconstructor

--- tutorials/04_kinematic_reconstruction_tutorial.py
+++ tutorials/04_kinematic_reconstruction_tutorial.py
@@ -9,7 +9,6 @@ from matplotlib import pyplot as plt
 from artist.core.heliostat_ray_tracer import HeliostatRayTracer
 from artist.core.kinematic_reconstructor import KinematicReconstructor
 from artist.core.loss_functions import FocalSpotLoss
-from artist.data_parser import paint_scenario_parser
 from artist.data_parser.calibration_data_parser import CalibrationDataParser
 from artist.data_parser.paint_calibration_parser import PaintCalibrationDataParser
 from artist.scenario.scenario import Scenario
@@ -21,7 +20,7 @@ torch.cuda.manual_seed(7)
 
 #############################################################################################################
 # Define helper functions for the plots.
-# Skip to line 146 for the tutorial code.
+# Skip to line 145 for the tutorial code.
 #############################################################################################################
 
 
