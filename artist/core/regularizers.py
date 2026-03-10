d87bcf8d5a8a43d33bc3a48c5dbededf853aa57c
Marie Weiel <marie.weiel@kit.edu>
2026-03-10 09:38:04 +0100

    fix docstring

--- artist/core/regularizers.py
+++ artist/core/regularizers.py
@@ -98,8 +98,8 @@ class SmoothnessRegularizer(Regularizer):
         current_control_points : torch.Tensor
             The current control points.
             Tensor of shape [number_of_heliostats, number_of_facets_per_surface, number_of_control_points_u_direction, number_of_control_points_v_direction, 3].
-        original_control_points : torch.Tensor
-            The current control points.
+        original_control_points : torch.Tensƒor
+            The original control points.
             Tensor of shape [number_of_heliostats, number_of_facets_per_surface, number_of_control_points_u_direction, number_of_control_points_v_direction, 3].
         device : torch.device | None
             The device on which to perform computations or load tensors and models (default is None).
