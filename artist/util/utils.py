1f6e0aa7f22a03d9db94bf3c375f2f2d693053e4
Marie Weiel <marie.weiel@kit.edu>
2026-04-21 21:22:38 +0200

    try to fix bug again

--- artist/util/utils.py
+++ artist/util/utils.py
@@ -493,8 +493,8 @@ def rotation_angle_and_axis(
         The angle of the rotation as a scalar tensor.
     """
     device = get_device(device=device)
-    from_orientation = torch.nn.functional.normalize(from_orientation[:3])
-    to_orientation = torch.nn.functional.normalize(to_orientation[:3])
+    from_orientation = torch.nn.functional.normalize(from_orientation[:3], dim=0)
+    to_orientation = torch.nn.functional.normalize(to_orientation[:3], dim=0)
     dot = torch.clamp(torch.dot(from_orientation, to_orientation), -1.0, 1.0)
     angle = torch.acos(dot)
     axis = torch.linalg.cross(from_orientation, to_orientation)
