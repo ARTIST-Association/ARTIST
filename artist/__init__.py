fac5bdb121db9f6494d1b0c161dc3dcfcc816ce9
Marie Weiel <marie.weiel@kit.edu>
2026-05-04 21:31:39 +0200

    update init to reflect new package structure

--- artist/__init__.py
+++ artist/__init__.py
@@ -2,14 +2,27 @@
 
 import os
 
-from . import field, io, nurbs, optimization, raytracing, scenario, scene, util
+from . import (
+    field,
+    flux,
+    geometry,
+    io,
+    nurbs,
+    optimization,
+    raytracing,
+    scenario,
+    scene,
+    util,
+)
 
 ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
 """Reference to the root directory of ``ARTIST``."""
 
 __all__ = [
-    "io",
     "field",
+    "flux",
+    "geometry",
+    "io",
     "nurbs",
     "optimization",
     "raytracing",
