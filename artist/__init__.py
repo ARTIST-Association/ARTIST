3a80b25abb6a8a7ff2cf86033f9ca1532b08dd17
Marie Weiel <marie.weiel@kit.edu>
2026-08-05 14:16:53 +0200

    fix package name

--- artist/__init__.py
+++ artist/__init__.py
@@ -7,7 +7,7 @@ ARTIST_ROOT = f"{os.sep}".join(__file__.split(os.sep)[:-2])
 """Reference to the root directory of ``ARTIST``."""
 
 try:
-    __version__ = version("paint-csp")
+    __version__ = version("artist-csp")
 except PackageNotFoundError:
     # Allows running from source without installation.
     __version__ = "0.0.0"
